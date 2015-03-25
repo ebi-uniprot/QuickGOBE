973cac542a846aea06e352b2f618c67915607bbf
twardell <twardell@ebi.ac.uk>
2015-03-25 11:08:00 +0000

    Change the name of the WebUtils method.

--- quickgo-webapp/src/main/java/uk/ac/ebi/quickgo/controller/SlimmingController.java
+++ quickgo-webapp/src/main/java/uk/ac/ebi/quickgo/controller/SlimmingController.java
@@ -29,9 +29,9 @@ import uk.ac.ebi.quickgo.web.util.term.TermUtil;
 
 /**
  * Controller for slimming process
- * 
+ *
  * @author cbonill
- * 
+ *
  */
 @Controller
 @Scope("session")
@@ -39,11 +39,11 @@ public class SlimmingController {
 
 	// All go terms
 	Map<String, GenericTerm> terms = TermUtil.getGOTerms();
-	
+
 	// Structure contains associated terms to each predefined set (to avoid recalculating it)
 	public static Map<String, Map<String,String>> predefindSetTerms = new HashMap<String, Map<String,String>>();
-	
-	
+
+
 	@RequestMapping(value = { "/", "annotation" }, method = {
 			RequestMethod.POST, RequestMethod.GET }, params = { "ownTerms" })
 	public String addOwnTerms(
@@ -51,18 +51,18 @@ public class SlimmingController {
 			HttpServletRequest httpRequest, HttpSession session, Model model) {
 		Map<String, String> ownTerms = SlimmingUtil.getTermsFromSession(SlimmingUtil.OWN_TERMS_ADDED, session);
 		Map<String, String> slimmingTerms = SlimmingUtil.getTermsFromSession(SlimmingUtil.SLIMMING_TERMS_ATTRIBUTE, session);
-		List<String> terms = WebUtils.processFilterValues(termsIds);	
+		List<String> terms = WebUtils.parseAndFormatFilterValues(termsIds);
 		Map<String, String> idName = calculateNames(terms);
 		ownTerms.putAll(idName);
 		session.setAttribute(SlimmingUtil.OWN_TERMS_ADDED, ownTerms);
 		slimmingTerms.putAll(idName);
 		session.setAttribute(SlimmingUtil.SLIMMING_TERMS_ATTRIBUTE, slimmingTerms);
-		
+
 		setSlimmingTermsByAspect(session);
 		return View.INDEX;
 
 	}
-	
+
 	@RequestMapping(value = { "/", "annotation" }, method = {
 			RequestMethod.POST, RequestMethod.GET }, params = { "basketTerms" })
 	public String addBasketTerms(
@@ -70,17 +70,17 @@ public class SlimmingController {
 			HttpServletRequest httpRequest, HttpSession session, Model model) {
 		Map<String, String>  basketTerms = SlimmingUtil.getTermsFromSession(SlimmingUtil.BASKET_TERMS_ADDED, session);
 		Map<String, String> slimmingTerms = SlimmingUtil.getTermsFromSession(SlimmingUtil.SLIMMING_TERMS_ATTRIBUTE, session);
-		List<String> terms = WebUtils.processFilterValues(termsIds);
+		List<String> terms = WebUtils.parseAndFormatFilterValues(termsIds);
 		Map<String, String> idName = calculateNames(terms);
 		basketTerms.putAll(idName);
 		session.setAttribute(SlimmingUtil.BASKET_TERMS_ADDED, basketTerms);
 		slimmingTerms.putAll(idName);
 		session.setAttribute(SlimmingUtil.SLIMMING_TERMS_ATTRIBUTE, slimmingTerms);
-		
+
 		setSlimmingTermsByAspect(session);
 		return View.INDEX;
 
-	}	
+	}
 
 	@RequestMapping(value = { "/", "annotation" }, method = {
 			RequestMethod.POST, RequestMethod.GET }, params = { "set" })
@@ -89,10 +89,10 @@ public class SlimmingController {
 			HttpServletRequest httpRequest, HttpSession session, Model model) {
 		Map<String, String>  predefinedSetTerms = SlimmingUtil.getTermsFromSession(SlimmingUtil.SETS_TERMS_ADDED, session);
 		Map<String, String> slimmingTerms = SlimmingUtil.getTermsFromSession(SlimmingUtil.SLIMMING_TERMS_ATTRIBUTE, session);
-		
+
 		Map<String, String> setTerms = new HashMap<>();
 		if (setName != null && !setName.isEmpty()) {
-		
+
 			if (!predefindSetTerms.containsKey(setName)) {// Check if it was calculated before
 				for (GenericTerm goTerm : terms.values()) {
 					if (goTerm.getSubsetsNames().contains(setName)) {
@@ -102,28 +102,28 @@ public class SlimmingController {
 				predefindSetTerms.put(setName, setTerms);
 			} else {// Get cached values
 				setTerms = predefindSetTerms.get(setName);
-			}		
+			}
 			predefinedSetTerms.putAll(setTerms);
 			session.setAttribute(SlimmingUtil.SETS_TERMS_ADDED, predefinedSetTerms);
 			session.setAttribute("selectedSet", setName);
 			slimmingTerms.putAll(setTerms);
 			session.setAttribute(SlimmingUtil.SLIMMING_TERMS_ATTRIBUTE, slimmingTerms);
 		}
-		
+
 		setSlimmingTermsByAspect(session);
 		return View.INDEX;
-	}	
-	
+	}
+
 	@RequestMapping(value = { "/", "annotation" }, method = {
 			RequestMethod.POST, RequestMethod.GET }, params = { "inactiveSlimmingTermId" })
 	public String inactivateSlimmingTerm(
 			@RequestParam(value = "inactiveSlimmingTermId", defaultValue = "", required = false) String inactiveSlimmingTermId,
 			HttpServletRequest httpRequest, HttpSession session, Model model) {
-			
-		Map<String, String> slimmingTerms = SlimmingUtil.getTermsFromSession(SlimmingUtil.SLIMMING_TERMS_ATTRIBUTE, session);		
-		
+
+		Map<String, String> slimmingTerms = SlimmingUtil.getTermsFromSession(SlimmingUtil.SLIMMING_TERMS_ATTRIBUTE, session);
+
 		Map<String, String> inactiveSlimmingTerms = SlimmingUtil.getTermsFromSession(SlimmingUtil.INACTIVE_SLIMMING_TERMS_ATTRIBUTE, session);
-		
+
 		if (inactiveSlimmingTermId.equals(EGOAspect.F.abbreviation)) {
 			inactiveSlimmingTerms.putAll(SlimmingUtil.getTermsFromSession(SlimmingUtil.MF_SLIMMING_TERMS_ATTRIBUTE, session));
 		} else if (inactiveSlimmingTermId.equals(EGOAspect.P.abbreviation)) {
@@ -131,21 +131,21 @@ public class SlimmingController {
 		} else if (inactiveSlimmingTermId.equals(EGOAspect.C.abbreviation)) {
 			inactiveSlimmingTerms.putAll(SlimmingUtil.getTermsFromSession(SlimmingUtil.CC_SLIMMING_TERMS_ATTRIBUTE, session));
 		} else { // GO term
-			inactiveSlimmingTerms.put(inactiveSlimmingTermId, slimmingTerms.get(inactiveSlimmingTermId));		
+			inactiveSlimmingTerms.put(inactiveSlimmingTermId, slimmingTerms.get(inactiveSlimmingTermId));
 		}
 		session.setAttribute(SlimmingUtil.INACTIVE_SLIMMING_TERMS_ATTRIBUTE, inactiveSlimmingTerms);
-				
+
 		setSlimmingTermsByAspect(session);
 		return View.INDEX;
 	}
-	
-	
+
+
 	@RequestMapping(value = { "/", "annotation" }, method = {
 			RequestMethod.POST, RequestMethod.GET }, params = { "activeSlimmingTermId" })
 	public String activateSlimmingTerm(
 			@RequestParam(value = "activeSlimmingTermId", defaultValue = "", required = false) String activeSlimmingTermId,
 			HttpServletRequest httpRequest, HttpSession session, Model model) {
-			
+
 		Map<String, String> inactiveSlimmingTerms = SlimmingUtil.getTermsFromSession(SlimmingUtil.INACTIVE_SLIMMING_TERMS_ATTRIBUTE, session);
 		if (activeSlimmingTermId.equals(EGOAspect.F.abbreviation)) {
 			inactiveSlimmingTerms.keySet().removeAll(SlimmingUtil.getTermsFromSession(SlimmingUtil.MF_SLIMMING_TERMS_ATTRIBUTE, session).keySet());
@@ -153,32 +153,32 @@ public class SlimmingController {
 			inactiveSlimmingTerms.keySet().removeAll(SlimmingUtil.getTermsFromSession(SlimmingUtil.BP_SLIMMING_TERMS_ATTRIBUTE, session).keySet());
 		} else if (activeSlimmingTermId.equals(EGOAspect.C.abbreviation)) {
 			inactiveSlimmingTerms.keySet().removeAll(SlimmingUtil.getTermsFromSession(SlimmingUtil.CC_SLIMMING_TERMS_ATTRIBUTE, session).keySet());
-		} else { // GO term		
+		} else { // GO term
 			inactiveSlimmingTerms.remove(activeSlimmingTermId);
 		}
 		session.setAttribute(SlimmingUtil.INACTIVE_SLIMMING_TERMS_ATTRIBUTE, inactiveSlimmingTerms);
-				
+
 		setSlimmingTermsByAspect(session);
 		return View.INDEX;
 	}
-	
+
 	@RequestMapping(value = { "/", "annotation" }, method = {
 			RequestMethod.POST, RequestMethod.GET }, params = { "removeAll" })
 	public String removeAll(@RequestParam(value = "removeAll", defaultValue = "true", required = false) String removeAll,
 			HttpServletRequest httpRequest, HttpSession session, Model model) {
-		
+
 		SlimmingUtil.removeAllSlimAttributes(session);
-		
+
 		return View.INDEX;
 	}
-	
-	
+
+
 	@RequestMapping(value = { "/", "annotation" }, method = {RequestMethod.POST, RequestMethod.GET }, params = { "slim", "proteinIds", "proteinSets" })
-	public ModelAndView slim(@RequestParam(value = "slim", defaultValue = "true", required = false) String slim, 
+	public ModelAndView slim(@RequestParam(value = "slim", defaultValue = "true", required = false) String slim,
 			@RequestParam(value = "proteinIds", defaultValue = "", required = false) String proteinIds,
 			@RequestParam(value = "proteinSets", defaultValue = "", required = false) String proteinSets,
 			HttpServletRequest httpRequest, HttpSession session, Model model) {
-		
+
 		// Go Ids
 		Map<String, String> slimmingTerms = SlimmingUtil.getTermsFromSession(SlimmingUtil.SLIMMING_TERMS_ATTRIBUTE, session);
 		Map<String, String> inactiveSlimmingTerms = SlimmingUtil.getTermsFromSession(SlimmingUtil.INACTIVE_SLIMMING_TERMS_ATTRIBUTE, session);
@@ -189,24 +189,24 @@ public class SlimmingController {
 		appliedFilterSet.addParameters(goSlimmingTerms);
 		// Proteins Ids
 		if(!proteinIds.trim().isEmpty()){
-			List<String> proteinsIds = WebUtils.processFilterValues(proteinIds);
+			List<String> proteinsIds = WebUtils.parseAndFormatFilterValues(proteinIds);
 			Map<String, List<String>> proteins = new HashMap<>();
 			proteins.put(AnnotationField.DBOBJECTID.getValue(), proteinsIds);
 			appliedFilterSet.addParameters(proteins);
 		}
 		// Proteins Sets
 		if(!proteinSets.trim().isEmpty()){
-			List<String> proteinsSets = WebUtils.processFilterValues(proteinSets);
+			List<String> proteinsSets = WebUtils.parseAndFormatFilterValues(proteinSets);
 			Map<String, List<String>> sets = new HashMap<>();
 			sets.put(AnnotationField.TARGETSET.getValue(), proteinsSets);
 			appliedFilterSet.addParameters(sets);
 		}
-		
+
 		session.setAttribute("appliedFilters", appliedFilterSet);
 
         return new ModelAndView("redirect:" + View.ANNOTATIONS_PATH);
 	}
-	
+
 	/**
 	 * Given a list of terms ids, return associated names
 	 * @param ids Terms ids
@@ -221,7 +221,7 @@ public class SlimmingController {
 		}
 		return idName;
 	}
-	
+
 	/**
 	 * Set slimming terms by aspect
 	 * @param session
@@ -231,8 +231,8 @@ public class SlimmingController {
 		Map<String, String>  slimmingTerms = SlimmingUtil.getTermsFromSession(SlimmingUtil.SLIMMING_TERMS_ATTRIBUTE, session);
 		Map<String, String>  mf_slimmingTerms = new HashMap<String, String>();
 		Map<String, String>  bp_slimmingTerms = new HashMap<String, String>();
-		Map<String, String>  cc_slimmingTerms = new HashMap<String, String>();		
-		
+		Map<String, String>  cc_slimmingTerms = new HashMap<String, String>();
+
 		for (String termId : slimmingTerms.keySet()) {
 			if (((GOTerm) terms.get(termId)).getAspect() == EGOAspect.F) {
 				mf_slimmingTerms.put(termId, slimmingTerms.get(termId));
@@ -244,6 +244,6 @@ public class SlimmingController {
 		}
 		session.setAttribute(SlimmingUtil.MF_SLIMMING_TERMS_ATTRIBUTE, mf_slimmingTerms);
 		session.setAttribute(SlimmingUtil.CC_SLIMMING_TERMS_ATTRIBUTE, cc_slimmingTerms);
-		session.setAttribute(SlimmingUtil.BP_SLIMMING_TERMS_ATTRIBUTE, bp_slimmingTerms);		
+		session.setAttribute(SlimmingUtil.BP_SLIMMING_TERMS_ATTRIBUTE, bp_slimmingTerms);
 	}
 }
