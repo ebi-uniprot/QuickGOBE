127f64dc8a79c163342b554c0872b37ae120de91
twardell <twardell@ebi.ac.uk>
2015-07-13 14:51:35 +0000

    Move from using Log4j to Simple Logging Facade For Java (SLF4J)

--- quickgo-solr-indexing/src/main/java/uk/ac/ebi/quickgo/indexer/QuickGOIndexerProcess.java
+++ quickgo-solr-indexing/src/main/java/uk/ac/ebi/quickgo/indexer/QuickGOIndexerProcess.java
@@ -4,6 +4,8 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,30 +14,31 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Process to index all the information in Solr
  *
  * @author cbonill
- *
  */
 public class QuickGOIndexerProcess {
 
 
-		static ApplicationContext appContext;
-		static QuickGOIndexer quickGOIndexer;
+	static ApplicationContext appContext;
+	static QuickGOIndexer quickGOIndexer;
 
-		/**
-		 * We can use this method for indexing the data in Solr for the moment
-		 *
-		 * @param args
-		 */
-		public static void main(String[] args) {
+	final static Logger logger = LoggerFactory.getLogger(QuickGOIndexerProcess.class);
 
-			appContext = new ClassPathXmlApplicationContext("common-beans.xml",	"indexing-beans.xml", "query-beans.xml");
-			quickGOIndexer = (QuickGOIndexer) appContext.getBean("quickGOIndexer");
-			String start = DateFormat.getInstance().format(Calendar.getInstance().getTime());
-			System.out.println("================================================================");
-			System.out.println("STARTED: " + start);
-			System.out.println("================================================================");
-			quickGOIndexer.index();
-			System.out.println("================================================================");
-			System.out.println("DONE: " + DateFormat.getInstance().format(Calendar.getInstance().getTime()));
-			System.out.println("================================================================");
-		}
+	/**
+	 * We can use this method for indexing the data in Solr for the moment
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		appContext = new ClassPathXmlApplicationContext("common-beans.xml", "indexing-beans.xml", "query-beans.xml");
+		quickGOIndexer = (QuickGOIndexer) appContext.getBean("quickGOIndexer");
+		String start = DateFormat.getInstance().format(Calendar.getInstance().getTime());
+		logger.info("================================================================");
+		logger.info("STARTED: " + start);
+		logger.info("================================================================");
+		quickGOIndexer.index();
+		logger.info("================================================================");
+		logger.info("DONE: " + DateFormat.getInstance().format(Calendar.getInstance().getTime()));
+		logger.info("================================================================");
+	}
 }
