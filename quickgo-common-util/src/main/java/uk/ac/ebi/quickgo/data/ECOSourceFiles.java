2404b992bde51d5625bc339702d97c14918cdf27
tonys <tonys@ebi.ac.uk>
2015-08-03 10:52:36 +0000

    Removed a bunch of warnings

--- quickgo-common-util/src/main/java/uk/ac/ebi/quickgo/data/ECOSourceFiles.java
+++ quickgo-common-util/src/main/java/uk/ac/ebi/quickgo/data/ECOSourceFiles.java
@@ -15,14 +15,14 @@ public class ECOSourceFiles extends OntologySourceFiles {
 	public ECOSourceFiles(File directory) {
 		super(directory);
 		
-		terms = new TSVDataFile<ETerm>(directory, "ECO_TERMS");
-		definitions = new TSVDataFile<ETermDefinition>(directory, "ECO_DEFINITIONS");
-		synonyms = new TSVDataFile<ETermSynonym>(directory, "ECO_SYNONYMS");
-		comments = new TSVDataFile<ETermComment>(directory, "ECO_COMMENTS");
-		relations = new TSVDataFile<ETermRelation>(directory, "ECO_RELATIONS");
-		xrefs = new TSVDataFile<ETermXref>(directory, "ECO_XREFS");
-		history = new TSVDataFile<ETermHistory>(directory, "ECO_TERM_HISTORY");
-		credits = new TSVDataFile<ETermCredit>(directory, "ECO_TERM_CREDITS");
+		terms = new TSVDataFile<>(directory, "ECO_TERMS");
+		definitions = new TSVDataFile<>(directory, "ECO_DEFINITIONS");
+		synonyms = new TSVDataFile<>(directory, "ECO_SYNONYMS");
+		comments = new TSVDataFile<>(directory, "ECO_COMMENTS");
+		relations = new TSVDataFile<>(directory, "ECO_RELATIONS");
+		xrefs = new TSVDataFile<>(directory, "ECO_XREFS");
+		history = new TSVDataFile<>(directory, "ECO_TERM_HISTORY");
+		credits = new TSVDataFile<>(directory, "ECO_TERM_CREDITS");
 	}
 
 	@Override
