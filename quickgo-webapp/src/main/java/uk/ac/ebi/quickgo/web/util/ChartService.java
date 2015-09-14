00f24e49de24c7b9fb81b57b1aef6f58ceb22a89
twardell <twardell@ebi.ac.uk>
2015-09-14 09:27:41 +0000

    Define set of relations for GO and ECO Terms.

--- quickgo-webapp/src/main/java/uk/ac/ebi/quickgo/web/util/ChartService.java
+++ quickgo-webapp/src/main/java/uk/ac/ebi/quickgo/web/util/ChartService.java
@@ -23,35 +23,42 @@ import uk.ac.ebi.quickgo.web.util.term.TermUtil;
 /**
  * Service to generate charts
  * @author cbonill
- * //TODO Refactor OntologyGraphController to use this class 
+ * //TODO Refactor OntologyGraphController to use this class
  */
 @Service
 public class ChartService {
 
 	GenericOntology genericOntology;
-	
+
 	GraphImage graphImage;
-	
+
 	RenderedImage renderableImage;
-	
+
 	String termsToDisplay;
-	
+
+	private static final EnumSet ECO_SET = EnumSet.of(RelationType.USEDIN, RelationType.ISA, RelationType.PARTOF,
+			RelationType.REGULATES, RelationType.OCCURSIN); /*RelationType.HASPART,*/
+
+	private static final EnumSet GO_SET = EnumSet.of(RelationType.ISA, RelationType.PARTOF,
+			RelationType.REGULATES, RelationType.POSITIVEREGULATES, RelationType.NEGATIVEREGULATES,
+			RelationType.OCCURSIN, RelationType.CAPABLEOF, RelationType.CAPABLEOFPARTOF); /*RelationType.HASPART,*/
+
 	public void createChart(String ids, String scope) {
 		genericOntology = null;
 		graphImage = null;
 		renderableImage = null;
 		termsToDisplay = "";
-		
+
 		boolean isGO = true;
 		if (scope != null && scope.trim().length() > 0) {
 			if(scope.equalsIgnoreCase(ECOTerm.ECO)){
 				isGO = false;
 			}
 		}
-		
-		List<String> termsIdsList = new ArrayList<>();		
+
+		List<String> termsIdsList = new ArrayList<>();
 		termsIdsList = Arrays.asList(ids.split(","));
-		
+
 		List<String> formattedTermsIdsList = new ArrayList<>();
 		for(String id : termsIdsList){
 			if ((isGO && id.matches(AnnotationParameters.GO_ID_REG_EXP
@@ -59,26 +66,26 @@ public class ChartService {
 					|| (!isGO && id.matches(AnnotationParameters.ECO_ID_REG_EXP
 							+ "\\d{7}"))) {// Valid GO id
 				formattedTermsIdsList.add(id);
-			}	
+			}
 		}
-		
+
 		termsIdsList = formattedTermsIdsList;
 		if(termsIdsList.size() > 0){
 			// Get corresponding ontology
 			genericOntology = TermUtil.getOntology(termsIdsList.get(0));
-			
+
 			// Create graph image
 			graphImage = createRenderableImage(ids);
 			renderableImage = graphImage.render();
-			
+
 			if(termsIdsList.size() == 1){//Just one term, set id as the graph title
-				termsToDisplay = "Ancestor chart for " + termsIdsList.get(0);							
+				termsToDisplay = "Ancestor chart for " + termsIdsList.get(0);
 			} else {
 				termsToDisplay = "Comparison chart for " + String.valueOf(termsIdsList.size());
 			}
 		}
 	}
-	
+
 	/**
 	 * Generates ancestors graph for a list of terms
 	 * @param termsIds List of terms to calculate the graph for
@@ -88,7 +95,7 @@ public class ChartService {
 		// Check if the selected terms exist
 		List<GenericTerm> terms = new ArrayList<GenericTerm>();
 		List<String> termsIdsList = Arrays.asList(termsIds.split(","));
-		for(String id : termsIdsList){			
+		for(String id : termsIdsList){
 			GenericTerm term = genericOntology.getTerm(id);
 			if(term != null){
 				terms.add(term);
@@ -98,16 +105,13 @@ public class ChartService {
 		// Build GO term set
 		GenericTermSet termSet = new GenericTermSet(genericOntology, "Term Set", 0);
 		for(GenericTerm term : terms){
-			termSet.addTerm(term);			
-		}		
-		
+			termSet.addTerm(term);
+		}
+
 		// Create ontology graph
-		OntologyGraph ontologyGraph = OntologyGraph.makeGraph(termSet, EnumSet
-				.of(RelationType.USEDIN, RelationType.ISA, RelationType.PARTOF,
-						RelationType.REGULATES, /*RelationType.HASPART,*/
-						RelationType.OCCURSIN), 0, 0, new GraphPresentation());
-		return ontologyGraph.layout();			
-	}	
+		OntologyGraph ontologyGraph = OntologyGraph.makeGraph(termSet, GO_SET, 0, 0, new GraphPresentation());
+		return ontologyGraph.layout();
+	}
 
 	public GenericOntology getGenericOntology() {
 		return genericOntology;
