9818b31a32d23da6c1b18be4c66d144bc5fb70dd
twardell <twardell@ebi.ac.uk>
2015-10-13 14:31:53 +0000

    Change some class and method names.

--- quickgo-webapp/src/main/java/uk/ac/ebi/quickgo/webservice/mapping/QueryToSolrProcess.java
+++ quickgo-webapp/src/main/java/uk/ac/ebi/quickgo/webservice/mapping/QueryToSolrProcess.java
@@ -2,8 +2,9 @@ package uk.ac.ebi.quickgo.webservice.mapping;
 
 import uk.ac.ebi.quickgo.solr.query.model.annotation.enums.AnnotationField;
 import uk.ac.ebi.quickgo.web.util.annotation.AnnotationWSUtil;
-import uk.ac.ebi.quickgo.webservice.definitions.FilterRequest;
+import uk.ac.ebi.quickgo.webservice.definitions.FilterRequestDefinition;
 import uk.ac.ebi.quickgo.webservice.definitions.WebServiceFilter;
+import uk.ac.ebi.quickgo.webservice.model.FilterRequest;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,38 +17,35 @@ import java.util.Map;
  */
 public class QueryToSolrProcess {
 
-	private final uk.ac.ebi.quickgo.webservice.model.FilterRequest filterRequest;
-	private final Map<WebServiceFilter, FilterMapping> filterMappings;
+	private final Map<WebServiceFilter, FilterMapping> filterMappers;
 
-	public QueryToSolrProcess(uk.ac.ebi.quickgo.webservice.model.FilterRequest filterRequest, AnnotationWSUtil annotationWSUtil) {
+	public QueryToSolrProcess(AnnotationWSUtil annotationWSUtil) {
 
-		this.filterRequest = filterRequest;
-
-		 filterMappings =  createMappingsList(annotationWSUtil);
+		 filterMappers = createMappingsList(annotationWSUtil);
 	}
 
 
-	public String toSolrQuery() {
+	public String toSolrQuery(FilterRequest filterRequest) {
 
 		boolean first=true;
 
 		StringBuilder stringBuilder = new StringBuilder();
-		for(FilterMapping aMapping: filterMappings.values()){
+		for(FilterMapping aMapping: filterMappers.values()){
 
-			aMapping.processRequestObject(this.filterRequest);
-			String fragment = aMapping.solrQueryFragment();
-			if(fragment.length()>0){
+			aMapping.processRequestObject(filterRequest);
+			String solrQueryForFilter = aMapping.toSolrQuery();
+			if(solrQueryForFilter.length()>0){
 
 				if(first){
 					stringBuilder.append("(");
-					stringBuilder.append(fragment);
+					stringBuilder.append(solrQueryForFilter);
 					stringBuilder.append(")");
 					first=false;
 				}else {
 
 					//
 					stringBuilder.append(" AND (");
-					stringBuilder.append(fragment);
+					stringBuilder.append(solrQueryForFilter);
 					stringBuilder.append(")");
 				}
 			}
@@ -73,38 +71,38 @@ public class QueryToSolrProcess {
 		//Gene Product Type
 		//Limits the scope to annotations where the target (annotated gene product) is of the specified type(s)
 		//Eg protein, rna, complex
-		filterMappings.put(WebServiceFilter.GeneProductType, new FilterMappingSimple(FilterRequest.GeneProductType, AnnotationField.DBOBJECTTYPE));
+		filterMappings.put(WebServiceFilter.GeneProductType, new FilterMappingSimple(FilterRequestDefinition.GeneProductType, AnnotationField.DBOBJECTTYPE));
 
-		filterMappings.put(WebServiceFilter.Database, new FilterMappingSimple(FilterRequest.Database, AnnotationField.DB));
+		filterMappings.put(WebServiceFilter.Database, new FilterMappingSimple(FilterRequestDefinition.Database, AnnotationField.DB));
 
 		//todo OK??
-		filterMappings.put(WebServiceFilter.GeneProductSet, new FilterMappingSimple(FilterRequest.GeneProductSet, AnnotationField.TARGETSETS));
+		filterMappings.put(WebServiceFilter.GeneProductSet, new FilterMappingSimple(FilterRequestDefinition.GeneProductSet, AnnotationField.TARGETSETS));
 
 		//todo does this exist??
 		//proteome --> something.
 
-		//filterMappings.put(WebServiceFilter.GeneProductId, new FilterMappingSimple(FilterRequest.GeneProductID, AnnotationField.DBOBJECTID));
-		filterMappings.put(WebServiceFilter.Aspect,new FilterMappingSimple(FilterRequest.Aspect, AnnotationField.GOASPECT));
-		filterMappings.put(WebServiceFilter.Qualifier,new FilterMappingSimple(FilterRequest.Qualifier, AnnotationField.QUALIFIER));
-		filterMappings.put(WebServiceFilter.Reference,new FilterMappingSimple(FilterRequest.Reference, AnnotationField.REFERENCE));
-		filterMappings.put(WebServiceFilter.With,new FilterMappingSimple(FilterRequest.With, AnnotationField.WITH));
-		filterMappings.put(WebServiceFilter.AssignedBy,new FilterMappingSimple(FilterRequest.AssignedBy, AnnotationField.ASSIGNEDBY));
-		filterMappings.put(WebServiceFilter.Taxon,new FilterMappingSimple(FilterRequest.Taxon, AnnotationField.TAXONOMYID));
+		//filterMappers.put(WebServiceFilter.GeneProductId, new FilterMappingSimple(FilterRequest.GeneProductID, AnnotationField.DBOBJECTID));
+		filterMappings.put(WebServiceFilter.Aspect,new FilterMappingSimple(FilterRequestDefinition.Aspect, AnnotationField.GOASPECT));
+		filterMappings.put(WebServiceFilter.Qualifier,new FilterMappingSimple(FilterRequestDefinition.Qualifier, AnnotationField.QUALIFIER));
+		filterMappings.put(WebServiceFilter.Reference,new FilterMappingSimple(FilterRequestDefinition.Reference, AnnotationField.REFERENCE));
+		filterMappings.put(WebServiceFilter.With,new FilterMappingSimple(FilterRequestDefinition.With, AnnotationField.WITH));
+		filterMappings.put(WebServiceFilter.AssignedBy,new FilterMappingSimple(FilterRequestDefinition.AssignedBy, AnnotationField.ASSIGNEDBY));
+		filterMappings.put(WebServiceFilter.Taxon,new FilterMappingSimple(FilterRequestDefinition.Taxon, AnnotationField.TAXONOMYID));
 
 
 		//Here are the complicated mappings where there could be several parameters that affect the mapping to be used,
 		//and sometimes change the operation completely
 
-		filterMappings.put(WebServiceFilter.GeneProductId,new GeneProductId());
-		filterMappings.put(WebServiceFilter.GoTerm,new GoTerm(annotationWSUtil));
-		filterMappings.put(WebServiceFilter.EcoEvidence,new Evidence());
+		filterMappings.put(WebServiceFilter.GeneProductId,new FilterMappingsGeneProductId());
+		filterMappings.put(WebServiceFilter.GoTerm,new FilterMappingsGoTerm(annotationWSUtil));
+		filterMappings.put(WebServiceFilter.EcoEvidence,new FilterMappingsEvidence());
 
 
 		return filterMappings;
 	}
 
 	public boolean isSlimmingRequired() {
-		GoTerm goTermMapping = ((GoTerm)(filterMappings.get(WebServiceFilter.GoTerm)));
-		return goTermMapping.useTermsAsGoSlim();
+		FilterMappingsGoTerm filterMappingsGoTermMapping = ((FilterMappingsGoTerm)(filterMappers.get(WebServiceFilter.GoTerm)));
+		return filterMappingsGoTermMapping.useTermsAsGoSlim();
 	}
 }
