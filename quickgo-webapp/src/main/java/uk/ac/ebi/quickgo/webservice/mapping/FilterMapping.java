8afc9e4beac4999b6096883c5cfdd1cc76939dea
twardell <twardell@ebi.ac.uk>
2015-10-13 14:33:59 +0000

    Don't replace 'GO' with a wild card.

--- quickgo-webapp/src/main/java/uk/ac/ebi/quickgo/webservice/mapping/FilterMapping.java
+++ quickgo-webapp/src/main/java/uk/ac/ebi/quickgo/webservice/mapping/FilterMapping.java
@@ -17,19 +17,24 @@ public abstract class FilterMapping {
 
 	protected AnnotationField solrField = null;
 	protected List<String> args = new ArrayList<>();
-	public static final String GO_ID_REG_EXP = "(go:|GO:|gO:|Go:)";
+
 
 	public FilterMapping(AnnotationField solrField) {
 		this.solrField = solrField;
 	}
 
-	public String solrQueryFragment(){
+	/**
+	 * Return the completed solr query for this mapping.
+	 * Escape colons in the argument list so they don't conlflict with the required colon.
+	 * todo - why doesn't the api for solr do this automatically?
+ 	 */
+
+	//
+	public String toSolrQuery(){
 
 		if(args.size()<=0) return "";
-
 		String argList = StringUtils.join(args.toArray(), " OR ");
-		return(solrField.getValue() + ":(" +  (argList.toString()).replaceAll(Evidence.GO_ID_REG_EXP, "*").replaceAll(":","\\\\:") + ")");
-
+		return(solrField.getValue() + ":(" +  (argList.toString()).replaceAll(":","\\\\:") + ")");
 	}
 
 	public abstract void processRequestObject(FilterRequest filterRequest);
