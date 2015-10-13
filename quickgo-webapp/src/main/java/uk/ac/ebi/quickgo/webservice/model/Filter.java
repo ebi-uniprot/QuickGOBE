4935b6e2d385bd3d3680c424cdf2f8009672f2a7
twardell <twardell@ebi.ac.uk>
2015-10-13 14:34:37 +0000

    Make this class imutable.

--- quickgo-webapp/src/main/java/uk/ac/ebi/quickgo/webservice/model/Filter.java
+++ quickgo-webapp/src/main/java/uk/ac/ebi/quickgo/webservice/model/Filter.java
@@ -13,21 +13,22 @@ public class Filter {
 	private String type;
 	private String value;
 
+	public Filter() {};	//Jackson needs default constructor
+
+	public Filter(String type, String value) {
+		this.type = type;
+		this.value = value;
+	}
+
 	public String getType() {
 		return type;
 	}
 
-	public void setType(String type) {
-		this.type = type;
-	}
 
 	public String getValue() {
 		return value;
 	}
 
-	public void setValues(String value) {
-		this.value = value;
-	}
 
 	@Override
 	public String toString() {
