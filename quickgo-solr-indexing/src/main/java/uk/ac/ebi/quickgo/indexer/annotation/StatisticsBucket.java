55fcb360ab59e9966243288a669529504683359b
twardell <twardell@ebi.ac.uk>
2015-11-04 15:54:58 +0000

    Fix test.

--- quickgo-solr-indexing/src/main/java/uk/ac/ebi/quickgo/indexer/annotation/StatisticsBucket.java
+++ quickgo-solr-indexing/src/main/java/uk/ac/ebi/quickgo/indexer/annotation/StatisticsBucket.java
@@ -207,6 +207,7 @@ public class StatisticsBucket {
 
 		//Calculate percentages
 		for(StatisticTuple aTuple:topHits){
+			if(aTuple==null)break;
 			aTuple.calculateStatisticTuplePercentage(summaryAnnotationBucket);
 		}
 
