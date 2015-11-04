018e2aded4799a6205876c05acd9b5d13c185d82
Edward Turner <eddturner@ebi.ac.uk>
2015-11-04 16:06:01 +0000

    basic miscellaneous search test framework

---
+++ quickgo-solr/src/test/java/uk/ac/ebi/quickgo/search/miscellaneous/MiscellaneousSearchEngine.java
@@ -0,0 +1,27 @@
+package uk.ac.ebi.quickgo.search.miscellaneous;
+
+import uk.ac.ebi.quickgo.search.AbstractSearchEngine;
+import uk.ac.ebi.quickgo.solr.model.miscellaneous.SolrMiscellaneous;
+
+/**
+ * Created 02/11/15
+ * @author Edd
+ */
+public class MiscellaneousSearchEngine extends AbstractSearchEngine<SolrMiscellaneous> {
+
+    private static final String CORE_NAME = "miscellaneous";
+    private static final String ID = "id";
+
+    public MiscellaneousSearchEngine() {
+        super(CORE_NAME);
+    }
+
+    /**
+     * Note, that the identifier field, {@code id}, is not used by
+     * all {@code docType}s!
+     * @return
+     */
+    @Override protected String identifierField() {
+        return ID;
+    }
+}
