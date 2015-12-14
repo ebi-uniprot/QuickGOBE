45118a6629e9a2def288a46b4f627c42f38e1bfb
Edward Turner <eddturner@ebi.ac.uk>
2015-12-14 17:56:41 +0000

    formatted

--- model/src/main/java/uk/ac/ebi/quickgo/model/ontology/eco/EvidenceCodeOntology.java
+++ model/src/main/java/uk/ac/ebi/quickgo/model/ontology/eco/EvidenceCodeOntology.java
@@ -3,8 +3,9 @@ package uk.ac.ebi.quickgo.model.ontology.eco;
 import uk.ac.ebi.quickgo.model.ontology.generic.GenericOntology;
 
 public class EvidenceCodeOntology extends GenericOntology {
-	public final static String NAME_SPACE = "ECO";
-	public EvidenceCodeOntology() {
-		super(NAME_SPACE);
-	}
+    public final static String NAME_SPACE = "ECO";
+
+    public EvidenceCodeOntology() {
+        super(NAME_SPACE);
+    }
 }
