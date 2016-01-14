d4f67f91425878a4539ed36bac272235fae63ad4
Edward Turner <eddturner@gmail.com>
2016-01-14 11:23:31 +0000

    refactored common GO/ECO tests to a new common OBO controller test class. specific tests only in the specialised GO/OBO classes now.

--- rest/src/test/java/uk/ac/ebi/quickgo/rest/controller/ECOControllerIT.java
+++ rest/src/test/java/uk/ac/ebi/quickgo/rest/controller/ECOControllerIT.java
@@ -2,205 +2,37 @@ package uk.ac.ebi.quickgo.rest.controller;
 
 import uk.ac.ebi.quickgo.document.ontology.OntologyDocMocker;
 import uk.ac.ebi.quickgo.document.ontology.OntologyDocument;
-import uk.ac.ebi.quickgo.repo.TemporarySolrDataStore;
-import uk.ac.ebi.quickgo.repo.ontology.OntologyRepository;
-import uk.ac.ebi.quickgo.rest.QuickGOREST;
-
-import org.junit.Before;
-import org.junit.ClassRule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
- * Tests the {@link ECOController} class using an embedded server.
+ * Tests the {@link ECOController} class. All tests for ECO
+ * are covered by the tests in the parent class, {@link OBOControllerIT}.
  *
  * Created 24/11/15
  * @author Edd
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = {QuickGOREST.class})
-@WebAppConfiguration
-public class ECOControllerIT {
-    // temporary data store for solr's data, which is automatically cleaned on exit
-    @ClassRule
-    public static final TemporarySolrDataStore solrDataStore = new TemporarySolrDataStore();
+public class ECOControllerIT extends OBOControllerIT {
 
     private static final String RESOURCE_URL = "/QuickGO/services/eco";
+    private static final String ECO_0000001 = "ECO:0000001";
 
-    @Autowired
-    private WebApplicationContext webApplicationContext;
-
-    @Autowired
-    private OntologyRepository ontologyRepository;
-
-    private MockMvc mockMvc;
-
-    @Before
-    public void setup() {
-        ontologyRepository.deleteAll();
-
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .build();
+    @Override
+    protected OntologyDocument createBasicDoc() {
+        return OntologyDocMocker.createECODoc(ECO_0000001, "eco doc name");
     }
 
-    @Test
-    public void canRetrieveCoreById() throws Exception {
-        saveTerm("ECO:0000001");
-
-        ResultActions response = mockMvc.perform(get(RESOURCE_URL + "/ECO:0000001"));
-
-        expectCoreFields(response, "ECO:0000001")
-                .andExpect(jsonPath("$.history").doesNotExist())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk());
+    @Override
+    protected String getResourceURL() {
+        return RESOURCE_URL;
     }
 
-    @Test
-    public void canRetrieveCompleteById() throws Exception {
-        saveTerm("ECO:0000001");
-
-        ResultActions response = mockMvc.perform(get(RESOURCE_URL + "/ECO:0000001/complete"));
-
-        expectCompleteFields(response, "ECO:0000001")
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk());
+    @Override
+    protected String idMissingInRepository() {
+        return "ECO:0000002";
     }
 
-    @Test
-    public void canRetrieveHistoryById() throws Exception {
-        saveTerm("ECO:0000001");
-
-        ResultActions response = mockMvc.perform(get(RESOURCE_URL + "/ECO:0000001/history"));
-
-        expectBasicFields(response, "ECO:0000001")
-                .andExpect(jsonPath("$.history").isArray())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk());
+    @Override
+    protected String invalidId() {
+        return "ECO;0000001";
     }
 
-    @Test
-    public void canRetrieveXRefsById() throws Exception {
-        saveTerm("ECO:0000001");
-
-        ResultActions response = mockMvc.perform(get(RESOURCE_URL + "/ECO:0000001/xrefs"));
-
-        expectBasicFields(response, "ECO:0000001")
-                .andExpect(jsonPath("$.xRefs").isArray())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk());
-    }
-
-    /**
-     * Shows taxon constraints and blacklist for term
-     * @throws Exception
-     */
-    @Test
-    public void canRetrieveTaxonConstraintsById() throws Exception {
-        saveTerm("ECO:0000001");
-
-        ResultActions response = mockMvc.perform(get(RESOURCE_URL + "/ECO:0000001/constraints"));
-
-        expectBasicFields(response, "ECO:0000001")
-                .andExpect(jsonPath("$.taxonConstraints").isArray())
-                .andExpect(jsonPath("$.blacklist").isArray())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void canRetrieveAnnotationGuideLinesById() throws Exception {
-        saveTerm("ECO:0000001");
-
-        ResultActions response = mockMvc.perform(get(RESOURCE_URL + "/ECO:0000001/guidelines"));
-
-        expectBasicFields(response, "ECO:0000001")
-                .andExpect(jsonPath("$.annotationGuidelines").isArray())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void canRetrieveXORelsById() throws Exception {
-        saveTerm("ECO:0000001");
-
-        ResultActions response = mockMvc.perform(get(RESOURCE_URL + "/ECO:0000001/xontologyrelations"));
-
-        expectBasicFields(response, "ECO:0000001")
-                .andExpect(jsonPath("$.xRelations").isArray())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void finds404IfIdDoesNotExist() throws Exception {
-        saveTerm("ECO:0000001");
-
-        mockMvc.perform(get(RESOURCE_URL + "/ECO:0000002"))
-                .andExpect(status().isNotFound());
-    }
-
-    @Test
-    public void finds400IfIdIsEmpty() throws Exception {
-        mockMvc.perform(get(RESOURCE_URL + "/"))
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    public void finds400OnInvalidGOId() throws Exception {
-        saveTerm("ECO:0000001");
-
-        mockMvc.perform(get(RESOURCE_URL + "/ECO;0000002"))
-                .andExpect(status().isBadRequest());
-    }
-
-    private void saveTerm(String ecoId) {
-        OntologyDocument ecoTerm = OntologyDocMocker.createECODoc(ecoId, "eco doc name");
-        ontologyRepository.save(ecoTerm);
-    }
-
-    private ResultActions expectCompleteFields(ResultActions result, String id) throws Exception {
-        return expectCoreFields(result, id)
-                .andExpect(jsonPath("$.children").exists())
-                .andExpect(jsonPath("$.secondaryIds").exists())
-                .andExpect(jsonPath("$.subsets").exists())
-                .andExpect(jsonPath("$.history").exists())
-                .andExpect(jsonPath("$.xRefs").exists())
-                .andExpect(jsonPath("$.xRelations").exists())
-                .andExpect(jsonPath("$.annotationGuidelines").exists())
-                .andExpect(jsonPath("$.taxonConstraints").exists())
-                .andExpect(jsonPath("$.blacklist").exists())
-                .andExpect(jsonPath("$.consider").exists())
-                .andExpect(jsonPath("$.replacedBy").exists());
-    }
-
-    private ResultActions expectCoreFields(ResultActions result, String id) throws Exception {
-        return expectBasicFields(result, id)
-                .andExpect(jsonPath("$.synonyms").exists())
-                .andExpect(jsonPath("$.ancestors").exists());
-    }
-
-    private ResultActions expectBasicFields(ResultActions result, String id) throws Exception {
-        return result
-                .andDo(print())
-                .andExpect(jsonPath("$.id").value(id))
-                .andExpect(jsonPath("$.name").exists())
-                .andExpect(jsonPath("$.isObsolete").exists())
-                .andExpect(jsonPath("$.comment").exists())
-                .andExpect(jsonPath("$.definition").exists());
-    }
 }
--- rest/src/test/java/uk/ac/ebi/quickgo/rest/controller/GOControllerIT.java
+++ rest/src/test/java/uk/ac/ebi/quickgo/rest/controller/GOControllerIT.java
@@ -1,218 +1,52 @@
 package uk.ac.ebi.quickgo.rest.controller;
 
+import org.springframework.test.web.servlet.ResultActions;
 import uk.ac.ebi.quickgo.document.ontology.OntologyDocMocker;
 import uk.ac.ebi.quickgo.document.ontology.OntologyDocument;
-import uk.ac.ebi.quickgo.repo.TemporarySolrDataStore;
-import uk.ac.ebi.quickgo.repo.ontology.OntologyRepository;
-import uk.ac.ebi.quickgo.rest.QuickGOREST;
 
-import org.junit.Before;
-import org.junit.ClassRule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
- * Tests the {@link GOController} class using an embedded server.
+ * Tests the {@link GOController} class. All tests for GO
+ * are covered by the tests in the parent class, {@link OBOControllerIT}.
  *
  * Created 16/11/15
  * @author Edd
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = {QuickGOREST.class})
-@WebAppConfiguration
-public class GOControllerIT {
-    // temporary data store for solr's data, which is automatically cleaned on exit
-    @ClassRule
-    public static final TemporarySolrDataStore solrDataStore = new TemporarySolrDataStore();
+public class GOControllerIT extends OBOControllerIT {
 
     private static final String RESOURCE_URL = "/QuickGO/services/go";
+    private static final String GO_0000001 = "GO:0000001";
 
-    @Autowired
-    private WebApplicationContext webApplicationContext;
-
-    @Autowired
-    private OntologyRepository ontologyRepository;
-
-    private MockMvc mockMvc;
-
-    @Before
-    public void setup() {
-        ontologyRepository.deleteAll();
-
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .build();
-    }
-
-    @Test
-    public void canRetrieveCoreById() throws Exception {
-        saveTerm("GO:0000001");
-
-        mockMvc.perform(get(RESOURCE_URL + "/GO:0000001"))
-                .andDo(print())
-                .andExpect(
-                        content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(
-                        jsonPath("$.id").value("GO:0000001")
-                )
-                .andExpect(
-                        jsonPath("$.history").doesNotExist()
-                )
-                .andExpect(
-                        jsonPath("$.aspect").value("Biological Process")
-                )
-                .andExpect(
-                        jsonPath("$.usage").value("Unrestricted")
-                )
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void canRetrieveCompleteById() throws Exception {
-        saveTerm("GO:0000001");
-
-        mockMvc.perform(get(RESOURCE_URL + "/GO:0000001/complete"))
-                .andDo(print())
-                .andExpect(
-                        content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(
-                        jsonPath("$.id").value("GO:0000001")
-                )
-                .andExpect(
-                        jsonPath("$.history").isArray()
-                )
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void canRetrieveHistoryById() throws Exception {
-        saveTerm("GO:0000001");
-
-        mockMvc.perform(get(RESOURCE_URL + "/GO:0000001/history"))
-                .andDo(print())
-                .andExpect(
-                        content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(
-                        jsonPath("$.id").value("GO:0000001")
-                )
-                .andExpect(
-                        jsonPath("$.history").isArray()
-                )
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void canRetrieveXRefsById() throws Exception {
-        saveTerm("GO:0000001");
-
-        mockMvc.perform(get(RESOURCE_URL + "/GO:0000001/xrefs"))
-                .andDo(print())
-                .andExpect(
-                        content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(
-                        jsonPath("$.id").value("GO:0000001")
-                )
-                .andExpect(
-                        jsonPath("$.xRefs").isArray()
-                )
-                .andExpect(status().isOk());
-    }
-
-    /**
-     * Shows taxon constraints and blacklist for term
-     * @throws Exception
+    /*
+     * GO produces two more attributes in its response (aspect and usage), when compared
+     * to the standard OBO response.
      */
-    @Test
-    public void canRetrieveTaxonConstraintsById() throws Exception {
-        saveTerm("GO:0000001");
-
-        mockMvc.perform(get(RESOURCE_URL + "/GO:0000001/constraints"))
-                .andDo(print())
-                .andExpect(
-                        content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(
-                        jsonPath("$.id").value("GO:0000001")
-                )
-                .andExpect(
-                        jsonPath("$.taxonConstraints").isArray()
-                )
-                .andExpect(
-                        jsonPath("$.blacklist").isArray()
-                )
-                .andExpect(status().isOk());
+    @Override
+    protected ResultActions expectCoreFields(ResultActions result, String id) throws Exception {
+        return super
+                .expectCoreFields(result, id)
+                .andExpect(jsonPath("$.aspect").value("Biological Process"))
+                .andExpect(jsonPath("$.usage").value("Unrestricted"));
     }
 
-    @Test
-    public void canRetrieveAnnotationGuideLinesById() throws Exception {
-        saveTerm("GO:0000001");
-
-        mockMvc.perform(get(RESOURCE_URL + "/GO:0000001/guidelines"))
-                .andDo(print())
-                .andExpect(
-                        content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(
-                        jsonPath("$.id").value("GO:0000001")
-                )
-                .andExpect(
-                        jsonPath("$.annotationGuidelines").isArray()
-                )
-                .andExpect(status().isOk());
+    @Override
+    protected OntologyDocument createBasicDoc() {
+        return OntologyDocMocker.createGODoc(GO_0000001, "go name");
     }
 
-    @Test
-    public void canRetrieveXORelsById() throws Exception {
-        saveTerm("GO:0000001");
-
-        mockMvc.perform(get(RESOURCE_URL + "/GO:0000001/xontologyrelations"))
-                .andDo(print())
-                .andExpect(
-                        content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(
-                        jsonPath("$.id").value("GO:0000001")
-                )
-                .andExpect(
-                        jsonPath("$.xRelations").isArray()
-                )
-                .andExpect(status().isOk());
+    @Override
+    protected String idMissingInRepository() {
+        return "GO:0000002";
     }
 
-    @Test
-    public void finds404IfIdDoesNotExist() throws Exception {
-        saveTerm("GO:0000001");
-
-        mockMvc.perform(get(RESOURCE_URL + "/GO:0000002"))
-                .andExpect(status().isNotFound());
+    @Override
+    protected String invalidId() {
+        return "GO;0000001";
     }
 
-    @Test
-    public void finds400IfIdIsEmpty() throws Exception {
-        mockMvc.perform(get(RESOURCE_URL + "/"))
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    public void finds400OnInvalidGOId() throws Exception {
-        saveTerm("GO:0000001");
-
-        mockMvc.perform(get(RESOURCE_URL + "/GO;0000002"))
-                .andExpect(status().isBadRequest());
-    }
-
-    private void saveTerm(String goId) {
-        OntologyDocument goTerm = OntologyDocMocker.createGODoc(goId, "go name");
-        ontologyRepository.save(goTerm);
+    @Override
+    protected String getResourceURL() {
+        return RESOURCE_URL;
     }
 }
