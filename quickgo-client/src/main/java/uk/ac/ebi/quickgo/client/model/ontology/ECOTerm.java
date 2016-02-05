82852811685cf92c7bac3295ed8dc28e4cfdab09
rantunes <rantunes@ebi.ac.uk>
2016-02-05 14:40:54 +0000

    Create an ontology model to be used by the quickgo client.

---
+++ quickgo-client/src/main/java/uk/ac/ebi/quickgo/client/model/ontology/ECOTerm.java
@@ -0,0 +1,8 @@
+package uk.ac.ebi.quickgo.client.model.ontology;
+
+/**
+ * Represents the data contained within an ECO term.
+ *
+ * @author Ricardo Antunes
+ */
+public class ECOTerm extends OntologyTerm {}
