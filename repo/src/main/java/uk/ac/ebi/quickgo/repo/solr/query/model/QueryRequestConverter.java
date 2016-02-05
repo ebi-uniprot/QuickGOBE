f1493334f2aae66dd7afe95de720617ca5406b48
rantunes <rantunes@ebi.ac.uk>
2016-02-05 14:50:35 +0000

    Remove old unused modules from the repo.

--- repo/src/main/java/uk/ac/ebi/quickgo/repo/solr/query/model/QueryRequestConverter.java
+++
@@ -1,8 +0,0 @@
-package uk.ac.ebi.quickgo.repo.solr.query.model;
-
-/**
- * Converts a {@link QueryRequest} into a query that an implementing data source can execute.
- */
-public interface QueryRequestConverter<T> {
-    T convert(QueryRequest request);
-}
