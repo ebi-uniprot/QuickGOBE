e63a58711db7f8b4d82dd9913449246c34eeeff3
rantunes <rantunes@ebi.ac.uk>
2016-02-09 17:00:51 +0000

    Minor formatting issues.

--- common/src/main/java/uk/ac/ebi/quickgo/common/search/solr/SolrRetrievalConfigHelper.java
+++ common/src/main/java/uk/ac/ebi/quickgo/common/search/solr/SolrRetrievalConfigHelper.java
@@ -9,21 +9,26 @@ import org.slf4j.LoggerFactory;
  * Created 08/02/16
  * @author Edd
  */
-public class SolrRetrievalConfigHelper {
-    private static final Logger LOGGER = LoggerFactory.getLogger(SolrRetrievalConfigHelper.class);
-
+public final class SolrRetrievalConfigHelper {
     private static final String COMMA = ",";
-    public static final String DEFAULT_HIGHLIGHT_DELIMS = "<em>,</em>";
+
+    public static final String DEFAULT_HIGHLIGHT_DELIMS = "<em>" + COMMA + "</em>";
     public static final int HIGHLIGHT_START_DELIM_INDEX = 0;
     public static final int HIGHLIGHT_END_DELIM_INDEX = 1;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SolrRetrievalConfigHelper.class);
+
+    private SolrRetrievalConfigHelper() {}
+
     public static String[] convertHighlightDelims(String highlightDelims, String delim) {
         String[] delims = highlightDelims.split(delim);
+
         if (delims.length != 2) {
             LOGGER.warn("Invalid highlighting delimiters specified: " + highlightDelims + ". Using defaults: " +
                     DEFAULT_HIGHLIGHT_DELIMS);
             delims = DEFAULT_HIGHLIGHT_DELIMS.split(COMMA);
         }
+
         return delims;
     }
 }
