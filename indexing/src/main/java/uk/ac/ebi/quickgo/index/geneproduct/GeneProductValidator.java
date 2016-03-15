20fd2d82c9695def2c7458d9d74dd15770631865
rantunes <rantunes@ebi.ac.uk>
2016-03-15 14:35:03 +0000

    Add more taxon name validation logic.

--- indexing/src/main/java/uk/ac/ebi/quickgo/index/geneproduct/GeneProductValidator.java
+++ indexing/src/main/java/uk/ac/ebi/quickgo/index/geneproduct/GeneProductValidator.java
@@ -47,10 +47,11 @@ public class GeneProductValidator implements Validator<GeneProduct> {
         Map<String, String> properties =
                 convertToMap(geneProduct.properties, interValueDelimiter, intraValueDelimiter);
 
-        checkTaxonName(geneProduct.taxonId, properties.get(TAXON_NAME_KEY));
+        checkIsNullOrEmpty(properties.get(TAXON_NAME_KEY), TAXON_NAME_KEY);
+
         checkBooleanValue(properties.get(COMPLETE_PROTEOME_KEY), COMPLETE_PROTEOME_KEY);
         checkBooleanValue(properties.get(IS_ANNOTATED_KEY), IS_ANNOTATED_KEY);
-        checkBooleanValue(properties.get(IS_ISOFORM), IS_ISOFORM);
+        checkBooleanValue(properties.get(IS_ISOFORM_KEY), IS_ISOFORM_KEY);
     }
 
     /**
