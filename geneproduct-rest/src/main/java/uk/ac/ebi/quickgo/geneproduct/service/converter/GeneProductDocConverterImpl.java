6bcaa3583d0366d31da60da585e769010d681916
twardell <twardell@ebi.ac.uk>
2016-04-01 14:56:39 +0100

    New.

---
+++ geneproduct-rest/src/main/java/uk/ac/ebi/quickgo/geneproduct/service/converter/GeneProductDocConverterImpl.java
@@ -0,0 +1,34 @@
+package uk.ac.ebi.quickgo.geneproduct.service.converter;
+
+import uk.ac.ebi.quickgo.geneproduct.common.document.GeneProductDocument;
+import uk.ac.ebi.quickgo.geneproduct.model.GeneProduct;
+
+/**
+ * Turn a GeneProduct document from Solr into a model to be used by the RESTful service.
+ *
+ * @Author Tony Wardell
+ * Date: 01/04/2016
+ * Time: 14:31
+ * Created with IntelliJ IDEA.
+ */
+public class GeneProductDocConverterImpl implements GeneProductDocConverter {
+
+
+	@Override
+	public GeneProduct convert(GeneProductDocument geneProductDocument) {
+
+		GeneProduct geneProduct			= new GeneProduct();
+		geneProduct.database 			= geneProductDocument.database;
+		geneProduct.databaseSubset 		= geneProductDocument.databaseSubsets;
+		geneProduct.identifier			= geneProductDocument.id;
+		geneProduct.isIsoform			= geneProductDocument.isIsoform;
+		geneProduct.name				= geneProductDocument.name;
+		geneProduct.referenceProteome 	= geneProductDocument.referenceProteome;
+		geneProduct.synonyms 			=  geneProductDocument.synonyms;
+		geneProduct.symbol				= geneProductDocument.symbol;
+		geneProduct.taxonomy			= new GeneProduct.Taxonomy(geneProductDocument.taxonId, geneProductDocument.taxonName);
+		geneProduct.type				= geneProductDocument.type;
+
+		return geneProduct;
+	}
+}
