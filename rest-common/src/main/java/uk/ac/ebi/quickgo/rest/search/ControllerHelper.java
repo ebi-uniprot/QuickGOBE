7bcec40dc00ca4c52dee2948e2bd48d1d4021513
twardell <twardell@ebi.ac.uk>
2016-04-01 10:57:24 +0100

    Devolve  parsing of the csv list to common code.

--- rest-common/src/main/java/uk/ac/ebi/quickgo/rest/search/ControllerHelper.java
+++ rest-common/src/main/java/uk/ac/ebi/quickgo/rest/search/ControllerHelper.java
@@ -1,20 +1,14 @@
 package uk.ac.ebi.quickgo.rest.search;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-
-import static com.google.common.base.Strings.isNullOrEmpty;
 
 /**
  * @Author Tony Wardell
- * Date: 29/03/2016
- * Time: 10:39
+ * Date: 01/04/2016
+ * Time: 10:13
  * Created with IntelliJ IDEA.
  */
-public class ControllerHelper {
-
-	private static final String COMMA = ",";
+public interface ControllerHelper {
 
 	/**
 	 * Creates a list of items from a scalar representation of a list, in CSV format. If the
@@ -23,12 +17,5 @@ public class ControllerHelper {
 	 * @param csv a CSV list of items
 	 * @return a list of values originally comprising the CSV input String
 	 */
-	public List<String> csvToList(String csv) {
-		if (!isNullOrEmpty(csv)) {
-			return Arrays.asList(csv.split(COMMA));
-		} else {
-			return Collections.emptyList();
-		}
-	}
-
+	public List<String> csvToList(String csv);
 }
---
+++ rest-common/src/main/java/uk/ac/ebi/quickgo/rest/search/ControllerHelperImpl.java
@@ -0,0 +1,30 @@
+package uk.ac.ebi.quickgo.rest.search;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static com.google.common.base.Strings.isNullOrEmpty;
+
+/**
+ * @Author Tony Wardell
+ * Date: 29/03/2016
+ * Time: 10:39
+ * Created with IntelliJ IDEA.
+ *
+ * Some logic is common to all REST services
+ */
+public class ControllerHelperImpl implements ControllerHelper{
+
+	private static final String COMMA = ",";
+
+
+	public List<String> csvToList(String csv) {
+		if (!isNullOrEmpty(csv)) {
+			return Arrays.asList(csv.split(COMMA));
+		} else {
+			return Collections.emptyList();
+		}
+	}
+
+}
