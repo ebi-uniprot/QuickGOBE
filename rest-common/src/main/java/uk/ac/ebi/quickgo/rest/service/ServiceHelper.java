586dc15e510c63dfa23291d535daa1c4caf42256
twardell <twardell@ebi.ac.uk>
2016-04-01 13:59:22 +0100

    Add class to hold code common to Service level.

--- rest-common/src/main/java/uk/ac/ebi/quickgo/rest/service/ServiceHelper.java
+++ rest-common/src/main/java/uk/ac/ebi/quickgo/rest/service/ServiceHelper.java
@@ -1,28 +1,16 @@
 package uk.ac.ebi.quickgo.rest.service;
 
-import com.google.common.base.Preconditions;
-import uk.ac.ebi.quickgo.rest.search.QueryStringSanitizer;
-
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @Author Tony Wardell
- * Date: 29/03/2016
- * Time: 11:01
+ * Date: 01/04/2016
+ * Time: 13:32
+ *
+ * There is some code that is common to the Service layer of the RESTful services
+ *
  * Created with IntelliJ IDEA.
  */
-public class ServiceHelper {
-
-	private QueryStringSanitizer queryStringSanitizer;
-
-	public ServiceHelper(QueryStringSanitizer queryStringSanitizer) {
-		this.queryStringSanitizer = queryStringSanitizer;
-	}
-
-	public List<String> buildIdList(List<String> ids) {
-		Preconditions.checkArgument(ids != null, "List of IDs cannot be null");
-
-		return ids.stream().map(queryStringSanitizer::sanitize).collect(Collectors.toList());
-	}
+public interface ServiceHelper {
+	List<String> buildIdList(List<String> ids);
 }
