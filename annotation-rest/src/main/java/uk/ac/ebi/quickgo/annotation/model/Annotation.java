4c337c0ac58bfc61cb2e4dd46afc86b80e87d216
twardell <twardell@ebi.ac.uk>
2016-04-28 14:13:40 +0100

    Populate model.

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/model/Annotation.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/model/Annotation.java
@@ -1,5 +1,7 @@
 package uk.ac.ebi.quickgo.annotation.model;
 
+import java.util.List;
+
 /**
  * @author Tony Wardell
  *         Date: 21/04/2016
@@ -7,4 +9,26 @@ package uk.ac.ebi.quickgo.annotation.model;
  *         Created with IntelliJ IDEA.
  */
 public class Annotation {
+
+    public String id;
+
+    public String geneProductId;
+
+    public String qualifier;
+
+    public String goId;
+
+    public String goEvidence;
+
+    public String ecoId;
+
+    public String reference;
+
+    public List<String> withFrom;
+
+    public String taxonId;
+
+    public String assignedBy;
+
+    public String extension;
 }
