914a46ef096cbcb86a976630c4e32ea7e9d0970f
twardell <twardell@ebi.ac.uk>
2016-04-29 17:33:23 +0100

    New.

---
+++ annotation-rest/src/test/java/uk/ac/ebi/quickgo/annotation/search/AnnotationSolrQueryConverterTest.java
@@ -0,0 +1,39 @@
+package uk.ac.ebi.quickgo.annotation.search;
+
+import uk.ac.ebi.quickgo.rest.search.query.QueryRequest;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+/**
+ * @author Tony Wardell
+ * Date: 29/04/2016
+ * Time: 17:24
+ * Created with IntelliJ IDEA.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class AnnotationSolrQueryConverterTest {
+
+    @Mock
+    private SolrQuery mockSolrQuery;
+
+    @Mock
+    private QueryRequest mockRequest;
+
+    @Test
+    public void justReturns(){
+        AnnotationSolrQueryConverter converter = new AnnotationSolrQueryConverter("/handler");
+        verifyNoMoreInteractions(mockRequest);
+        verifyNoMoreInteractions(mockSolrQuery);
+        converter.assignQuery(mockRequest, mockSolrQuery);
+
+
+    }
+}
