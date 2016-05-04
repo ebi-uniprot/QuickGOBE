60d2eac72fd64f1e98587250ef33f1b898293c89
Edward Turner <eddturner@ebi.ac.uk>
2016-05-04 14:11:26 +0100

    unit test the geneproducttype

---
+++ geneproduct-common/src/test/java/uk/ac/ebi/quickgo/geneproduct/common/document/GeneProductTypeTest.java
@@ -0,0 +1,35 @@
+package uk.ac.ebi.quickgo.geneproduct.common.document;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.util.MatcherAssertionErrors.assertThat;
+
+/**
+ * Ensures interaction of the {@link GeneProductType} enumeration works as expected.
+ *
+ * Created 04/05/16
+ * @author Edd
+ */
+public class GeneProductTypeTest {
+    @Test
+    public void typeOfSucceedsForAllValidUpperCaseGeneProductTypes() {
+        for (GeneProductType geneProductType : GeneProductType.values()) {
+            GeneProductType retrievedGeneProductType = GeneProductType.typeOf(geneProductType.getName().toUpperCase());
+            assertThat(geneProductType, is(retrievedGeneProductType));
+        }
+    }
+
+    @Test
+    public void typeOfSucceedsForAllValidLowerCaseGeneProductTypes() {
+        for (GeneProductType geneProductType : GeneProductType.values()) {
+            GeneProductType retrievedGeneProductType = GeneProductType.typeOf(geneProductType.getName().toLowerCase());
+            assertThat(geneProductType, is(retrievedGeneProductType));
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void typeOfProducesIllegalArgumentExceptionForUnknownGeneProductType() {
+        GeneProductType.typeOf("this doesn't exist");
+    }
+}
