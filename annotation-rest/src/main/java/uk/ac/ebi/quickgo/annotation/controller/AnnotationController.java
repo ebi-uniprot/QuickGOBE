f9183b4288b6e2c467ff637e3843bfcbd5a062bb
twardell <twardell@ebi.ac.uk>
2016-05-10 10:07:43 +0100

    Improve javadoc, formatting and checking of preconditions.

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/controller/AnnotationController.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/controller/AnnotationController.java
@@ -1,5 +1,8 @@
 package uk.ac.ebi.quickgo.annotation.controller;
 
+import com.google.common.base.Preconditions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,27 +19,42 @@ import uk.ac.ebi.quickgo.rest.search.results.QueryResult;
 
 import java.util.Objects;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
 import static uk.ac.ebi.quickgo.rest.search.SearchDispatcher.search;
 
 /**
- * Provides RESTful endpoints for retrieving Annotations
+ * Provides RESTful endpoints for retrieving Gene Ontology (GO) Annotations to gene products.
+ *
+ * Gene Ontology: the framework for the model of biology. The GO defines concepts/classes used
+ * to describe gene function, and relationships between these concepts.
+ *
+ * GO annotations: the model of biology. Annotations are statements describing the functions of specific genes,
+ * using concepts in the Gene Ontology. The simplest and most common annotation links one gene to one function,
+ * e.g. FZD4 + Wnt signaling pathway. Each statement is based on a specified piece of evidence
+ *
+ * Sets of annotations can be tailored for each user by powerful filtering capabilities
+ * Annotations will be downloadable in a variety of formats.
  *
  * taxon=1234,343434
  *
  * gp=A0A000,A0A001
  * gpSet=BHF-UCL,Exosome
- * gpType=protein,rna,complexe
+ * gpType=protein,rna,complex
  *
  * goTerm=GO:0016021,GO:0016022
- * goTermSet=goslim_chembl,goSlimGeneric
+ * goTermSet=goslim_chembl, goSlimGeneric .. and others.
  *
  * ..the following are only applicable if goTerm ids or sets have been selected
  * goTermUse=ancestor or goTermUse=slim  or goTermUse=exact
+ *
  * goTermRelationship=I or goTermRelationship=IPO or goTermRelationship=IPOR
  *
  * aspect=F,P,C
  *
- * evidence=ECO:0000352,ECO0000269            ?allow go evidence codes??
+ * evidence=ECO:0000352,ECO0000269
+ *
+ * goEvidence=IEA etc
  *
  * ..the following is only applicable if any evidence code has been selected
  * evidenceRelationship=ancestor or evidenceRelationship=exact
@@ -58,14 +76,19 @@ import static uk.ac.ebi.quickgo.rest.search.SearchDispatcher.search;
 @RequestMapping(value = "/QuickGO/services/annotation")
 public class AnnotationController {
 
+	Logger logger = LoggerFactory.getLogger(AnnotationController.class);
+
+
 	private final SearchService<Annotation> annotationSearchService;
 	private final FilterOnlySearchQueryTemplate requestTemplate;
 
 	@Autowired
 	public AnnotationController(SearchService<Annotation> annotationSearchService,
 			SearchServiceConfig.AnnotationCompositeRetrievalConfig annotationRetrievalConfig ) {
-		Objects.requireNonNull(annotationSearchService, "The SearchService<Annotation> instance passed to the constructor of " +
+		checkNotNull(annotationSearchService, "The SearchService<Annotation> instance passed to the constructor of " +
 				"AnnotationController should not be null.");
+		checkNotNull(annotationRetrievalConfig, "The SearchServiceConfig.AnnotationCompositeRetrievalConfig" +
+				" instance passed to the constructor of AnnotationController should not be null.");
 		this.annotationSearchService = annotationSearchService;
 		this.requestTemplate = new FilterOnlySearchQueryTemplate(annotationRetrievalConfig.getSearchReturnedFields());
 	}
@@ -77,10 +100,9 @@ public class AnnotationController {
 	@RequestMapping(value = "/search", method = {RequestMethod.GET}, produces = {MediaType.APPLICATION_JSON_VALUE})
 	public ResponseEntity<QueryResult<Annotation>> annotationLookup(AnnotationFilter filter, BindingResult bindingResult) {
 
-		System.out.println(bindingResult.toString());
-
+		checkState(!bindingResult.hasErrors(), "The binding of the request parameters to " +
+				"AnnotationFilter %s has errors, see binding result %s", filter, bindingResult);
 		filter.validation();
-
 		FilterOnlySearchQueryTemplate.Builder requestBuilder = requestTemplate.newBuilder()
 				.setFilterProvider(filter);
 		return search(requestBuilder.build(), annotationSearchService);
