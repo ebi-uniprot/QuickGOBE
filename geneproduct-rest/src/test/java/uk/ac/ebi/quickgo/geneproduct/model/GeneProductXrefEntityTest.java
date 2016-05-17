65746adcf35ff4db3b2c16ad42a730d889dcf826
twardell <twardell@ebi.ac.uk>
2016-05-17 17:00:34 +0100

    Test preconditions defending against nulls in constructor for parameters as required.

--- geneproduct-rest/src/test/java/uk/ac/ebi/quickgo/geneproduct/model/GeneProductXrefEntityTest.java
+++ geneproduct-rest/src/test/java/uk/ac/ebi/quickgo/geneproduct/model/GeneProductXrefEntityTest.java
@@ -1,7 +1,9 @@
 package uk.ac.ebi.quickgo.geneproduct.model;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -14,18 +16,22 @@ import static org.hamcrest.core.Is.is;
  */
 public class GeneProductXrefEntityTest {
 
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
+
 	private String database = "UniProtKB";
 	private String entityType = "PR:000000001";
 	private String entityTypeName = "protein";
-	private String localIdSyntax = "([OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z]([0-9][A-Z][A-Z0-9]{2}){1,2}[0-9])((-[0-9]+)|:PRO_[0-9]{10}|:VAR_[0-9]{6}){0,1}";
-	private String urlSyntax = " http://www.uniprot.org/uniprot/[example_id]";
+	private String idValidationPattern = "([OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z]([0-9][A-Z][A-Z0-9]{2}){1,2}[0-9])((-[0-9]+)|:PRO_[0-9]{10}|:VAR_[0-9]{6}){0,1}";
+	private String dbURL = " http://www.uniprot.org/uniprot/[example_id]";
 
 	GeneProductDbXRefIDFormat geneProductXrefEntity;
 
 	@Before
 	public void setup(){
 		geneProductXrefEntity = new GeneProductDbXRefIDFormat(database, entityType, entityTypeName,
-				localIdSyntax, urlSyntax);
+				idValidationPattern, dbURL);
 	}
 
 	@Test
@@ -38,5 +44,31 @@ public class GeneProductXrefEntityTest {
 		assertThat(geneProductXrefEntity.matches("99999"), is(false));
 	}
 
+	@Test
+	public void exceptionThrownIfDatabaseIsNull(){
+		thrown.expect(NullPointerException.class);
+		new GeneProductDbXRefIDFormat(null, entityType, entityTypeName, idValidationPattern, dbURL);
+	}
 
+	@Test
+	public void exceptionThrownIfEntityTypeIsNull(){
+		thrown.expect(NullPointerException.class);
+		new GeneProductDbXRefIDFormat(database, null, entityTypeName, idValidationPattern, dbURL);
+	}
+
+	@Test
+	public void exceptionThrownIfIdValidationPatternIsNull(){
+		thrown.expect(NullPointerException.class);
+		new GeneProductDbXRefIDFormat(database, entityType, entityTypeName, null, dbURL);
+	}
+
+	@Test
+	public void exceptionNotThrownIfEntityTypeNameIsNull(){
+		new GeneProductDbXRefIDFormat(database, entityType, null, idValidationPattern, dbURL);
+	}
+
+	@Test
+	public void exceptionNotThrownIfDbURLIsNull(){
+		new GeneProductDbXRefIDFormat(database, entityType, entityTypeName, idValidationPattern, null);
+	}
 }
