1013c0359f8443ef141294a271f9c1752df8b6c3
Edward Turner <eddturner@ebi.ac.uk>
2016-05-20 18:15:59 +0100

    contract for OntologyGraphTraversal defined and javadocced

--- ontology-rest/src/main/java/uk/ac/ebi/quickgo/ontology/traversal/OntologyGraphTraversal.java
+++ ontology-rest/src/main/java/uk/ac/ebi/quickgo/ontology/traversal/OntologyGraphTraversal.java
@@ -1,5 +1,8 @@
 package uk.ac.ebi.quickgo.ontology.traversal;
 
+import java.util.List;
+import java.util.Set;
+
 /**
  * This class defines a contract for typical ontology graph traversal operations.
  *
@@ -7,4 +10,37 @@ package uk.ac.ebi.quickgo.ontology.traversal;
  * @author Edd
  */
 public interface OntologyGraphTraversal {
+    /**
+     * Find the list of paths between two vertices in a graph, navigable via
+     * a specified set of relations.
+     *
+     * @param v1 the start vertex from which returned paths must start
+     * @param v2 the end vertex from which returned paths end
+     * @param relations a varargs value containing the relationships over which paths can only travel.
+     *                  By omitting a {@code relation} value, all paths will be returned.
+     * @return a list of paths, ordered shorted first, from {@code v1} to {@code v2} via {@code relations}
+     */
+    List<OntologyGraph.LabelledEdge> paths(String v1, String v2, OntologyRelation... relations);
+
+    /**
+     * Find the set of ancestor vertices reachable from a base vertex, navigable via a specified
+     * set of relations.
+     *
+     * @param base the base vertex whose ancestors one is interested in
+     * @param relations a varargs value containing the relationships over which paths can only travel.
+     *                  By omitting a {@code relation} value, all paths will be returned.
+     * @return the set of ancestor vertices.
+     */
+    Set<String> ancestors(String base, OntologyRelation... relations);
+
+    /**
+     * Find the set of descendant vertices reachable from a top vertex, navigable via a specified
+     * set of relations.
+     *
+     * @param top the top-most vertex whose descendants one is interested in
+     * @param relations a varargs value containing the relationships over which paths can only travel.
+     *                  By omitting a {@code relation} value, all paths will be returned.
+     * @return the set of descendant vertices.
+     */
+    Set<String> descendants(String top, OntologyRelation... relations);
 }
