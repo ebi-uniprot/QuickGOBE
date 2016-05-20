a9fb45173a41e6ed94d281358a6d8106c97a20ff
rantunes <rantunes@ebi.ac.uk>
2016-05-20 09:25:28 +0100

    Fix broken TestUtil import.

--- rest-common/src/test/java/uk/ac/ebi/quickgo/rest/search/query/SolrQueryConverterTest.java
+++ rest-common/src/test/java/uk/ac/ebi/quickgo/rest/search/query/SolrQueryConverterTest.java
@@ -10,8 +10,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.arrayContaining;
 import static org.hamcrest.Matchers.arrayContainingInAnyOrder;
 import static org.hamcrest.Matchers.equalTo;
+import static uk.ac.ebi.quickgo.rest.TestUtil.asSet;
 import static uk.ac.ebi.quickgo.rest.search.query.SolrQueryConverter.CROSS_CORE_JOIN_SYNTAX;
-import static uk.ac.ebi.quickgo.rest.search.query.TestUtil.asSet;
 
 /**
  * Tests the implementations of the {@link SolrQueryConverter} implementation.
