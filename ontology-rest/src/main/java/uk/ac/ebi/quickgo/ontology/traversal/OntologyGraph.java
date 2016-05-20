d5f4ceeb66b9f8ad8968c2b0a69e5ea3bde72fdc
Edward Turner <eddturner@ebi.ac.uk>
2016-05-20 17:36:21 +0100

    OntologyGraph encapsulate a JGraphT graph instance. Using DirectedMultigraph because we want the possibility of directed edges, and for pairs of vertices to have multiple edges between them (in the case of multiple relationships existing between them)

--- ontology-rest/src/main/java/uk/ac/ebi/quickgo/ontology/traversal/OntologyGraph.java
+++ ontology-rest/src/main/java/uk/ac/ebi/quickgo/ontology/traversal/OntologyGraph.java
@@ -1,26 +1,80 @@
 package uk.ac.ebi.quickgo.ontology.traversal;
 
-import uk.ac.ebi.quickgo.ontology.traversal.read.OntologyRelationshipTuple;
+import uk.ac.ebi.quickgo.ontology.traversal.read.OntologyRelationship;
 
-import java.util.HashSet;
+import java.util.Collection;
 import java.util.Set;
+import org.jgrapht.DirectedGraph;
+import org.jgrapht.graph.ClassBasedEdgeFactory;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DirectedMultigraph;
 
 /**
+ * This class represents an ontology graph whose vertices are ontology terms,
+ * and edges are the relationships between terms.
+ *
  * Created 18/05/16
  * @author Edd
  */
 public class OntologyGraph {
-    private final Set<OntologyRelationshipTuple> tuples;
+    private final DirectedGraph<String, LabelledEdge> ontology;
 
     public OntologyGraph() {
-        tuples = new HashSet<>();
+        ontology =
+                new DirectedMultigraph<>(new ClassBasedEdgeFactory<>(LabelledEdge.class));
     }
 
-    public Set<OntologyRelationshipTuple> getTuples() {
-        return tuples;
+    public Set<LabelledEdge> getEdges() {
+        return ontology.edgeSet();
     }
 
-    public boolean removeTuple(OntologyRelationshipTuple tuple) {
-        return tuples.remove(tuple);
+    public Set<String> getVertices() {
+        return ontology.vertexSet();
+    }
+
+
+    public boolean addTuples(Collection<? extends OntologyRelationship> tuples) {
+        // populate graph with edges, whilst recording the vertices
+        tuples.stream().forEach(
+                oEdge -> {
+                    if (!ontology.containsVertex(oEdge.child)) {
+                        ontology.addVertex(oEdge.child);
+                    }
+                    if (!ontology.containsVertex(oEdge.parent)) {
+                        ontology.addVertex(oEdge.parent);
+                    }
+                    ontology.addEdge(
+                            oEdge.child,
+                            oEdge.parent,
+                            new LabelledEdge<>(oEdge.child, oEdge.parent,
+                                    OntologyRelations.getByShortName(oEdge.relationship)));
+                }
+        );
+
+        return true;
+    }
+
+    static class LabelledEdge<V> extends DefaultEdge {
+        private V v1;
+        private V v2;
+        private OntologyRelations relation;
+
+        LabelledEdge(V v1, V v2, OntologyRelations relation) {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.relation = relation;
+        }
+
+        public V getV1() {
+            return v1;
+        }
+
+        public V getV2() {
+            return v2;
+        }
+
+        public String toString() {
+            return relation.getShortName();
+        }
     }
 }
