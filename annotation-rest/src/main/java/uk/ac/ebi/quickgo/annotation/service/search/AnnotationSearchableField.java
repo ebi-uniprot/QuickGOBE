1dd34cc37760a4739b226a345ceee096d635b13c
rantunes <rantunes@ebi.ac.uk>
2016-05-25 16:09:25 +0100

    Create a class that is responsible for indicating when a field is searchable or not.

---
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/service/search/AnnotationSearchableField.java
@@ -0,0 +1,28 @@
+package uk.ac.ebi.quickgo.annotation.service.search;
+
+import uk.ac.ebi.quickgo.annotation.common.document.AnnotationFields;
+import uk.ac.ebi.quickgo.common.SearchableDocumentFields;
+import uk.ac.ebi.quickgo.rest.search.SearchableField;
+
+import java.util.stream.Stream;
+import org.springframework.stereotype.Component;
+
+/**
+ * Checks if a given field is searchable searchable.
+ *
+ * @author Ricardo Antunes
+ */
+@Component
+public class AnnotationSearchableField implements SearchableField, SearchableDocumentFields {
+    @Override public boolean isDocumentSearchable(String field) {
+        return AnnotationFields.Searchable.isSearchable(field);
+    }
+
+    @Override public Stream<String> searchableDocumentFields() {
+        return AnnotationFields.Searchable.searchableFields().stream();
+    }
+
+    @Override public boolean isSearchable(String field) {
+        return AnnotationFields.Searchable.isSearchable(field);
+    }
+}
