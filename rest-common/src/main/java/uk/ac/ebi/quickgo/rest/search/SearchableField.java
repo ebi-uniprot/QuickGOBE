34428e1030c1797b1ad89f5b702b2956c1da1537
rantunes <rantunes@ebi.ac.uk>
2016-05-25 09:46:49 +0100

    Update Javadoc to try and make the documentation more explicit.

--- rest-common/src/main/java/uk/ac/ebi/quickgo/rest/search/SearchableField.java
+++ rest-common/src/main/java/uk/ac/ebi/quickgo/rest/search/SearchableField.java
@@ -1,9 +1,15 @@
 package uk.ac.ebi.quickgo.rest.search;
 
 /**
- * Interface used on the Field enumerations. It checks whether a string is one of
- * the searchable fields of the enumeration.
+ * Interface that defines methods that check whether the field is searchable, within its runtime context.
+ *
+ * @author Edd Turner
  */
 public interface SearchableField {
+    /**
+     * Checks whether the provided {@param field} is a searchable field.
+     * @param field the field to check
+     * @return true if the provided field is searchable, false otherwise
+     */
     boolean isSearchable(String field);
 }
