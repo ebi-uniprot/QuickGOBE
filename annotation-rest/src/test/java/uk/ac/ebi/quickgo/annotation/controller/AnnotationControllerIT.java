0c96ffaefd6bbd7ab6514931472ca7648297d483
rantunes <rantunes@ebi.ac.uk>
2016-05-26 13:29:25 +0100

    General test refactor, and add taxon integration tests.

--- annotation-rest/src/test/java/uk/ac/ebi/quickgo/annotation/controller/AnnotationControllerIT.java
+++ annotation-rest/src/test/java/uk/ac/ebi/quickgo/annotation/controller/AnnotationControllerIT.java
@@ -28,7 +28,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -51,10 +51,15 @@ public class AnnotationControllerIT {
     // temporary data store for solr's data, which is automatically cleaned on exit
     @ClassRule
     public static final TemporarySolrDataStore solrDataStore = new TemporarySolrDataStore();
-    private static final String UNAVAILABLE_ASSIGNED_BY = "ZZZZZ";
+
+    private static final int NUMBER_OF_GENERIC_DOCS = 3;
+
     private static final String ASSIGNED_BY_PARAM = "assignedBy";
     private static final String PAGE_PARAM = "page";
     private static final String LIMIT_PARAM = "limit";
+    private static final String TAXON_ID_PARAM = "taxon";
+
+    private static final String UNAVAILABLE_ASSIGNED_BY = "ZZZZZ";
 
     private static final List<String> VALID_ASSIGNED_BY_PARMS = Arrays.asList("ASPGD", "ASPGD,Agbase", "ASPGD_,Agbase",
             "ASPGD,Agbase_", "ASPGD,Agbase", "BHF-UCL,Agbase", "Roslin_Institute,BHF-UCL,Agbase");
@@ -65,65 +70,92 @@ public class AnnotationControllerIT {
 
     private MockMvc mockMvc;
 
-    private String savedAssignedBy;
-    private List<AnnotationDocument> basicDocs;
+    private List<AnnotationDocument> genericDocs;
     private static final String RESOURCE_URL = "/QuickGO/services/annotation";
 
     @Autowired
     protected WebApplicationContext webApplicationContext;
 
     @Autowired
-    protected AnnotationRepository annotationRepository;
+    protected AnnotationRepository repository;
 
     @Before
     public void setup() {
-        annotationRepository.deleteAll();
+        repository.deleteAll();
 
         mockMvc = MockMvcBuilders.
                 webAppContextSetup(webApplicationContext)
                 .build();
 
-        basicDocs = createBasicDocs();
-        assertThat(basicDocs.size(), is(greaterThan(1)));
-        savedAssignedBy = basicDocs.get(0).assignedBy;
-        annotationRepository.save(basicDocs);
+        genericDocs = createGenericDocs(NUMBER_OF_GENERIC_DOCS);
+        repository.save(genericDocs);
     }
 
+    //ASSIGNED BY
     @Test
     public void lookupAnnotationFilterByAssignedBySuccessfully() throws Exception {
+        String geneProductId = "P99999";
+        String assignedBy = "ASPGD";
+
+        AnnotationDocument document = createDocWithAssignedBy(geneProductId, assignedBy);
+        repository.save(document);
+
         ResultActions response = mockMvc.perform(
-                get(RESOURCE_URL + "/search").param(ASSIGNED_BY_PARAM, savedAssignedBy));
+                get(RESOURCE_URL + "/search").param(ASSIGNED_BY_PARAM, assignedBy));
 
         expectResultsInfoExists(response)
                 .andExpect(jsonPath("$.numberOfHits").value(1))
-                .andExpect(jsonPath("$.results.*").exists())
+                .andExpect(jsonPath("$.results[0].geneProductId").value(geneProductId))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk());
     }
 
     @Test
     public void lookupAnnotationFilterByMultipleAssignedBySuccessfully() throws Exception {
+        String geneProductId1 = "P99999";
+        String assignedBy1 = "ASPGD";
+
+        AnnotationDocument document1 = createDocWithAssignedBy(geneProductId1, assignedBy1);
+        repository.save(document1);
+
+        String geneProductId2 = "P99998";
+        String assignedBy2 = "BHF-UCL";
+
+        AnnotationDocument document2 = createDocWithAssignedBy(geneProductId2, assignedBy2);
+        repository.save(document2);
+
         ResultActions response = mockMvc.perform(
-                get(RESOURCE_URL + "/search").param(ASSIGNED_BY_PARAM, basicDocs.get(0).assignedBy + ","
-                        + basicDocs.get(1).assignedBy));
+                get(RESOURCE_URL + "/search").param(ASSIGNED_BY_PARAM, assignedBy1 + "," + assignedBy2));
 
         expectResultsInfoExists(response)
                 .andExpect(jsonPath("$.numberOfHits").value(2))
-                .andExpect(jsonPath("$.results.*").exists())
+                .andExpect(jsonPath("$.results[*].geneProductId", contains(geneProductId1, geneProductId2)))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk());
     }
 
     @Test
     public void lookupAnnotationFilterByRepetitionOfParmsSuccessfully() throws Exception {
+        String geneProductId1 = "P99999";
+        String assignedBy1 = "ASPGD";
+
+        AnnotationDocument document1 = createDocWithAssignedBy(geneProductId1, assignedBy1);
+        repository.save(document1);
+
+        String geneProductId2 = "P99998";
+        String assignedBy2 = "BHF-UCL";
+
+        AnnotationDocument document2 = createDocWithAssignedBy(geneProductId2, assignedBy2);
+        repository.save(document2);
+
         ResultActions response = mockMvc.perform(
-                get(RESOURCE_URL + "/search").param(ASSIGNED_BY_PARAM, basicDocs.get(0).assignedBy)
-                        .param(ASSIGNED_BY_PARAM, basicDocs.get(1).assignedBy)
-                        .param(ASSIGNED_BY_PARAM, basicDocs.get(3).assignedBy));
+                get(RESOURCE_URL + "/search")
+                        .param(ASSIGNED_BY_PARAM, assignedBy1)
+                        .param(ASSIGNED_BY_PARAM, assignedBy2));
 
         expectResultsInfoExists(response)
-                .andExpect(jsonPath("$.numberOfHits").value(3))
-                .andExpect(jsonPath("$.results.*").exists())
+                .andExpect(jsonPath("$.numberOfHits").value(2))
+                .andExpect(jsonPath("$.results[*].geneProductId", contains(geneProductId1, geneProductId2)))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk());
     }
@@ -141,60 +173,112 @@ public class AnnotationControllerIT {
 
     @Test
     public void lookupAnnotationFilterByMultipleAssignedByOneCorrectAndOneUnavailable() throws Exception {
+        String geneProductId = "P99999";
+        String assignedBy = "ASPGD";
+
+        AnnotationDocument document = createDocWithAssignedBy(geneProductId, assignedBy);
+        repository.save(document);
+
         ResultActions response = mockMvc.perform(
                 get(RESOURCE_URL + "/search").param(ASSIGNED_BY_PARAM, UNAVAILABLE_ASSIGNED_BY + ","
-                        + basicDocs.get(1).assignedBy));
+                        + assignedBy));
 
         expectResultsInfoExists(response)
                 .andExpect(jsonPath("$.numberOfHits").value(1))
-                .andExpect(jsonPath("$.results.*").exists())
+                .andExpect(jsonPath("$.results[0].geneProductId").value(geneProductId))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk());
     }
 
     @Test
-    public void allTheseValuesForAssignedShouldNotThrowAnError() throws Exception {
-        for (String validAssignedBy : VALID_ASSIGNED_BY_PARMS) {
-            ResultActions response = mockMvc.perform(
-                    get(RESOURCE_URL + "/search").param(ASSIGNED_BY_PARAM, validAssignedBy));
-            expectResultsInfoExists(response)
-                    .andExpect(status().isOk());
-        }
+    public void invalidAssignedByThrowsAnError() throws Exception {
+        String invalidAssignedBy = "_ASPGD";
+
+        ResultActions response = mockMvc.perform(
+                get(RESOURCE_URL + "/search").param(ASSIGNED_BY_PARAM, invalidAssignedBy));
+        response.andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    //TAXON ID
+    @Test
+    public void lookupAnnotationFilterByTaxonIdSuccessfully() throws Exception {
+        String geneProductId = "P99999";
+        int taxonId = 2;
+
+        AnnotationDocument document = createDocWithTaxonId(geneProductId, taxonId);
+        repository.save(document);
+
+        ResultActions response = mockMvc.perform(
+                get(RESOURCE_URL + "/search").param(TAXON_ID_PARAM, "2"));
+
+        expectResultsInfoExists(response)
+                .andExpect(jsonPath("$.numberOfHits").value(1))
+                .andExpect(jsonPath("$.results[0].geneProductId").value(geneProductId))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk());
     }
 
     @Test
-    public void allTheseValuesForAssignedShouldThrowAnError() throws Exception {
-        for (String inValidAssignedBy : INVALID_ASSIGNED_BY_PARMS) {
-            ResultActions response = mockMvc.perform(
-                    get(RESOURCE_URL + "/search").param(ASSIGNED_BY_PARAM, inValidAssignedBy));
-            response.andDo(print())
-                    .andExpect(status().isBadRequest());
-        }
+    public void lookupAnnotationFilterByMultipleTaxonIdsSuccessfully() throws Exception {
+        String geneProductId1 = "P99999";
+        int taxonId1 = 2;
+
+        AnnotationDocument document1 = createDocWithTaxonId(geneProductId1, taxonId1);
+        repository.save(document1);
+
+        String geneProductId2 = "P99998";
+        int taxonId2 = 3;
+
+        AnnotationDocument document2 = createDocWithTaxonId(geneProductId2, taxonId2);
+        repository.save(document2);
+
+        ResultActions response = mockMvc.perform(
+                get(RESOURCE_URL + "/search")
+                        .param(TAXON_ID_PARAM, String.valueOf(taxonId1))
+                        .param(TAXON_ID_PARAM, String.valueOf(taxonId2)));
+
+        expectResultsInfoExists(response)
+                .andExpect(jsonPath("$.numberOfHits").value(2))
+                .andExpect(jsonPath("$.results.[*].geneProductId", contains(geneProductId1, geneProductId2)))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void invalidTaxIdThrowsError() throws Exception {
+        ResultActions response = mockMvc.perform(
+                get(RESOURCE_URL + "/search").param(TAXON_ID_PARAM, "-2"));
+
+        response.andDo(print())
+                .andExpect(status().isBadRequest());
     }
 
     //---------- Page related tests.
 
     @Test
     public void retrievesSecondPageOfAllEntriesRequest() throws Exception {
-        annotationRepository.deleteAll();
-        annotationRepository.save(createAndSaveDocs(60));
+        int totalEntries = 60;
+
+        repository.deleteAll();
+        repository.save(createGenericDocs(totalEntries));
 
         ResultActions response = mockMvc.perform(
-                get(RESOURCE_URL + "/search").param(ASSIGNED_BY_PARAM, savedAssignedBy)
+                get(RESOURCE_URL + "/search")
                         .param(PAGE_PARAM, "2"));
 
         expectResultsInfoExists(response)
-                .andExpect(jsonPath("$.results").isArray())
+                .andExpect(jsonPath("$.numberOfHits").value(totalEntries))
                 .andExpect(jsonPath("$.results", hasSize(AnnotationRequest.DEFAULT_ENTRIES_PER_PAGE)));
     }
 
     @Test
     public void pageRequestEqualToAvailablePagesReturns200() throws Exception {
         ResultActions response = mockMvc.perform(
-                get(RESOURCE_URL + "/search").param(ASSIGNED_BY_PARAM, savedAssignedBy).param(PAGE_PARAM, "1"));
+                get(RESOURCE_URL + "/search").param(PAGE_PARAM, "1"));
 
         expectResultsInfoExists(response)
-                .andExpect(jsonPath("$.numberOfHits").value(1))
+                .andExpect(jsonPath("$.numberOfHits").value(NUMBER_OF_GENERIC_DOCS))
                 .andExpect(jsonPath("$.results.*").exists())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk());
@@ -203,7 +287,7 @@ public class AnnotationControllerIT {
     @Test
     public void pageRequestOfZeroAndResultsAvailableReturns400() throws Exception {
         ResultActions response = mockMvc.perform(
-                get(RESOURCE_URL + "/search").param(ASSIGNED_BY_PARAM, savedAssignedBy).param(PAGE_PARAM, "0"));
+                get(RESOURCE_URL + "/search").param(PAGE_PARAM, "0"));
 
         response.andDo(print())
                 .andExpect(status().isBadRequest());
@@ -212,13 +296,13 @@ public class AnnotationControllerIT {
     @Test
     public void pageRequestHigherThanAvailablePagesReturns400() throws Exception {
 
-        annotationRepository.deleteAll();
+        repository.deleteAll();
 
         int existingPages = 4;
-        createAndSaveDocs(SearchServiceConfig.MAX_PAGE_RESULTS * existingPages);
+        createGenericDocs(SearchServiceConfig.MAX_PAGE_RESULTS * existingPages);
 
         ResultActions response = mockMvc.perform(
-                get(RESOURCE_URL + "/search").param(ASSIGNED_BY_PARAM, savedAssignedBy)
+                get(RESOURCE_URL + "/search")
                         .param(PAGE_PARAM, String.valueOf(existingPages + 1)));
 
         response.andDo(print())
@@ -229,11 +313,8 @@ public class AnnotationControllerIT {
 
     @Test
     public void limitForPageExceedsMaximumAllowed() throws Exception {
-        annotationRepository.deleteAll();
-        annotationRepository.save(createAndSaveDocs(60));
-
         ResultActions response = mockMvc.perform(
-                get(RESOURCE_URL + "/search").param(ASSIGNED_BY_PARAM, savedAssignedBy)
+                get(RESOURCE_URL + "/search")
                         .param(LIMIT_PARAM, "101"));
 
         response.andDo(print())
@@ -242,12 +323,11 @@ public class AnnotationControllerIT {
 
     @Test
     public void limitForPageWithinMaximumAllowed() throws Exception {
-
         ResultActions response = mockMvc.perform(
-                get(RESOURCE_URL + "/search").param(ASSIGNED_BY_PARAM, savedAssignedBy).param(LIMIT_PARAM, "100"));
+                get(RESOURCE_URL + "/search").param(LIMIT_PARAM, "100"));
 
         expectResultsInfoExists(response)
-                .andExpect(jsonPath("$.numberOfHits").value(1))
+                .andExpect(jsonPath("$.numberOfHits").value(NUMBER_OF_GENERIC_DOCS))
                 .andExpect(jsonPath("$.results.*").exists())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk());
@@ -255,17 +335,12 @@ public class AnnotationControllerIT {
 
     @Test
     public void limitForPageThrowsErrorWhenNegative() throws Exception {
-
-        ResultActions response = mockMvc.perform(get(RESOURCE_URL + "/search").param(ASSIGNED_BY_PARAM, savedAssignedBy)
+        ResultActions response = mockMvc.perform(get(RESOURCE_URL + "/search")
                 .param(LIMIT_PARAM, "-20"));
 
         response.andDo(print())
                 .andExpect(status().isBadRequest());
     }
-
-    /**
-     *      TESTING RESULTS
-     */
 
     private ResultActions expectResultsInfoExists(ResultActions result) throws Exception {
         return expectFieldsInResults(result)
@@ -280,7 +355,7 @@ public class AnnotationControllerIT {
     private ResultActions expectFieldsInResults(ResultActions result) throws Exception {
         int index = 0;
 
-        for (int i = 0; i > basicDocs.size(); i++) {
+        for (int i = 0; i > genericDocs.size(); i++) {
             expectFields(result, "$.results[" + index++ + "].");
         }
 
@@ -299,21 +374,24 @@ public class AnnotationControllerIT {
                 .andExpect(jsonPath(path + "withFrom").exists())
                 .andExpect(jsonPath(path + "taxonId").exists())
                 .andExpect(jsonPath(path + "assignedBy").exists())
-                .andExpect(jsonPath(path + "extension").exists());
+                .andExpect(jsonPath(path + "extensions").exists());
     }
 
-    /**
-     * Create some
-     */
-    private List<AnnotationDocument> createBasicDocs() {
-        return Arrays.asList(
-                AnnotationDocMocker.createAnnotationDoc("A0A000"),
-                AnnotationDocMocker.createAnnotationDoc("A0A001", "ASPGD"),
-                AnnotationDocMocker.createAnnotationDoc("A0A001", "BHF-UCL"),
-                AnnotationDocMocker.createAnnotationDoc("A0A002", "AGPRD"));
+    private AnnotationDocument createDocWithAssignedBy(String geneProductId, String assignedBy) {
+        AnnotationDocument doc = AnnotationDocMocker.createAnnotationDoc(geneProductId);
+        doc.assignedBy = assignedBy;
+
+        return doc;
     }
 
-    private List<AnnotationDocument> createAndSaveDocs(int n) {
+    private AnnotationDocument createDocWithTaxonId(String geneProductId, int taxonId) {
+        AnnotationDocument doc = AnnotationDocMocker.createAnnotationDoc(geneProductId);
+        doc.taxonId = taxonId;
+
+        return doc;
+    }
+
+    private List<AnnotationDocument> createGenericDocs(int n) {
         return IntStream.range(0, n)
                 .mapToObj(i -> AnnotationDocMocker.createAnnotationDoc(createId(i))).collect
                         (Collectors.toList());
