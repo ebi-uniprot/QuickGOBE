d39444bccf24afd3ef9a2f89e65bcbb6ea23ae50
rantunes <rantunes@ebi.ac.uk>
2016-05-26 11:41:24 +0100

    Add more conversion logic to convert the majority of the annotation fields.

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/service/converter/AnnotationDocConverterImpl.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/service/converter/AnnotationDocConverterImpl.java
@@ -3,19 +3,38 @@ package uk.ac.ebi.quickgo.annotation.service.converter;
 import uk.ac.ebi.quickgo.annotation.common.document.AnnotationDocument;
 import uk.ac.ebi.quickgo.annotation.model.Annotation;
 
+import java.util.ArrayList;
+
 /**
+ * Concrete implementation of the {@link AnnotationDocConverter}.
+ *
  * @author Tony Wardell
  * Date: 26/04/2016
  * Time: 16:52
- * Created with IntelliJ IDEA.
  */
 public class AnnotationDocConverterImpl implements AnnotationDocConverter {
 
     @Override public Annotation convert(AnnotationDocument annotationDocument) {
         Annotation annotation = new Annotation();
+        annotation.id = annotationDocument.id;
+        annotation.geneProductId = annotationDocument.geneProductId;
+        annotation.qualifier = annotationDocument.qualifier;
+        annotation.goId = annotationDocument.goId;
+        annotation.goEvidence = annotationDocument.goEvidence;
+        annotation.ecoId = annotationDocument.ecoId;
+        annotation.reference = annotationDocument.reference;
+        annotation.taxonId = annotationDocument.taxonId;
 
-        //todo take elements
         annotation.assignedBy = annotationDocument.assignedBy;
+
+        if(annotationDocument.withFrom != null) {
+            annotation.withFrom = new ArrayList<>(annotationDocument.withFrom);
+        }
+
+        if(annotationDocument.extensions != null) {
+            annotation.extensions = new ArrayList<>(annotationDocument.extensions);
+        }
+
         return annotation;
     }
 }
