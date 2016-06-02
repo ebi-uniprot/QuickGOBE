ee524d96a8d9cceb5367f0f836a8c984cbd123ba
twardell <twardell@ebi.ac.uk>
2016-06-02 12:25:42 +0100

    Create a system wide holder for definitions.

---
+++ common/src/main/java/uk/ac/ebi/quickgo/common/Definitions.java
@@ -0,0 +1,14 @@
+package uk.ac.ebi.quickgo.common;
+
+/**
+ * QuickGOBE wide definitions
+ *
+ * @author Tony Wardell
+ * Date: 02/06/2016
+ * Time: 12:11
+ * Created with IntelliJ IDEA.
+ */
+public class Definitions {
+
+    public static final String APP_PROPERTIES_DELIMITER=",";
+}
