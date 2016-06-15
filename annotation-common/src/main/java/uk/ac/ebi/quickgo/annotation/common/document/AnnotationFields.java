8ebdd68bffc8617015a97147ec42970100910d2e
twardell <twardell@ebi.ac.uk>
2016-06-15 16:57:10 +0100

    Add GeneProductID to the list of searchable fields.

--- annotation-common/src/main/java/uk/ac/ebi/quickgo/annotation/common/document/AnnotationFields.java
+++ annotation-common/src/main/java/uk/ac/ebi/quickgo/annotation/common/document/AnnotationFields.java
@@ -51,6 +51,7 @@ public class AnnotationFields {
         private static final Set<String> VALUES = new HashSet<>();
 
         public static final String ASSIGNED_BY = storeAndGet(VALUES, AnnotationFields.ASSIGNED_BY);
+        public static final String GENEPRODUCT_ID = storeAndGet(VALUES, AnnotationFields.GENE_PRODUCT_ID);
 
         public static boolean isSearchable(String field) {
             return VALUES.contains(field);
