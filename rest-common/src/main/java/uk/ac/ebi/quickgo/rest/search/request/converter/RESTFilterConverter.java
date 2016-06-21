f4738ca55115fa982253bf2f4b3b4b443ffe0b4e
Edward Turner <eddturner@ebi.ac.uk>
2016-06-21 10:50:35 +0100

    tidying and adding more tests for rest filter converter.

--- rest-common/src/main/java/uk/ac/ebi/quickgo/rest/search/request/converter/RESTFilterConverter.java
+++ rest-common/src/main/java/uk/ac/ebi/quickgo/rest/search/request/converter/RESTFilterConverter.java
@@ -10,7 +10,6 @@ import com.jayway.jsonpath.JsonPath;
 import java.util.Optional;
 import java.util.StringJoiner;
 import java.util.concurrent.ExecutionException;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import net.minidev.json.JSONArray;
 import org.slf4j.Logger;
@@ -18,29 +17,53 @@ import org.slf4j.Logger;
 import static org.slf4j.LoggerFactory.getLogger;
 
 /**
- * <p>Defines the conversion of a REST request to a corresponding {@link QuickGOQuery}.
- *
- * <p>NB. This class is a placeholder for a real implementation
+ * <p>Defines the conversion of a {@link FilterRequest} representing a REST request
+ * to a corresponding {@link QuickGOQuery}.
  *
  * Created by Edd on 05/06/2016.
  */
 class RESTFilterConverter implements FilterConverter {
-    private static final String IP = "ip";
-    private static final String RESOURCE_FORMAT = "resourceFormat";
-    private static final String BODY_PATH = "responseBodyPath";
-
     private static final Logger LOGGER = getLogger(RESTFilterConverter.class);
-    private static final Collector<CharSequence, ?, String> COMMA = Collectors.joining(",");
-    private static final String LOCAL_FIELD = "localField";
+    static final String HOST = "ip";
+    static final String RESOURCE_FORMAT = "resourceFormat";
+    static final String BODY_PATH = "responseBodyPath";
+    static final String LOCAL_FIELD = "localField";
+    private static final String COMMA = ",";
+    private static final String HTTP_HOST_PREFIX = "http://";
+    private static final String FORWARD_SLASH = "/";
 
     private final FilterConfig filterConfig;
 
     RESTFilterConverter(FilterConfig filterConfig) {
         this.filterConfig = filterConfig;
 
+        checkMandatoryProperty(HOST);
         checkMandatoryProperty(RESOURCE_FORMAT);
         checkMandatoryProperty(BODY_PATH);
         checkMandatoryProperty(LOCAL_FIELD);
+    }
+
+    @Override public QuickGOQuery transform(FilterRequest request) {
+        Preconditions.checkArgument(request != null, "FilterRequest cannot be null");
+
+        // create REST request executor
+        RESTRequesterImpl.Builder restRequesterBuilder =
+                RESTRequesterImpl.newBuilder(buildResourceTemplate(filterConfig));
+        request.getProperties().entrySet().stream()
+                .forEach(entry ->
+                    restRequesterBuilder.addRequestParameter(
+                            entry.getKey(),
+                            entry.getValue().stream()
+                                    .collect(Collectors.joining(COMMA)))
+                );
+
+        // apply request and store results
+        JsonPath jsonPath = JsonPath.compile(filterConfig.getProperties().get(BODY_PATH));
+        StringJoiner joinedValues = new StringJoiner(COMMA);
+        fetchResults(restRequesterBuilder.build())
+                .ifPresent(responseBody -> extractValues(responseBody, jsonPath, joinedValues));
+
+        return QuickGOQuery.createQuery(filterConfig.getProperties().get(LOCAL_FIELD), joinedValues.toString());
     }
 
     private void checkMandatoryProperty(String mandatoryProperty) {
@@ -48,30 +71,7 @@ class RESTFilterConverter implements FilterConverter {
                 "FilterConfig must have mandatory field: " + mandatoryProperty);
     }
 
-    @Override public QuickGOQuery transform(FilterRequest request) {
-        Preconditions.checkArgument(request != null, "FilterRequest cannot be null");
-
-        // create REST request executor
-        RESTRequesterImpl.Builder restRequesterBuilder = RESTRequesterImpl
-                .newBuilder(buildResource());
-        request.getProperties().entrySet().stream()
-                .forEach(entry -> {
-                    restRequesterBuilder.addRequestParameter(
-                            entry.getKey(),
-                            entry.getValue().stream()
-                                    .collect(COMMA));
-                });
-
-        // apply request and store results
-        JsonPath jsonPath = JsonPath.compile(filterConfig.getProperties().get(BODY_PATH));
-        StringJoiner joinedValues = new StringJoiner(",");
-        fetchResults(restRequesterBuilder.build())
-                .ifPresent(responseBody -> insertValues(responseBody, jsonPath, joinedValues));
-
-        return QuickGOQuery.createQuery(filterConfig.getProperties().get(LOCAL_FIELD), joinedValues.toString());
-    }
-
-    private void insertValues(String responseBody, JsonPath jsonPath, StringJoiner joinedValues) {
+    private void extractValues(String responseBody, JsonPath jsonPath, StringJoiner joinedValues) {
         ((JSONArray) jsonPath.read(responseBody)).iterator()
                 .forEachRemaining(value -> joinedValues.add(value.toString()));
     }
@@ -86,7 +86,26 @@ class RESTFilterConverter implements FilterConverter {
         return Optional.empty();
     }
 
-    private String buildResource() {
-        return filterConfig.getProperties().get(IP) + filterConfig.getProperties().get(RESOURCE_FORMAT);
+    static String buildResourceTemplate(FilterConfig config) {
+        return retrieveHostProperty(config) + retrieveResourceFormat(config);
+    }
+
+    private static String retrieveResourceFormat(FilterConfig config) {
+        String resourceFormat = config.getProperties().get(RESOURCE_FORMAT);
+        if (!resourceFormat.startsWith(FORWARD_SLASH)) {
+            resourceFormat = FORWARD_SLASH + resourceFormat;
+        }
+        return resourceFormat;
+    }
+
+    private static String retrieveHostProperty(FilterConfig config) {
+        String host = config.getProperties().get(HOST).trim();
+        if (!host.startsWith(HTTP_HOST_PREFIX)) {
+            host = HTTP_HOST_PREFIX + host;
+        }
+        if (host.endsWith(FORWARD_SLASH)) {
+            host = host.substring(0, host.length() - 1);
+        }
+        return host;
     }
 }
--- rest-common/src/test/java/uk/ac/ebi/quickgo/rest/search/request/converter/RESTFilterConverterTest.java
+++ rest-common/src/test/java/uk/ac/ebi/quickgo/rest/search/request/converter/RESTFilterConverterTest.java
@@ -9,6 +9,10 @@ import java.util.Map;
 import org.junit.Test;
 import org.springframework.web.client.RestTemplate;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static uk.ac.ebi.quickgo.rest.search.request.converter.RESTFilterConverter.*;
+
 /**
  * Created 20/06/16
  * @author Edd
@@ -23,10 +27,10 @@ public class RESTFilterConverterTest {
         FilterConfig config = new FilterConfig();
         config.setExecution(FilterConfig.ExecutionType.REST_COMM);
         Map<String, String> configMap = new HashMap<>();
-        configMap.put("ip", "http://ves-hx-c2:8082");
-        configMap.put("responseBodyPath", "$.results[*].ancestors[*]");
-        configMap.put("resourceFormat", "/QuickGO/services/go/terms/{id}/complete");
-        configMap.put("localField", "goId");
+        configMap.put(HOST, "http://ves-hx-c2:8082");
+        configMap.put(BODY_PATH, "$.results[*].ancestors[*]");
+        configMap.put(RESOURCE_FORMAT, "/QuickGO/services/go/terms/{id}/complete");
+        configMap.put(LOCAL_FIELD, "goId");
         config.setProperties(configMap);
 
         RESTFilterConverter converter = new RESTFilterConverter(config);
@@ -43,6 +47,53 @@ public class RESTFilterConverterTest {
         String forObject = template.getForObject("http://ves-hx-c2:8082/QuickGO/services/go/terms/{id}/complete", String
                 .class, m);
         System.out.println(forObject);
+    }
+
+    @Test
+    public void buildsResourceTemplateWithHostNameOnlyAndResourceOnly() {
+        FilterConfig config = new FilterConfig();
+        config.setExecution(FilterConfig.ExecutionType.REST_COMM);
+        Map<String, String> configMap = new HashMap<>();
+        String host = "ves-hx-c2:8082";
+        configMap.put(HOST, host);
+        String resource = "/QuickGO/services/go/terms/{id}/complete";
+        configMap.put(RESOURCE_FORMAT, resource);
+        config.setProperties(configMap);
+
+        String resourceTemplate = buildResourceTemplate(config);
+        assertThat(resourceTemplate, is("http://" + host + resource));
+    }
+
+    @Test
+    public void buildsResourceTemplateWithHostNameEndingInForwardSlashAndResourceOnly() {
+        FilterConfig config = new FilterConfig();
+        config.setExecution(FilterConfig.ExecutionType.REST_COMM);
+        Map<String, String> configMap = new HashMap<>();
+        String ip = "ves-hx-c2:8082";
+        String host = ip + "/";
+        configMap.put(HOST, host);
+        String resourceNoLeadingSlash = "QuickGO/services/go/terms/{id}/complete";
+        configMap.put(RESOURCE_FORMAT, resourceNoLeadingSlash);
+        config.setProperties(configMap);
+
+        String resourceTemplate = buildResourceTemplate(config);
+        assertThat(resourceTemplate, is("http://" + ip + "/" + resourceNoLeadingSlash));
+    }
+
+    @Test
+    public void buildsResourceTemplateWithHttpHostNameAndResourceOnly() {
+        FilterConfig config = new FilterConfig();
+        config.setExecution(FilterConfig.ExecutionType.REST_COMM);
+        Map<String, String> configMap = new HashMap<>();
+        String ip = "ves-hx-c2:8082";
+        String host = "http://" + ip + "/";
+        configMap.put(HOST, host);
+        String resource = "QuickGO/services/go/terms/{id}/complete";
+        configMap.put(RESOURCE_FORMAT, resource);
+        config.setProperties(configMap);
+
+        String resourceTemplate = buildResourceTemplate(config);
+        assertThat(resourceTemplate, is("http://" + ip + "/" + resource));
     }
 
 }
