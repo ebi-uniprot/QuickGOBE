c37976b00751558ef4824f89afd728135a27b84c
twardell <twardell@ebi.ac.uk>
2016-06-28 11:58:18 +0100

    Add Eco Id

--- annotation-common/src/main/java/uk/ac/ebi/quickgo/annotation/common/document/AnnotationFields.java
+++ annotation-common/src/main/java/uk/ac/ebi/quickgo/annotation/common/document/AnnotationFields.java
@@ -53,6 +53,7 @@ public class AnnotationFields {
         public static final String ASSIGNED_BY = storeAndGet(VALUES, AnnotationFields.ASSIGNED_BY);
         public static final String TAXON_ID = storeAndGet(VALUES, AnnotationFields.TAXON_ID);
         public static final String GO_EVIDENCE = storeAndGet(VALUES, AnnotationFields.GO_EVIDENCE);
+        public static final String ECO_ID = storeAndGet(VALUES, AnnotationFields.ECO_ID);
 
         public static boolean isSearchable(String field) {
             return VALUES.contains(field);
