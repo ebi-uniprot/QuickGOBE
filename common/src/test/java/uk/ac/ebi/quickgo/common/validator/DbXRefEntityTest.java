fb0bbd049245c97addf56e3d20c0ce13e419b3ad
twardell <twardell@ebi.ac.uk>
2016-07-08 14:51:36 +0100

    Reformat and rearrange code.

--- common/src/test/java/uk/ac/ebi/quickgo/common/validator/DbXRefEntityTest.java
+++ common/src/test/java/uk/ac/ebi/quickgo/common/validator/DbXRefEntityTest.java
@@ -9,39 +9,36 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 /**
+ * Test for DbXRefEntity
  * @author Tony Wardell
  *         Date: 19/04/2016
  *         Time: 13:50
  *         Created with IntelliJ IDEA.
  */
-public class GeneProductXrefEntityTest {
+public class DbXRefEntityTest {
 
+	private static final String database = "UniProtKB";
+	private static final String entityType = "PR:000000001";
+	private static final String entityTypeName = "protein";
+	private static final String idValidationPattern = "([OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z]([0-9][A-Z][A-Z0-9]{2}){1,2}[0-9])((-[0-9]+)|:PRO_[0-9]{10}|:VAR_[0-9]{6}){0,1}";
+	private static final String dbURL = " http://www.uniprot.org/uniprot/[example_id]";
 	@Rule
 	public ExpectedException thrown = ExpectedException.none();
-
-
-	private String database = "UniProtKB";
-	private String entityType = "PR:000000001";
-	private String entityTypeName = "protein";
-	private String idValidationPattern = "([OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z]([0-9][A-Z][A-Z0-9]{2}){1,2}[0-9])((-[0-9]+)|:PRO_[0-9]{10}|:VAR_[0-9]{6}){0,1}";
-	private String dbURL = " http://www.uniprot.org/uniprot/[example_id]";
-
-	DbXRefEntity geneProductXrefEntity;
+	private DbXRefEntity dbXrefEntity;
 
 	@Before
 	public void setup(){
-		geneProductXrefEntity = new DbXRefEntity(database, entityType, entityTypeName,
-				idValidationPattern, dbURL);
+		dbXrefEntity = new DbXRefEntity(database, entityType, entityTypeName, idValidationPattern, dbURL);
 	}
 
 	@Test
 	public void validId(){
-		assertThat(geneProductXrefEntity.matches("A0A000"), is(true));
+		assertThat(dbXrefEntity.matches("A0A000"), is(true));
 	}
 
 	@Test
 	public void inValidId(){
-		assertThat(geneProductXrefEntity.matches("99999"), is(false));
+		assertThat(dbXrefEntity.matches("99999"), is(false));
 	}
 
 	@Test
