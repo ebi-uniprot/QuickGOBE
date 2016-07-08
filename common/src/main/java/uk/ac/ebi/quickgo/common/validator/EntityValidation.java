9395ec401c5ab8ca6256647438c4720b2a576c30
twardell <twardell@ebi.ac.uk>
2016-07-08 11:54:19 +0100

    Copy over the test classes from the gene product core.

--- common/src/main/java/uk/ac/ebi/quickgo/common/validator/EntityValidation.java
+++ common/src/main/java/uk/ac/ebi/quickgo/common/validator/EntityValidation.java
@@ -7,27 +7,29 @@ import java.util.Map;
 import static com.google.common.base.Preconditions.checkArgument;
 
 /**
- *
- * Holds the entries from 'DB_XREFS_ENTITIES.dat.gz'
- * Ask this class if the id is a valid gene product id.
- * Can either use defaults or supply the identifying database and typeName (protein or gene).
- *
+ * This class allows the ids of entities (genes, proteins etc) to be validated. The id is validated against one or
+ * more entity definitions, each of which is defined by a database - ids are valid in terms of the database they are
+ * sourced from. An id is confirmed to be valid by successfully matching a regular expression defined for the
+ * combination of database and entity type.
  *
  * @author Tony Wardell
  *         Date: 18/04/2016
  *         Time: 13:36
  *         Created with IntelliJ IDEA.
  */
-public class GeneProductDbXRefIDFormats {
+public class EntityValidation {
 
-    private final Map<GeneProductDbXRefIDFormats.Key, GeneProductDbXRefIDFormat> geneProductXrefEntities;
-    static final Key[] targetDBs = new Key[]{ new Key("UniProtKB","PR:000000001"), new Key("IntAct","GO:0043234"),
-            new Key("RNAcentral","CHEBI:33697")};
+    // A list of entries loaded from 'DB_XREFS_ENTITIES.dat.gz' keyed by database and entity type id.
+    private final Map<EntityValidation.Key, GeneProductDbXRefIDFormat> entityList;
 
-    private GeneProductDbXRefIDFormats(Map<Key, GeneProductDbXRefIDFormat> geneProductXrefEntities) {
-        checkArgument(geneProductXrefEntities != null, "Gene product xref entities map cannot be null");
+    // Default list of databases and entity types to validate against.
+    static final Key[] targetDBs = new Key[]{new Key("UniProtKB", "PR:000000001"), new Key("IntAct", "GO:0043234"),
+            new Key("RNAcentral", "CHEBI:33697")};
 
-        this.geneProductXrefEntities = geneProductXrefEntities;
+    private EntityValidation(Map<Key, GeneProductDbXRefIDFormat> entityList) {
+        checkArgument(entityList != null, "Gene product xref entities map cannot be null");
+
+        this.entityList = entityList;
     }
 
     /**
@@ -38,13 +40,13 @@ public class GeneProductDbXRefIDFormats {
     public boolean isValidId(String id) {
 
         //If we haven't managed to load the validation regular expressions then pass everything
-        if (geneProductXrefEntities.size()==0) {
+        if (entityList.size() == 0) {
             return true;
         }
-        for(Key dbKey : targetDBs){
-            GeneProductDbXRefIDFormat entity = this.geneProductXrefEntities.get(dbKey);
-            if(null==entity)continue;
-            if(entity.matches(id)) return true;
+        for (Key dbKey : targetDBs) {
+            if (this.entityList.get(dbKey).matches(id)) {
+                return true;
+            }
         }
 
         //no matches
@@ -56,32 +58,38 @@ public class GeneProductDbXRefIDFormats {
      * @param entities A list of regex expressions specified by database and usage.
      * @return
      */
-    public static GeneProductDbXRefIDFormats createWithData(List<GeneProductDbXRefIDFormat> entities) {
+    public static EntityValidation createWithData(List<GeneProductDbXRefIDFormat> entities) {
         Preconditions.checkArgument(entities != null, "The list of GeneProductDbXRefIDFormat entities is null, which " +
                 "is illegal");
 
         Map<Key, GeneProductDbXRefIDFormat> mappedEntities = new HashMap<>();
 
-        for(GeneProductDbXRefIDFormat entity : entities) {
-            GeneProductDbXRefIDFormats.Key key = new GeneProductDbXRefIDFormats.Key(entity.getDatabase(), entity
+        for (GeneProductDbXRefIDFormat entity : entities) {
+            EntityValidation.Key key = new EntityValidation.Key(entity.getDatabase(), entity
                     .getEntityType());
             mappedEntities.put(key, entity);
         }
 
-        return new GeneProductDbXRefIDFormats(mappedEntities);
+        return new EntityValidation(mappedEntities);
     }
 
     /**
      * Key class holds the variables we need to lookup up the validating regex
      */
 
-    public static class Key {
+    private static class Key {
         String database;
         String entityType;
 
-        public Key(String database, String entityType) {
+        private Key(String database, String entityType) {
             this.database = database;
             this.entityType = entityType;
+        }
+
+        @Override public int hashCode() {
+            int result = database.hashCode();
+            result = 31 * result + entityType.hashCode();
+            return result;
         }
 
         @Override public boolean equals(Object o) {
@@ -99,12 +107,6 @@ public class GeneProductDbXRefIDFormats {
             }
             return entityType.equals(key.entityType);
 
-        }
-
-        @Override public int hashCode() {
-            int result = database.hashCode();
-            result = 31 * result + entityType.hashCode();
-            return result;
         }
 
         @Override public String toString() {
