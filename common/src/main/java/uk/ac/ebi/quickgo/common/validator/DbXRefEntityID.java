dc8cbd6db41e3c6937e36d2cf8d3ae361cb61b66
twardell <twardell@ebi.ac.uk>
2016-07-08 11:56:37 +0100

    Rename to DbXRefEntityID

--- common/src/main/java/uk/ac/ebi/quickgo/common/validator/DbXRefEntityID.java
+++ common/src/main/java/uk/ac/ebi/quickgo/common/validator/DbXRefEntityID.java
@@ -13,7 +13,7 @@ import static com.google.common.base.Preconditions.checkArgument;
  *         Time: 13:52
  *         Created with IntelliJ IDEA.
  */
-public class GeneProductDbXRefIDFormat {
+public class DbXRefEntityID {
 
     private final String database;
     private final String entityType;
@@ -21,7 +21,7 @@ public class GeneProductDbXRefIDFormat {
     private final Pattern idValidationPattern;
     private final String databaseURL;
 
-    public GeneProductDbXRefIDFormat(String database, String entityType, String entityTypeName,
+    public DbXRefEntityID(String database, String entityType, String entityTypeName,
             String idValidationPattern, String databaseURL) {
 
         checkArgument(database != null, "The database ID should not be null");
