da24cb2eeaeffd55d772e3a5b3ec76d1a9afa44e
rantunes <rantunes@ebi.ac.uk>
2016-07-10 17:28:19 +0100

    Add extra methods to check for the presence or abscence of results, nested aggregations and buckets.

--- rest-common/src/main/java/uk/ac/ebi/quickgo/rest/search/results/Aggregation.java
+++ rest-common/src/main/java/uk/ac/ebi/quickgo/rest/search/results/Aggregation.java
@@ -33,38 +33,121 @@ public class Aggregation {
         buckets = new LinkedHashSet<>();
     }
 
-    public void addAggregationResult(AggregateFunction function, String name, double result) {
-        aggregationResultsManager.addAggregateResult(function, name, result);
+    /**
+     * Adds the result of the application of an aggregation function over a given field.
+     *
+     * @param function the aggregation function that was applied
+     * @param field the field the function was applied over
+     * @param result the result of the application of the function over the field
+     */
+    public void addAggregationResult(AggregateFunction function, String field, double result) {
+        aggregationResultsManager.addAggregateResult(function, field, result);
     }
 
+    /**
+     * Adds a set of distinct values of a given field. The bucket can also potentially contain aggregation
+     * results.
+     *
+     * @param bucket a set of distinct results of field
+     */
     public void addBucket(AggregationBucket bucket) {
         Preconditions.checkArgument(bucket != null, "AggregationBucket cannot be null");
         buckets.add(bucket);
     }
 
-    public void addAggregation(Aggregation aggregation) {
+    /**
+     * Adds a lower level aggregation to the current one. Usually nested aggregations are drilled down results
+     * applied to a different field than the current aggregation.
+     *
+     * @param aggregation an aggregation
+     */
+    public void addNestedAggregation(Aggregation aggregation) {
         Preconditions.checkArgument(aggregation != null, "Nested aggregation cannot be null");
         nestedAggregations.add(aggregation);
     }
 
+    /**
+     * The name of the aggregation.
+     * <p/>
+     * Usually the name will take the form of the name of the field(s) the aggregation is focused upon. Although this
+     * is not strictly necessary.
+     *
+     * @return the name of the aggregation
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns all the results of aggregation functions applied on fields.
+     *
+     * @return a set of {@link AggregationResult}s.
+     */
     public Set<AggregationResult> getAggregationResults() {
         return aggregationResultsManager.getAggregationResults();
     }
 
+    /**
+     * Checks if there are any aggregations results stored within the aggregation.
+     *
+     * @return true if there is at least one aggregation result, false otherwise.
+     */
+    public boolean hasAggregationResults() {
+        return !aggregationResultsManager.getAggregationResults().isEmpty();
+    }
+
+    /**
+     * Returns a specific aggregation result, identified by the aggregation function and the field it was applied upon.
+     *
+     * @param function the aggregation function to lookup
+     * @param fieldName the field the {@param function} was applied on
+     * @return the {@link AggregationResult}
+     */
     public Optional<AggregationResult> getAggregationResult(AggregateFunction function, String fieldName) {
         return aggregationResultsManager.getAggregationResult(function, fieldName);
     }
 
+    /**
+     * Returns a set of all nested aggregations.
+     *
+     * @return a set of the nested aggregations.
+     */
     public Set<Aggregation> getNestedAggregations() {
         return nestedAggregations;
     }
 
+    /**
+     * Indicates whether the aggregation has nested aggregations stored within it.
+     * @return true if there are nested aggregations associated to this aggregation, false otherwise.
+     */
+    public boolean hasNestedAggregations() {
+        return !nestedAggregations.isEmpty();
+    }
+
+    /**
+     * Returns the set of aggregation buckets associated to this aggregation.
+     *
+     * @return a set of {@link AggregationBucket}
+     */
     public Set<AggregationBucket> getBuckets() {
         return buckets;
     }
 
+    /**
+     * Indicates whther the aggregation, at the moment of the method call, has any buckets associated to it.
+     * @return
+     */
+    public boolean hasBuckets() {
+        return !buckets.isEmpty();
+    }
+
+    /**
+     * Inidcates whether the aggregation, at the time of the method call, has at least one of its state elements
+     * populated.
+     *
+     * @return true if it has at least one object stored within it, false otherwise
+     */
+    public boolean isPopulated() {
+        return this.hasAggregationResults() || this.hasNestedAggregations() || this.hasBuckets();
+    }
 }
--- rest-common/src/test/java/uk/ac/ebi/quickgo/rest/search/results/AggregationTest.java
+++ rest-common/src/test/java/uk/ac/ebi/quickgo/rest/search/results/AggregationTest.java
@@ -1,11 +1,14 @@
 package uk.ac.ebi.quickgo.rest.search.results;
 
+import uk.ac.ebi.quickgo.rest.search.AggregateFunction;
+
 import java.util.Set;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.hasSize;
@@ -69,24 +72,96 @@ public class AggregationTest {
     }
 
     @Test
+    public void aggregationWithNoBucketsReturnsFalseWhenQueriedAboutThePresenceOfBuckets() throws Exception {
+        assertThat(aggregation.hasBuckets(), is(false));
+    }
+
+    @Test
+    public void aggregationWithOneBucketReturnsTrueWhenQueriedAboutThePresenceOfBuckets() throws Exception {
+        AggregationBucket bucket = new AggregationBucket("value");
+        aggregation.addBucket(bucket);
+
+        assertThat(aggregation.hasBuckets(), is(true));
+    }
+
+    @Test
     public void addingNullNestedAggregateThrowsException() throws Exception {
         Aggregation nestedAggregation = null;
 
         thrown.expect(IllegalArgumentException.class);
         thrown.expectMessage("Nested aggregation cannot be null");
 
-        aggregation.addAggregation(nestedAggregation);
+        aggregation.addNestedAggregation(nestedAggregation);
     }
 
     @Test
     public void nestedAggregateGetsAddedToStoredAggregates() throws Exception {
         Aggregation nestedAggregation = new Aggregation("nestedAggregation");
 
-        aggregation.addAggregation(nestedAggregation);
+        aggregation.addNestedAggregation(nestedAggregation);
 
         Set<Aggregation> retrievedAggregates = aggregation.getNestedAggregations();
 
         assertThat(retrievedAggregates, hasSize(1));
         assertThat(retrievedAggregates, contains(nestedAggregation));
+    }
+
+    @Test
+    public void aggregationWithNoNestedAggregationsReturnsFalseWhenQueriedAboutThePresenceOfNestedAggregations()
+            throws Exception {
+        assertThat(aggregation.hasNestedAggregations(), is(false));
+    }
+
+    @Test
+    public void aggregationWithANestedAggregationsReturnsTrueWhenQueriedAboutThePresenceOfNestedAggregations()
+            throws Exception {
+        Aggregation nestedAggregation = new Aggregation("nestedAggregation");
+        aggregation.addNestedAggregation(nestedAggregation);
+
+        assertThat(aggregation.hasNestedAggregations(), is(true));
+    }
+
+    @Test
+    public void aggregationWithNoAggregationsResultsReturnsFalseWhenQueriedAboutThePresenceOfAggregationResults()
+            throws Exception {
+        assertThat(aggregation.hasAggregationResults(), is(false));
+    }
+
+    @Test
+    public void aggregationWithAnAggregationsResultsReturnsTrueWhenQueriedAboutThePresenceOfAggregationResults()
+            throws Exception {
+        aggregation.addAggregationResult(AggregateFunction.COUNT, "field", 0);
+
+        assertThat(aggregation.hasAggregationResults(), is(true));
+    }
+
+    @Test
+    public void aggregationWithNoResultsOrNestedingsOrBucketsReturnsFalseWhenQueriedIfItsPopulated() throws Exception {
+        assertThat(aggregation.isPopulated(), is(false));
+    }
+
+    @Test
+    public void aggregationWithAResultAndNoNestingsAndNoBucketsReturnsTrueWhenQueriedIfItsPopulated() throws Exception {
+        aggregation.addAggregationResult(AggregateFunction.COUNT, "field", 0);
+
+        assertThat(aggregation.isPopulated(), is(true));
+    }
+
+    @Test
+    public void aggregationWithANestingAndNoResultAndNoBucketsReturnsTrueWhenQueriedIfItsPopulated()
+            throws Exception {
+        Aggregation nestedAggregation = new Aggregation("nestedAggregation");
+        aggregation.addNestedAggregation(nestedAggregation);
+
+        assertThat(aggregation.isPopulated(), is(true));
+    }
+
+    @Test
+    public void aggregationWithABucketAndNoResultAndNoNestingsReturnsTrueWhenQueriedIfItsPopulated()
+            throws Exception {
+        AggregationBucket bucket = new AggregationBucket("value");
+        aggregation.addBucket(bucket);
+
+        assertThat(aggregation.isPopulated(), is(true));
     }
 }
