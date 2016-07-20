7981849615ad3445220fb9848cb2e245cc9a774e
Edward Turner <eddturner@ebi.ac.uk>
2016-07-20 13:44:58 +0100

    javadocs for statisticsservice

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/service/statistics/StatisticsService.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/service/statistics/StatisticsService.java
@@ -5,11 +5,19 @@ import uk.ac.ebi.quickgo.annotation.model.StatisticsGroup;
 import uk.ac.ebi.quickgo.rest.search.results.QueryResult;
 
 /**
- * TODO: write something here
+ * Contract for a service that provides annotation based statistics.
  *
  * @author Ricardo Antunes
  */
 public interface StatisticsService {
 
+    /**
+     * Calculates the statistics based on the results returned by
+     * an {@link AnnotationRequest}.
+     *
+     * @param request the {@link AnnotationRequest} whose results will have statistics calculated
+     * @return a {@link QueryResult} containing a {@link StatisticsGroup}, representing the
+     * statistics corresponding to the annotation data-set
+     */
     QueryResult<StatisticsGroup> calculate(AnnotationRequest request);
 }
