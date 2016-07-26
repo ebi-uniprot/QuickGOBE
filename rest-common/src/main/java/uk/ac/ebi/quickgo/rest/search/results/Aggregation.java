e5ed606fadcf2b4002d4c09d5f6f2b0906f43057
rantunes <rantunes@ebi.ac.uk>
2016-07-26 11:34:42 +0100

    Add more Javadoc explaining what an aggregation represents.

--- rest-common/src/main/java/uk/ac/ebi/quickgo/rest/search/results/Aggregation.java
+++ rest-common/src/main/java/uk/ac/ebi/quickgo/rest/search/results/Aggregation.java
@@ -10,7 +10,48 @@ import java.util.Set;
 
 /**
  * Stores the result of the aggregation section found within the response of the underlying data source.
- *
+ * <p/>
+ * An instance of an aggregation could have one or several of the following:
+ * <ul>
+ *     <li>The name of the aggregation. If the aggregation is based on a field, then the name is name of the field,
+ *     however if the aggregation is based on a query result then the name could be anything.</li>
+ *     <li>A set of {@link AggregationResult}s: An aggregation result is a value that reflects the application of
+ *     an {@link AggregateFunction} on a particular field. This field could be the aggregate field, or any other
+ *     field that can be calculated within the response's ability, example: sum(field1) or unique(field2)</li>
+ *     <li>A set of {@link AggregationBucket}: Each bucket represents a distinct value of the field being
+ *     aggregated upon, as well as any {@link AggregationResult} applied to the bucket value</li>
+ *     <li>A set of {@link Aggregation#nestedAggregations}: A nested aggregation is a drilled down view of the main
+ *     aggregation. This means that it tries to retrieve metrics on a different aggregate field, but always based on
+ *     the result of the current aggregation</li>
+ * </ul>
+ * <p/>
+ * As an example, assume that the data source has a table/collection of orders, with the following fields:
+ * order_item_id, quantity, cost.
+ * Sending an {@link Aggregate} object, to the data store, with the following requests:
+ * <ul>
+ *     <li>provide the total cost of all orders</li>
+ *     <li>aggregate on order_item_id</li>
+ *        <ul>
+ *            <li>sum the quantity of all items with the same order_item_id</li>
+ *        </ul>
+ *     </li>
+ * </ul>
+ * Aggregating on the field would turn up an the following aggregation:
+ * <pre>
+ * aggregation:
+ *    - name: query_result
+ *    - aggregationResults: [5] - where 5 represents the total cost of all orders
+ *    - nestedAggregations: [
+ *        - name: order_item_id
+ *        - aggregationBuckets: [
+ *            - value: order_item_1
+ *                - aggregationResults: [10] - where 10 represents the sum of the quantities for order_item_1
+ *            - value: order_item_2
+ *                - aggregationResults: [5] - where 5 represents the sum of the quantities for order_item_2
+ *            ...
+ *        ]
+ * </pre>
+ * <p/>
  * If {@link Aggregate} instances are attached to the
  * {@link uk.ac.ebi.quickgo.rest.search.query.QueryRequest}, then the underlying data source will always return
  * aggregate specific data.
