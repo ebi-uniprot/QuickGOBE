b8e4cb404fcca972b44515df376bf328277a0fbf
Edward Turner <eddturner@ebi.ac.uk>
2016-08-24 08:08:31 +0100

    separate descendants and slimming filter converters

--- annotation-rest/src/test/java/uk/ac/ebi/quickgo/annotation/service/comm/rest/ontology/descendants/SlimmingFilterConverterTest.java
+++ annotation-rest/src/test/java/uk/ac/ebi/quickgo/annotation/service/comm/rest/ontology/descendants/SlimmingFilterConverterTest.java
@@ -1,18 +1,22 @@
 package uk.ac.ebi.quickgo.annotation.service.comm.rest.ontology.descendants;
 
 import uk.ac.ebi.quickgo.annotation.common.document.AnnotationFields;
-import uk.ac.ebi.quickgo.annotation.service.comm.rest.ontology.converter.DescendantsFilterConverter;
+import uk.ac.ebi.quickgo.annotation.service.comm.rest.ontology.converter.SlimmingConversionInfo;
+import uk.ac.ebi.quickgo.annotation.service.comm.rest.ontology.converter.SlimmingFilterConverter;
 import uk.ac.ebi.quickgo.annotation.service.comm.rest.ontology.model.ConvertedOntologyFilter;
 import uk.ac.ebi.quickgo.rest.search.query.QuickGOQuery;
 import uk.ac.ebi.quickgo.rest.search.request.converter.ConvertedFilter;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
 import org.junit.Before;
 import org.junit.Test;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsMapContaining.hasEntry;
 import static org.hamcrest.core.Is.is;
 import static uk.ac.ebi.quickgo.rest.search.query.QuickGOQuery.not;
 import static uk.ac.ebi.quickgo.rest.search.query.QuickGOQuery.or;
@@ -21,15 +25,15 @@ import static uk.ac.ebi.quickgo.rest.search.query.QuickGOQuery.or;
  * Created 10/08/16
  * @author Edd
  */
-public class DescendantsFilterConverterTest {
+public class SlimmingFilterConverterTest {
     private ConvertedOntologyFilter response;
-    private DescendantsFilterConverter converter;
+    private SlimmingFilterConverter converter;
 
     @Before
     public void setUp() {
         response = new ConvertedOntologyFilter();
         response.setResults(new ArrayList<>());
-        converter = new DescendantsFilterConverter();
+        converter = new SlimmingFilterConverter();
     }
 
     @Test
@@ -41,7 +45,7 @@ public class DescendantsFilterConverterTest {
         ConvertedFilter<QuickGOQuery> convertedFilter = converter.transform(response);
 
         assertThat(convertedFilter.getConvertedValue(), is(QuickGOQuery.createQuery(AnnotationFields.GO_ID, desc1)));
-        assertThat(convertedFilter.getFilterContext(), is(Optional.empty()));
+        assertThat(convertedFilter.getFilterContext().isPresent(), is(true));
     }
 
     @Test
@@ -59,7 +63,7 @@ public class DescendantsFilterConverterTest {
         assertThat(convertedFilter.getConvertedValue(), is(
                 or(QuickGOQuery.createQuery(AnnotationFields.GO_ID, desc1),
                         QuickGOQuery.createQuery(AnnotationFields.GO_ID, desc2))));
-        assertThat(convertedFilter.getFilterContext(), is(Optional.empty()));
+        assertThat(convertedFilter.getFilterContext().isPresent(), is(true));
     }
 
     @Test
@@ -75,7 +79,7 @@ public class DescendantsFilterConverterTest {
 
         assertThat(convertedFilter.getConvertedValue(), is(
                 QuickGOQuery.createQuery(AnnotationFields.GO_ID, desc1)));
-        assertThat(convertedFilter.getFilterContext(), is(Optional.empty()));
+        assertThat(convertedFilter.getFilterContext().isPresent(), is(true));
     }
 
     @Test
@@ -84,7 +88,6 @@ public class DescendantsFilterConverterTest {
         ConvertedFilter<QuickGOQuery> convertedFilter = converter.transform(response);
 
         assertThat(convertedFilter.getConvertedValue(), is(not(QuickGOQuery.createAllQuery())));
-        assertThat(convertedFilter.getFilterContext(), is(Optional.empty()));
     }
 
     @Test
@@ -92,7 +95,45 @@ public class DescendantsFilterConverterTest {
         ConvertedFilter<QuickGOQuery> convertedFilter = converter.transform(response);
 
         assertThat(convertedFilter.getConvertedValue(), is(not(QuickGOQuery.createAllQuery())));
-        assertThat(convertedFilter.getFilterContext(), is(Optional.empty()));
+    }
+
+    @Test
+    public void conversionContextContainsOneMapping() {
+        String id1 = "id1";
+        String desc1 = "desc1";
+
+        addResponseDescendant(id1, desc1);
+        ConvertedFilter<QuickGOQuery> convertedFilter = converter.transform(response);
+
+        assertThat(extractContextProperties(convertedFilter), hasEntry(desc1, singletonList(id1)));
+    }
+
+    @Test
+    public void conversionContextContainsTwoMappings() {
+        String id1 = "id1";
+        String id2 = "id2";
+        String desc1 = "desc1";
+        String desc2 = "desc2";
+        String desc3 = "desc3";
+
+        addResponseDescendant(id1, desc1);
+        addResponseDescendant(id1, desc2);
+        addResponseDescendant(id1, desc3);
+        addResponseDescendant(id2, desc3);
+        ConvertedFilter<QuickGOQuery> convertedFilter = converter.transform(response);
+
+        assertThat(extractContextProperties(convertedFilter), hasEntry(desc1, singletonList(id1)));
+        assertThat(extractContextProperties(convertedFilter), hasEntry(desc2, singletonList(id1)));
+        assertThat(extractContextProperties(convertedFilter), hasEntry(desc3, asList(id1, id2)));
+    }
+
+    private Map<String, List<String>> extractContextProperties(ConvertedFilter<QuickGOQuery> convertedFilter) {
+        SlimmingConversionInfo conversionInfo = convertedFilter.getFilterContext()
+                .map(t -> t.get(SlimmingConversionInfo.class)
+                        .orElse(new SlimmingConversionInfo()))
+                .orElseThrow(IllegalStateException::new);
+
+        return conversionInfo.getInfo();
     }
 
     private void addResponseDescendant(String termId, String descendantId) {
