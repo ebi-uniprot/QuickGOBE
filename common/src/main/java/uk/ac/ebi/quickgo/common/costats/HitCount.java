2e226054b5178fa45073a1d8b8fc46e9cd3d332e
twardell <twardell@ebi.ac.uk>
2016-09-07 10:55:59 +0100

    Add co-occurrence stats from old code.

---
+++ common/src/main/java/uk/ac/ebi/quickgo/common/costats/HitCount.java
@@ -0,0 +1,19 @@
+package uk.ac.ebi.quickgo.common.costats;
+
+/**
+ * @author Tony Wardell
+ * Date: 20/07/2016
+ * Time: 15:35
+ * Created with IntelliJ IDEA.
+ */
+public class HitCount {
+
+    public long hits;
+
+    public HitCount(int i) {
+        hits = i;
+    }
+
+    public HitCount() {
+    }
+}
