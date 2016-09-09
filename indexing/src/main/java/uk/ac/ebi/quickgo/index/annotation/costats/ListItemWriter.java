9270e268bf90dd301555e613dc587831b35f2af8
twardell <twardell@ebi.ac.uk>
2016-09-09 17:37:00 +0100

    New class, wrapper for ItemWriter that can deal with being passed a list (of lists) from the ItemProcessor.

---
+++ indexing/src/main/java/uk/ac/ebi/quickgo/index/annotation/costats/ListItemWriter.java
@@ -0,0 +1,28 @@
+package uk.ac.ebi.quickgo.index.annotation.costats;
+
+
+import java.util.List;
+import org.springframework.batch.item.ItemWriter;
+
+/**
+ * Custom Spring Batch writer can process a list produced by an item processor.
+ *
+ * @author Tony Wardell
+ * Date: 09/09/2016
+ * Time: 16:47
+ * Created with IntelliJ IDEA.
+ */
+public class ListItemWriter<T> implements ItemWriter<List<T>>{
+
+    private ItemWriter<T> wrapped;
+
+    public ListItemWriter(ItemWriter<T> coOccurringTermItemWriter) {
+        wrapped = coOccurringTermItemWriter;
+    }
+
+    @Override public void write(List<? extends List<T>> list) throws Exception {
+        for (List<T> subList : list) {
+            wrapped.write(subList);
+        }
+    }
+}
