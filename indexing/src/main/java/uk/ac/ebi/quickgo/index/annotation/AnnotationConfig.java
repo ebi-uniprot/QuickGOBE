a19a6143fab1c65e2206899709956c786eaa8fef
twardell <twardell@ebi.ac.uk>
2016-09-09 14:29:25 +0100

    Add configuration for co-occurrence stats.

--- indexing/src/main/java/uk/ac/ebi/quickgo/index/annotation/AnnotationConfig.java
+++ indexing/src/main/java/uk/ac/ebi/quickgo/index/annotation/AnnotationConfig.java
@@ -4,6 +4,8 @@ import uk.ac.ebi.quickgo.annotation.common.AnnotationRepoConfig;
 import uk.ac.ebi.quickgo.annotation.common.AnnotationRepository;
 import uk.ac.ebi.quickgo.annotation.common.document.AnnotationDocument;
 import uk.ac.ebi.quickgo.common.QuickGODocument;
+import uk.ac.ebi.quickgo.common.costats.CoOccurrenceStat;
+import uk.ac.ebi.quickgo.index.annotation.costats.*;
 import uk.ac.ebi.quickgo.index.common.SolrServerWriter;
 import uk.ac.ebi.quickgo.index.common.listener.ItemRateWriterListener;
 import uk.ac.ebi.quickgo.index.common.listener.LogJobListener;
@@ -17,12 +19,8 @@ import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.FlatFileParseException;
-import org.springframework.batch.item.file.LineMapper;
-import org.springframework.batch.item.file.MultiResourceItemReader;
+import org.springframework.batch.item.*;
+import org.springframework.batch.item.file.*;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.mapping.FieldSetMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
@@ -39,6 +37,8 @@ import org.springframework.context.annotation.Import;
 import org.springframework.core.io.Resource;
 import org.springframework.data.solr.core.SolrTemplate;
 
+import static uk.ac.ebi.quickgo.index.annotation.costats.CoStatsConfiguration.COSTATS_ALL_COMPLETION_STEP_NAME;
+import static uk.ac.ebi.quickgo.index.annotation.costats.CoStatsConfiguration.COSTATS_MANUAL_COMPLETION_STEP_NAME;
 import static uk.ac.ebi.quickgo.index.common.datafile.GOADataFileParsingHelper.TAB;
 
 /**
@@ -49,7 +49,7 @@ import static uk.ac.ebi.quickgo.index.common.datafile.GOADataFileParsingHelper.T
  */
 @Configuration
 @EnableBatchProcessing
-@Import({AnnotationRepoConfig.class})
+@Import({AnnotationRepoConfig.class, CoStatsConfiguration.class})
 public class AnnotationConfig {
     static final String ANNOTATION_INDEXING_JOB_NAME = "annotationIndexingJob";
     static final String ANNOTATION_INDEXING_STEP_NAME = "annotationIndexStep";
@@ -78,10 +78,36 @@ public class AnnotationConfig {
     @Autowired
     private StepBuilderFactory stepBuilders;
 
+    @Autowired
+    ItemProcessor<Annotation, Annotation> coStatsManual;
+
+    @Autowired
+    ItemProcessor<Annotation, Annotation> coStatsAll;
+
+    @Autowired
+    ItemProcessor<String, List<CoOccurrenceStat>> coStatsManualItemProcessor;
+
+    @Autowired
+    ItemProcessor<String, List<CoOccurrenceStat>> coStatsAllItemProcessor;
+
+    @Autowired
+    ItemReader<String> coStatsManualItemReader;
+
+    @Autowired
+    ItemWriter<List<CoOccurrenceStat>> coStatManualFlatFileWriter;
+
+    @Autowired
+    ItemReader<String> coStatsAllItemReader;
+
+    @Autowired
+    ItemWriter<List<CoOccurrenceStat>> coStatsAllFlatFileWriter;
+
     @Bean
     public Job annotationJob() {
         return jobBuilders.get(ANNOTATION_INDEXING_JOB_NAME)
                 .start(annotationIndexingStep())
+                .start(coStatsManualSummarizationStep())
+                .start(coStatsAllSummarizationStep())
                 .listener(logJobListener())
                 // commit the documents to the solr server
                 .listener(new JobExecutionListener() {
@@ -105,6 +131,30 @@ public class AnnotationConfig {
                 .processor(annotationCompositeProcessor())
                 .<AnnotationDocument>writer(annotationSolrServerWriter())
                 .listener(logWriteRateListener())
+                .listener(logStepListener())
+                .listener(skipLogListener())
+                .build();
+    }
+
+
+    @Bean
+    public Step coStatsManualSummarizationStep() {
+        return stepBuilders.get(COSTATS_MANUAL_COMPLETION_STEP_NAME)
+                .<String, List<CoOccurrenceStat>>chunk(chunkSize)
+                .<Annotation>reader(coStatsManualItemReader)
+                .processor(coStatsManualItemProcessor)
+                .<List<CoOccurrenceStat>>writer(coStatManualFlatFileWriter)
+                .listener(logStepListener())
+                .build();
+    }
+
+    @Bean
+    public Step coStatsAllSummarizationStep() {
+        return stepBuilders.get(COSTATS_ALL_COMPLETION_STEP_NAME)
+                .<String, List<CoOccurrenceStat>>chunk(chunkSize)
+                .<Annotation>reader(coStatsAllItemReader)
+                .processor(coStatsAllItemProcessor)
+                .<List<CoOccurrenceStat>>writer(coStatsAllFlatFileWriter)
                 .listener(logStepListener())
                 .listener(skipLogListener())
                 .build();
@@ -177,6 +227,8 @@ public class AnnotationConfig {
     ItemProcessor<Annotation, AnnotationDocument> annotationCompositeProcessor() {
         List<ItemProcessor<Annotation, ?>> processors = new ArrayList<>();
         processors.add(annotationValidator());
+        processors.add(coStatsManual);
+        processors.add(coStatsAll);
         processors.add(annotationDocConverter());
 
         CompositeItemProcessor<Annotation, AnnotationDocument> compositeProcessor = new CompositeItemProcessor<>();
