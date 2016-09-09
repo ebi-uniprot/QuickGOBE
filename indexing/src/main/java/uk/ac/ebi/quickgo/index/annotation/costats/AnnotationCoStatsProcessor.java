aa538f0396129d7062f3cf8ab3721d94db414b5e
twardell <twardell@ebi.ac.uk>
2016-09-09 14:30:28 +0100

    Rename class; methods and variables.

---
+++ indexing/src/main/java/uk/ac/ebi/quickgo/index/annotation/costats/AnnotationCoStatsProcessor.java
@@ -0,0 +1,42 @@
+package uk.ac.ebi.quickgo.index.annotation.costats;
+
+
+import uk.ac.ebi.quickgo.index.annotation.Annotation;
+import uk.ac.ebi.quickgo.index.annotation.costats.CoStatsPermutations;
+
+import java.nio.file.Path;
+import java.util.function.Predicate;
+import org.springframework.batch.item.ItemProcessor;
+
+/**
+ * @author Tony Wardell
+ * Date: 06/09/2016
+ * Time: 16:30
+ * Created with IntelliJ IDEA.
+ *
+ * A version of ItemProcessor for adding details from an {@link uk.ac.ebi.quickgo.index.annotation.Annotation}
+ * to a matrix of permutations where GOTerm A and GOTerm B both annotate the same Gene Product.
+ *
+ * Version of GPAFileToSummary from Beta
+ */
+public class AnnotationCoStatsProcessor implements ItemProcessor<Annotation, Annotation> {
+
+    private final CoStatsPermutations coStatsPermutations;
+    private final Predicate<Annotation> toBeProcessed;
+
+    public AnnotationCoStatsProcessor(CoStatsPermutations coStatsPermutations,
+            Predicate<Annotation> toBeProcessed) {
+        this.coStatsPermutations = coStatsPermutations;
+        this.toBeProcessed = toBeProcessed;
+    }
+
+    @Override
+    public Annotation process(Annotation annotation) throws Exception {
+
+        if(toBeProcessed.test(annotation)){
+            coStatsPermutations.addRowToMatrix(annotation);
+        }
+
+        return annotation;
+    }
+}
