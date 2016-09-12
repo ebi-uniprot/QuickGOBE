b1c58a237e3d89ffeba7a8523722433cc1aa9a99
twardell <twardell@ebi.ac.uk>
2016-09-12 16:40:41 +0100

    New class to carry out final batch updates to CoTerms data 'bucket' when the end of the annotations have been read.

---
+++ indexing/src/main/java/uk/ac/ebi/quickgo/index/annotation/coterms/CoTermsStepExecutionListener.java
@@ -0,0 +1,33 @@
+package uk.ac.ebi.quickgo.index.annotation.coterms;
+
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.StepExecutionListener;
+
+/**
+ * @author Tony Wardell
+ * Date: 12/09/2016
+ * Time: 15:01
+ * Created with IntelliJ IDEA.
+ */
+public class CoTermsStepExecutionListener implements StepExecutionListener {
+
+    private CoStatsPermutations all;
+    CoStatsPermutations manual;
+
+    public CoTermsStepExecutionListener(CoStatsPermutations all,
+            CoStatsPermutations manual) {
+        this.all = all;
+        this.manual = manual;
+    }
+
+    @Override public void beforeStep(StepExecution stepExecution) {
+
+    }
+
+    @Override public ExitStatus afterStep(StepExecution stepExecution) {
+        all.finish();
+        manual.finish();
+        return ExitStatus.COMPLETED;
+    }
+}
