e143fd1e7a89001466b23593ca30567344173a0e
twardell <twardell@ebi.ac.uk>
2016-09-13 16:35:51 +0100

    New. Output the column heading names.

---
+++ indexing/src/main/java/uk/ac/ebi/quickgo/index/annotation/coterms/CoTermsFlatFileHeaderCallBack.java
@@ -0,0 +1,23 @@
+package uk.ac.ebi.quickgo.index.annotation.coterms;
+
+import java.io.IOException;
+import java.io.Writer;
+import org.springframework.batch.item.file.FlatFileHeaderCallback;
+
+/**
+ * @author Tony Wardell
+ * Date: 13/09/2016
+ * Time: 16:24
+ * Created with IntelliJ IDEA.
+ */
+public class CoTermsFlatFileHeaderCallBack implements FlatFileHeaderCallback {
+
+    private static final String COL_HEADER = "Source Term|Compared Term|Probability Ratio|Significance Ratio|Together" +
+            " (Overlap)|Compared count";
+
+
+    @Override public void writeHeader(Writer writer) throws IOException {
+        writer.write(COL_HEADER);
+
+    }
+}
