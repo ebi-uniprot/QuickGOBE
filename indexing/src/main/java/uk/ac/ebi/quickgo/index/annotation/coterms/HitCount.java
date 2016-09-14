540a714e6582f8783e08d8fb1d83034c2ca4dfce
twardell <twardell@ebi.ac.uk>
2016-09-14 11:04:37 +0100

    Add javadoc.

--- indexing/src/main/java/uk/ac/ebi/quickgo/index/annotation/coterms/HitCount.java
+++ indexing/src/main/java/uk/ac/ebi/quickgo/index/annotation/coterms/HitCount.java
@@ -1,6 +1,8 @@
 package uk.ac.ebi.quickgo.index.annotation.coterms;
 
 /**
+ * A value object for the number of occurrences of a 'thing'.
+ *
  * @author Tony Wardell
  * Date: 20/07/2016
  * Time: 15:35
