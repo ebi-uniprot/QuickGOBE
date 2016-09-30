ff2ba7cfda608d63144f0c0de9420e4e9ac9534e
twardell <twardell@ebi.ac.uk>
2016-09-30 15:11:31 +0100

    Define the types of co-occurring by source - manual (not electronic) or all (every source).

---
+++ ontology-common/src/main/java/uk/ac/ebi/quickgo/ontology/common/coterm/CoTermType.java
@@ -0,0 +1,13 @@
+package uk.ac.ebi.quickgo.ontology.common.coterm;
+
+/**
+ * @author Tony Wardell
+ * Date: 29/09/2016
+ * Time: 15:49
+ * Created with IntelliJ IDEA.
+ */
+public enum CoTermType {
+
+    ALL,
+    MANUAL
+}
