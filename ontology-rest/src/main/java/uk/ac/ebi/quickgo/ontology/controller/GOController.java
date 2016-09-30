f0fc39d35254c39bf76ffa5a98d8e294c4c2bd5d
twardell <twardell@ebi.ac.uk>
2016-09-30 15:03:54 +0100

    Add co-occurring terms lookup.

--- ontology-rest/src/main/java/uk/ac/ebi/quickgo/ontology/controller/GOController.java
+++ ontology-rest/src/main/java/uk/ac/ebi/quickgo/ontology/controller/GOController.java
@@ -1,18 +1,26 @@
 package uk.ac.ebi.quickgo.ontology.controller;
 
-import uk.ac.ebi.quickgo.rest.search.SearchService;
-import uk.ac.ebi.quickgo.rest.search.SearchableField;
+import uk.ac.ebi.quickgo.ontology.common.coterm.CoTerm;
+import uk.ac.ebi.quickgo.ontology.common.coterm.CoTermLimit;
+import uk.ac.ebi.quickgo.ontology.common.coterm.CoTermType;
 import uk.ac.ebi.quickgo.ontology.common.document.OntologyType;
 import uk.ac.ebi.quickgo.ontology.model.GOTerm;
 import uk.ac.ebi.quickgo.ontology.model.OBOTerm;
 import uk.ac.ebi.quickgo.ontology.service.OntologyService;
 import uk.ac.ebi.quickgo.ontology.service.search.SearchServiceConfig;
+import uk.ac.ebi.quickgo.rest.search.SearchService;
+import uk.ac.ebi.quickgo.rest.search.SearchableField;
+import uk.ac.ebi.quickgo.rest.search.results.QueryResult;
 
+import com.google.common.base.Preconditions;
+import io.swagger.annotations.ApiOperation;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * REST controller for accessing GO related information.
@@ -28,6 +36,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class GOController extends OBOController<GOTerm> {
 
     private static final Pattern GO_ID_FORMAT = Pattern.compile("^GO:[0-9]{7}$");
+    private static final String COTERMS_RESOURCE = "coterms";
+
+    private CoTermLimit coTermLimit;
+
+    @Value("${coterm.default.limit:50}")
+    private int defaultLimit;
 
     @Autowired
     public GOController(OntologyService<GOTerm> goOntologyService,
@@ -35,7 +49,42 @@ public class GOController extends OBOController<GOTerm> {
             SearchableField searchableField,
             SearchServiceConfig.OntologyCompositeRetrievalConfig ontologyRetrievalConfig) {
         super(goOntologyService, ontologySearchService, searchableField, ontologyRetrievalConfig);
+
+        coTermLimit = new CoTermLimit(defaultLimit);
     }
+
+
+    /**
+     * Get co-occurring term information about a list of terms in comma-separated-value (CSV) format
+     *
+     * @param id ontology identifier
+     * @param type whether manual only or all
+     * @param limit Optional:  requires a String so the user can specify 'ALL' or a number.
+     * @param similarityThreshold requires a String so the user can specify 'ALL' or a number
+     * @return
+     * <ul>
+     *     <li>all ids are valid: response consists of a 200 with the chosen information about the ontology terms</li>
+     *     <li>any id is not found: response returns 404</li>
+     *     <li>any id is of the an invalid format: response returns 400</li>
+     * </ul>
+     */
+    @ApiOperation(value = "Get co-occurring term information about a (CSV) list of terms based on their ids",
+            notes = "If possible, response fields include: id, name, definition, probability ratio, similarity ratio," +
+                    " together, compared.")
+    @RequestMapping(value = TERMS_RESOURCE + "/{id}/" + COTERMS_RESOURCE, method = RequestMethod.GET,
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<QueryResult<CoTerm>> findCoTerms(@PathVariable(value = "id") String id,
+            @RequestParam(value = "type", defaultValue = "ALL") String type,
+            @RequestParam(value = "limit") String limit,
+            @RequestParam(value = "similarityThreshold") int similarityThreshold) {
+
+        Preconditions.checkArgument(similarityThreshold <0 || similarityThreshold>100, "The value for " +
+                "similarityThreshold should be an integer between 0 and 100, not {}", similarityThreshold );
+
+        return getResultsResponse(ontologyService.findCoTermsByOntologyId(id, CoTermType.valueOf(type),
+                coTermLimit.workoutLimit(limit), similarityThreshold));
+    }
+
 
     @Override
     public Predicate<String> idValidator() {
