633a07167ae15b514c07211eff606414d59eb293
twardell <twardell@ebi.ac.uk>
2016-09-30 14:26:16 +0100

    Entity for co-occurring terms.

---
+++ ontology-common/src/main/java/uk/ac/ebi/quickgo/ontology/common/coterm/CoTerm.java
@@ -0,0 +1,52 @@
+package uk.ac.ebi.quickgo.ontology.common.coterm;
+
+/**
+ * @author Tony Wardell
+ * Date: 29/09/2016
+ * Time: 11:30
+ * Created with IntelliJ IDEA.
+ */
+public class CoTerm {
+
+    private String id;
+    private String compare;
+    private float probabilityRatio;
+    private float significance;
+    private long together;
+    private long compared;
+
+    public CoTerm(String id, String compare, float probabilityRatio, float significance, long together,
+            long compared) {
+        this.id = id;
+        this.compare = compare;
+        this.probabilityRatio = probabilityRatio;
+        this.significance = significance;
+        this.together = together;
+        this.compared = compared;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getCompare() {
+        return compare;
+    }
+
+    public float getProbabilityRatio() {
+        return probabilityRatio;
+    }
+
+    public float getSignificance() {
+        return significance;
+    }
+
+    public long getTogether() {
+        return together;
+    }
+
+    public long getCompared() {
+        return compared;
+    }
+
+}
