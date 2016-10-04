ad41deb5c37908d06c978449857c103df615c344
twardell <twardell@ebi.ac.uk>
2016-10-04 15:45:33 +0100

    Change javadoc.  Add defensive preconditions.

--- indexing/src/main/java/uk/ac/ebi/quickgo/index/annotation/coterms/CoTermItemReader.java
+++ indexing/src/main/java/uk/ac/ebi/quickgo/index/annotation/coterms/CoTermItemReader.java
@@ -2,13 +2,14 @@ package uk.ac.ebi.quickgo.index.annotation.coterms;
 
 import uk.ac.ebi.quickgo.annotation.common.document.AnnotationDocument;
 
+import com.google.common.base.Preconditions;
 import java.util.Iterator;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 
 /**
- * Provide a list of all GO Terms for which co-occurrence has been determined ( which is all of them that have been
- * processed, since at worst a GO Term will have a statistic for co-occurring with itself).
+ * Iterates (reads) through the list of all GO terms which have a co-occurring go term. Note that, by definition,
+ * each GO term co-occurs with itself.
  *
  * @author Tony Wardell
  * Date: 07/09/2016
@@ -20,12 +21,21 @@ class CoTermItemReader implements ItemReader<String> {
     private final CoTermsAggregator aggregator;
     private Iterator<String> termsIt;
 
-    public CoTermItemReader(ItemWriter<AnnotationDocument> annotationCoOccurringTermsAggregator) {
-        this.aggregator = (CoTermsAggregator)annotationCoOccurringTermsAggregator;
+    public CoTermItemReader(ItemWriter<AnnotationDocument> aggregator) {
+        this.aggregator = (CoTermsAggregator)aggregator;
+        Preconditions.checkArgument(aggregator!=null, "An instance of CoTermItemReader has been passed a null " +
+                "ItemWriter<AnnotationDocument> to it's constructor which is illegal");
 
     }
 
+    /**
+     * Provide the next GO Term id from the list of all co-occurring GO Terms.
+     * @return a single GO Term id
+     * @throws Exception
+     */
     @Override public String read() throws Exception {
+
+        Preconditions.checkArgument(aggregator!=null, "CoTermsAggregator instance is null");
 
         //Delay providing full list until aggregator has fully processed all records.
         if (termsIt == null) {
