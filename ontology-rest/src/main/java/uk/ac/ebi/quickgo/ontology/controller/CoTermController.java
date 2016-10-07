5b021ea18abf40569be6e2b8e72b2d9bf13b348a
twardell <twardell@ebi.ac.uk>
2016-10-07 15:01:05 +0100

    Change some parameter names, modify validation.

--- ontology-rest/src/main/java/uk/ac/ebi/quickgo/ontology/controller/CoTermController.java
+++ ontology-rest/src/main/java/uk/ac/ebi/quickgo/ontology/controller/CoTermController.java
@@ -1,21 +1,25 @@
 package uk.ac.ebi.quickgo.ontology.controller;
 
 import uk.ac.ebi.quickgo.ontology.common.coterm.CoTerm;
-import uk.ac.ebi.quickgo.ontology.common.coterm.CoTermType;
-import uk.ac.ebi.quickgo.ontology.common.document.OntologyType;
+import uk.ac.ebi.quickgo.ontology.common.coterm.CoTermLimit;
+import uk.ac.ebi.quickgo.ontology.common.coterm.CoTermSource;
 import uk.ac.ebi.quickgo.ontology.model.GOTerm;
 import uk.ac.ebi.quickgo.ontology.service.OntologyService;
 import uk.ac.ebi.quickgo.rest.search.results.QueryResult;
 
 import com.google.common.base.Preconditions;
 import io.swagger.annotations.ApiOperation;
+import java.util.Collections;
+import java.util.List;
 import java.util.function.Predicate;
-import java.util.regex.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static uk.ac.ebi.quickgo.ontology.controller.GOController.GO_ID_FORMAT;
 
 /**
  * REST controller for accessing GO related information.
@@ -28,18 +32,25 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping(value = "/QuickGO/services/go")
-public class CotermController {
-    private static final Pattern GO_ID_FORMAT = Pattern.compile("^GO:[0-9]{7}$");
+public class CoTermController {
+
     private static final String COTERMS_RESOURCE = "coterms";
+    private final CoTermLimit coTermLimit;
 
     @Value("${coterm.default.limit:50}")
     private int defaultLimit;
 
     final OntologyService<GOTerm> ontologyService;
 
+    /**
+     * Create the endpoint for Co Terms.
+     * @param goOntologyService Service layer class consolidates ontology functionality
+     * @param coTermLimit provides the correct limit for the number of co terms to return.
+     */
     @Autowired
-    public CotermController(OntologyService<GOTerm> goOntologyService) {
+    public CoTermController(OntologyService<GOTerm> goOntologyService, CoTermLimit coTermLimit) {
         this.ontologyService = goOntologyService;
+        this.coTermLimit = coTermLimit;
     }
 
 
@@ -47,7 +58,7 @@ public class CotermController {
      * Get co-occurring term information for a single GO Term
      *
      * @param id ontology identifier
-     * @param type whether manual only or all
+     * @param source whether manual only or all
      * @param limit Optional: requires a String so the user can specify 'ALL' or a number.
      * @param similarityThreshold Optional: either not provided or a integer between 0 and 100.
      * @return
@@ -64,12 +75,39 @@ public class CotermController {
     @RequestMapping(value = COTERMS_RESOURCE + "/{id}/", method = RequestMethod.GET,
             produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<QueryResult<CoTerm>> findCoTerms(@PathVariable(value = "id") String id,
-            @RequestParam(value = "type", defaultValue = "ALL") String type,
+            @RequestParam(value = "source", defaultValue = "ALL") String source,
             @RequestParam(value = "limit") String limit,
             @RequestParam(value = "similarityThreshold") String similarityThreshold) {
 
-        int similarityNumeric = 0;
+        int similarityNumeric = validateSimilarity(similarityThreshold);
+        CoTermSource coTermSource = validateCoTermSource(source, similarityThreshold);
+        validateGoTerm(id);
 
+        return getResultsResponse(ontologyService.findCoTermsByOntologyId(id, coTermSource,
+                coTermLimit.workoutLimit(limit), similarityNumeric));
+    }
+
+    private void validateGoTerm(@PathVariable(value = "id") String id) {
+        if(!idValidator().test(id)){
+            String errorMessage = "Provided ID: '" + id + "' is invalid";
+            throw new IllegalArgumentException(errorMessage);
+        }
+    }
+
+    private CoTermSource validateCoTermSource(@RequestParam(value = "source", defaultValue = "ALL") String source,
+            @RequestParam(value = "similarityThreshold") String similarityThreshold) {
+        CoTermSource coTermSource = null;
+        try {
+            coTermSource = CoTermSource.valueOf(source);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("The value for similarityThreshold should be an integer between 0 " +
+                    "and 100, not " + similarityThreshold);
+        }
+        return coTermSource;
+    }
+
+    private int validateSimilarity(@RequestParam(value = "similarityThreshold") String similarityThreshold) {
+        int similarityNumeric = 0;
         if(similarityThreshold!=null) {
             try {
                 similarityNumeric = Integer.parseInt(similarityThreshold);
@@ -80,18 +118,28 @@ public class CotermController {
             Preconditions.checkArgument(similarityNumeric < 0 || similarityNumeric > 100, "The value for " +
                     "similarityThreshold should be an integer between 0 and 100, not {}", similarityThreshold);
         }
-
-        return getResultsResponse(ontologyService.findCoTermsByOntologyId(id, CoTermType.valueOf(type),
-                coTermLimit.workoutLimit(limit), similarityThreshold));
+        return similarityNumeric;
     }
 
-
-    @Override
     public Predicate<String> idValidator() {
         return id -> GO_ID_FORMAT.matcher(id).matches();
     }
 
-    @Override protected OntologyType getOntologyType() {
-        return OntologyType.GO;
+    /**
+     * Creates a {@link ResponseEntity} containing a {@link QueryResult} for a list of results.
+     *
+     * @param results a list of results
+     * @return a {@link ResponseEntity} containing a {@link QueryResult} for a list of results
+     */
+    <ResponseType> ResponseEntity<QueryResult<ResponseType>> getResultsResponse(List<ResponseType> results) {
+        List<ResponseType> resultsToShow;
+        if (results == null) {
+            resultsToShow = Collections.emptyList();
+        } else {
+            resultsToShow = results;
+        }
+
+        QueryResult<ResponseType> queryResult = new QueryResult.Builder<>(resultsToShow.size(), resultsToShow).build();
+        return new ResponseEntity<>(queryResult, HttpStatus.OK);
     }
 }
