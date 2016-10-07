3ffb279749801c1d3eb7a35491e7c2131474d18f
twardell <twardell@ebi.ac.uk>
2016-10-07 16:21:56 +0100

    Add javadoc, change return code a little.

--- ontology-common/src/main/java/uk/ac/ebi/quickgo/ontology/common/coterm/CoTermRepositorySimpleMap.java
+++ ontology-common/src/main/java/uk/ac/ebi/quickgo/ontology/common/coterm/CoTermRepositorySimpleMap.java
@@ -1,16 +1,14 @@
 package uk.ac.ebi.quickgo.ontology.common.coterm;
 
-import com.google.common.base.Preconditions;
-import java.io.PrintStream;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
+ * Retrieve the co-occurring terms for the selected term from the in-memory map.
+ *
  * @author Tony Wardell
  * Date: 29/09/2016
  * Time: 13:48
@@ -19,69 +17,51 @@ import org.springframework.stereotype.Component;
 @Component
 public class CoTermRepositorySimpleMap  implements CoTermRepository{
 
-    private static final Logger logger = LoggerFactory.getLogger(CoTermRepositorySimpleMap.class);
     private Map<String, List<CoTerm>> coTermsAll;
     private Map<String, List<CoTerm>> coTermsManual;
 
-    public CoTermRepositorySimpleMap(Map<String, List<CoTerm>> costatsAll, Map<String, List<CoTerm>> costatsManual) {
-        this.coTermsAll = costatsAll;
-        this.coTermsManual = costatsManual;
+    public CoTermRepositorySimpleMap(Map<String, List<CoTerm>> coTermsAll, Map<String, List<CoTerm>> coTermsManual) {
+        this.coTermsAll = coTermsAll;
+        this.coTermsManual = coTermsManual;
     }
 
     /**
      * Get all co-occurring terms for the requested term up to the supplied limit
-     * @param id
-     * @param limit
-     * @return
+     * @param id the GO Term for which we will lookup co-occurring terms.
+     * @param limit Limit the number of co-occurring terms return to the limit specified.
+     * @return a list of objects, each one of which represent a GO Term that is used to annotate the same gene
+     * product as the id. Each object holds statistics related to that co-occurrence.
      */
-    public List<CoTerm> findCoTerms(String id, CoTermType type, int limit, int similarityThreshold) {
-        return type==CoTermType.MANUAL?findCoTermsFromMap(coTermsManual, id, limit, similarityThreshold)
+    public List<CoTerm> findCoTerms(String id, CoTermSource source, int limit, int similarityThreshold) {
+        return source == CoTermSource.MANUAL?findCoTermsFromMap(coTermsManual, id, limit, similarityThreshold)
                 :findCoTermsFromMap(coTermsAll, id, limit, similarityThreshold);
     }
 
 
     /**
      * Get all co-occurring terms for the requested term up to the supplied limit
-     * @param id
-     * @param limit
-     * @return
+     * @param id the GO Term for which we will lookup co-occurring terms.
+     * @param limit Limit the number of co-occurring terms return to the limit specified.
+     * @return a list of objects, each one of which represent a GO Term that is used to annotate the same gene
+     * product as the id. Each object holds statistics related to that co-occurrence.
      */
     private List<CoTerm> findCoTermsFromMap(Map<String, List<CoTerm>> map, String id, int limit, int
             similarityThreshold) {
-
         List<CoTerm> results = map.get(id);
-
         if(results==null){
-            return new ArrayList<>();
+            return Collections.emptyList();
         }
 
-        if(similarityThreshold >0){
+        //use similarity threshold to filter results if it has been specified.
+        if(similarityThreshold>0){
             results = results.stream()
                     .filter(t -> t.getSignificance() >= similarityThreshold)    //todo requires sorting too?
                     .collect(Collectors.toList());
         }
-
         if(results.size()<=limit){
             return results.subList(0,results.size()-1);
         }
-
         return results.subList(0, limit);
     }
 
-    /**
-     * Show some statistics for the Costats
-     * @param out
-     */
-    public void report(PrintStream out) {
-
-        out.println("Manual Report");
-        out.println("==============");
-        out.println("Number of Terms: " + coTermsManual.keySet().size());
-        out.println("");
-        out.println("ALL Report");
-        out.println("==========");
-        out.println("Number of Terms: " + coTermsAll.keySet().size());
-        out.println("");
-
-    }
 }
