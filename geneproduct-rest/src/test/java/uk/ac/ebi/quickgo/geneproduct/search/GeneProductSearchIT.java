adcbe8c4505748ef79b2fdc611df7f94cc01654b
rantunes <rantunes@ebi.ac.uk>
2016-10-07 11:24:54 +0100

    Modify code so that highlighting is supported.
    We found that the faceted fields were displaying analyzed terms, instead of the complete field value. I have now moved all search related fields to copy fields.

--- geneproduct-rest/src/test/java/uk/ac/ebi/quickgo/geneproduct/search/GeneProductSearchIT.java
+++ geneproduct-rest/src/test/java/uk/ac/ebi/quickgo/geneproduct/search/GeneProductSearchIT.java
@@ -141,7 +141,7 @@ public class GeneProductSearchIT extends SearchControllerSetup {
 
         saveToRepository(doc1, doc2, doc3);
 
-        checkValidFacetResponse("glycine", GeneProductFields.Searchable.NAME);
+        checkValidFacetResponse("glycine", GeneProductFields.Searchable.TYPE);
     }
 
     @Test
@@ -152,8 +152,8 @@ public class GeneProductSearchIT extends SearchControllerSetup {
 
         saveToRepository(doc1, doc2, doc3);
 
-        checkValidFacetResponse("glycine", GeneProductFields.Searchable.ID,
-                GeneProductFields.Searchable.NAME);
+        checkValidFacetResponse("glycine", GeneProductFields.Searchable.TYPE,
+                GeneProductFields.Searchable.TAXON_ID);
     }
 
     @Test
@@ -201,15 +201,15 @@ public class GeneProductSearchIT extends SearchControllerSetup {
     @Test
     public void requestWithAFilterQueryReturnsFilteredResponse() throws Exception {
         GeneProductDocument doc1 = createGeneProductDocWithName("A0A0F8CSS1", "glycine metabolic process 1");
-        doc1.symbol = "important";
+        doc1.taxonId = 1;
         GeneProductDocument doc2 = createGeneProductDocWithName("A0A0F8CSS2", "glycine metabolic process 2");
-        doc2.symbol = "important";
+        doc2.taxonId = 1;
         GeneProductDocument doc3 = createGeneProductDocWithName("A0A0F8CSS3", "glycine metabolic process 3");
-        doc3.symbol = "pointless";
+        doc3.taxonId = 2;
 
         saveToRepository(doc1, doc2, doc3);
 
-        String fq = buildFilterQuery(GeneProductFields.Searchable.SYMBOL, "important");
+        String fq = buildFilterQuery(GeneProductFields.Searchable.TAXON_ID, "1");
 
         checkValidFilterQueryResponse("metabolic", 2, fq);
     }
@@ -238,15 +238,15 @@ public class GeneProductSearchIT extends SearchControllerSetup {
     @Test
     public void requestWithFilterQueryThatDoesNotFilterOutAnyEntryReturnsAllResults() throws Exception {
         GeneProductDocument doc1 = createGeneProductDocWithName("A0A0F8CSS1", "glycine metabolic process 1");
-        doc1.symbol = "important";
+        doc1.taxonId = 1;
         GeneProductDocument doc2 = createGeneProductDocWithName("A0A0F8CSS2", "glycine metabolic process 2");
-        doc2.symbol = "important";
+        doc2.taxonId = 1;
         GeneProductDocument doc3 = createGeneProductDocWithName("A0A0F8CSS3", "glycine metabolic process 3");
-        doc3.symbol = "important";
+        doc3.taxonId = 1;
 
         saveToRepository(doc1, doc2, doc3);
 
-        String fq = buildFilterQuery(GeneProductFields.Searchable.SYMBOL, "important");
+        String fq = buildFilterQuery(GeneProductFields.Searchable.TAXON_ID, "1");
 
         checkValidFilterQueryResponse("glycine", 3, fq);
     }
@@ -254,15 +254,15 @@ public class GeneProductSearchIT extends SearchControllerSetup {
     @Test
     public void requestWith1ValidFilterQueryReturnsFilteredResponse() throws Exception {
         GeneProductDocument doc1 = createGeneProductDocWithName("A0A0F8CSS1", "glycine metabolic process 1");
-        doc1.symbol = "important";
+        doc1.taxonId = 1;
         GeneProductDocument doc2 = createGeneProductDocWithName("A0A0F8CSS2", "glycine metabolic process 2");
-        doc2.symbol = "pointless";
+        doc2.taxonId = 2;
         GeneProductDocument doc3 = createGeneProductDocWithName("A0A0F8CSS3", "glycine metabolic process 3");
-        doc3.symbol = "important";
+        doc3.taxonId = 1;
 
         saveToRepository(doc1, doc2, doc3);
 
-        checkValidFilterQueryResponse("process", 2, GeneProductFields.Searchable.SYMBOL + ":important")
+        checkValidFilterQueryResponse("process", 2, GeneProductFields.Searchable.TAXON_ID + ":1")
                 .andExpect(jsonPath("$.results[0].id").value("A0A0F8CSS1"))
                 .andExpect(jsonPath("$.results[1].id").value("A0A0F8CSS3"));
     }
