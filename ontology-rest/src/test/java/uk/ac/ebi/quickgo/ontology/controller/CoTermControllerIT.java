db32075503de83eb6e1c472094028abe2583d0b0
twardell <twardell@ebi.ac.uk>
2016-10-10 14:53:43 +0100

    Add test to ensure data is being picked up from manual source if requested.

--- ontology-rest/src/test/java/uk/ac/ebi/quickgo/ontology/controller/CoTermControllerIT.java
+++ ontology-rest/src/test/java/uk/ac/ebi/quickgo/ontology/controller/CoTermControllerIT.java
@@ -4,6 +4,7 @@ import uk.ac.ebi.quickgo.ontology.OntologyREST;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,6 +18,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -42,7 +44,9 @@ public class CoTermControllerIT {
 
     private MockMvc mockMvc;
 
-    private String GO_0000001 = "GO:0000001";
+    private static final String GO_0000001 = "GO:0000001";
+    private static final String GO_9000001 = "GO:9000001";
+    private static final String MANUAL_ONLY_TERM = "GO:8888881";
 
     @Before
     public void setup() {
@@ -57,11 +61,40 @@ public class CoTermControllerIT {
 
         expectFieldsInResults(response, Arrays.asList(GO_0000001))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.results.*", hasSize(11)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void nothingRetrievedIfTermDoesntExist() throws Exception {
+        ResultActions response = mockMvc.perform(get(buildPathToResource(GO_9000001)));
+        response.andDo(print())
+                .andExpect(jsonPath("$.results.*", hasSize(0)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void retrieveManualCoTermsInformationWhenRequested() throws Exception {
+        ResultActions response = mockMvc.perform(get(buildPathToResource(MANUAL_ONLY_TERM, "source=MANUAL")));
+
+        expectFieldsInResults(response, Arrays.asList(MANUAL_ONLY_TERM))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.results.*", hasSize(1)))
+                .andExpect(jsonPath("$.results.*.compare").value("GO:0004444"))
+                .andExpect(jsonPath("$.results.*.probabilityRatio").value(302.4))
+                .andExpect(jsonPath("$.results.*.significance").value(78.28))
+                .andExpect(jsonPath("$.results.*.together").value(1933))
+                .andExpect(jsonPath("$.results.*.compared").value(5219))
                 .andExpect(status().isOk());
     }
 
     private String buildPathToResource(String id) {
         return RESOURCE_URL + "/" + id;
+    }
+
+    private String buildPathToResource(String id, String... args) {
+        return RESOURCE_URL + "/" + id + Arrays.stream(args)
+                .collect(Collectors.joining("&","?",""));
     }
 
     protected ResultActions expectFieldsInResults(ResultActions result, List<String> ids) throws Exception {
