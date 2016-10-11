60d31bae1f80856b480c77b4db2a4ee81b939209
twardell <twardell@ebi.ac.uk>
2016-10-11 17:02:17 +0100

    Compare some actual data.

--- ontology-common/src/test/java/uk/ac/ebi/quickgo/ontology/common/coterms/CoTermLoaderIT.java
+++ ontology-common/src/test/java/uk/ac/ebi/quickgo/ontology/common/coterms/CoTermLoaderIT.java
@@ -1,11 +1,15 @@
 package uk.ac.ebi.quickgo.ontology.common.coterms;
 
+import uk.ac.ebi.quickgo.ontology.common.coterm.CoTerm;
 import uk.ac.ebi.quickgo.ontology.common.coterm.CoTermLoader;
 
+import java.util.Iterator;
+import java.util.Set;
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 
@@ -20,14 +24,22 @@ public class CoTermLoaderIT {
     @Test
     public void allValuesShouldBeLoadedCorrectly(){
 
-        Resource manualCoTermsFile = new ClassPathResource("CoTermsAll");
-        Resource allCoTermsFile = new ClassPathResource("CoTermsManual");
-
+        Resource manualCoTermsFile = new ClassPathResource("CoTermsManual");
+        Resource allCoTermsFile = new ClassPathResource("CoTermsAll");
 
         CoTermLoader coTermLoader = new CoTermLoader(manualCoTermsFile, allCoTermsFile);
         coTermLoader.load();
         assertThat(coTermLoader.coTermsAll.keySet(), hasSize(2));
         assertThat(coTermLoader.coTermsManual.keySet(), hasSize(2));
+
+        //To a quick check that the columns are being loaded properly
+        CoTerm coTerm = coTermLoader.coTermsAll.get("GO:0000001").get(1);
+        assertThat(coTerm.getId(), is("GO:0000001"));
+        assertThat(coTerm.getCompare(), is("GO:0034643"));
+        assertThat(coTerm.getProbabilityRatio(), is(16446.73f));
+        assertThat(coTerm.getSignificance(), is(7.56f));
+        assertThat(coTerm.getTogether(), is(207l));
+        assertThat(coTerm.getCompared(), is(208l));
 
     }
 }
