23a601bb05438c0e0a3ef2d980e16edaddf29ee6
rantunes <rantunes@ebi.ac.uk>
2016-10-11 12:32:53 +0100

    Remove special treatment of the IllegalArgumentException.
    IllegalArgumentExceptions are now considered to be 500 errors.
    
    See issue: https://www.ebi.ac.uk/panda/jira/browse/GOA-2179

--- rest-common/src/main/java/uk/ac/ebi/quickgo/rest/ResponseExceptionHandler.java
+++ rest-common/src/main/java/uk/ac/ebi/quickgo/rest/ResponseExceptionHandler.java
@@ -19,16 +19,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
  */
 @ControllerAdvice
 public class ResponseExceptionHandler {
-    @ExceptionHandler({IllegalStateException.class, RetrievalException.class, ServiceConfigException.class})
+    @ExceptionHandler({IllegalArgumentException.class, IllegalStateException.class, RetrievalException.class,
+            ServiceConfigException.class})
     protected ResponseEntity<ErrorInfo> handleInternalServer(RuntimeException ex, HttpServletRequest request) {
         ErrorInfo error = new ErrorInfo(request.getRequestURL().toString(), ex.getMessage());
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    protected ResponseEntity<ErrorInfo> handleBadRequest(RuntimeException ex, HttpServletRequest request) {
-        ErrorInfo error = new ErrorInfo(request.getRequestURL().toString(), ex.getMessage());
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ParameterException.class)
