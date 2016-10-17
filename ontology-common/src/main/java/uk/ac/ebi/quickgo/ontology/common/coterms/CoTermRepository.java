6ee50792a44ae455bd2d0ead4180bba3d15337f4
twardell <twardell@ebi.ac.uk>
2016-10-17 15:24:02 +0100

    Wording changed.

--- ontology-common/src/main/java/uk/ac/ebi/quickgo/ontology/common/coterms/CoTermRepository.java
+++ ontology-common/src/main/java/uk/ac/ebi/quickgo/ontology/common/coterms/CoTermRepository.java
@@ -14,15 +14,13 @@ import java.util.function.Predicate;
 public interface CoTermRepository {
 
     /**
-     * For a single GO Term, retrieve a list of co-occurring terms and related statistics, in order of the
-     * co-occurring terms similarity probability (descending).
-     * @param id is the target GO term, for which the method will retrieve co-occurring terms (GO terms that are used
-     * to annotation the same gene products as this GO Term is used to annotate).
+     * For a single GO Term, retrieve a list of co-occurring terms and related statistics, each one of which represent
+     * a GO Term that is used to annotate the same gene.
+     * @param id is the target GO term, for which the method will retrieve co-occurring terms.
      * @param source is the method from which the annotation that uses the GO term was generated.
-     * @param limit Limit the number of co-occurring terms return to the limit specified.
+     * @param limit Limit the number of co-occurring terms returned to this number, as a maximum.
      * @param filter apply the predicate to filter the results.
-     * @return a list of objects, each one of which represent a GO Term that is used to annotate the same gene
-     * product as the id. Each object holds statistics related to that co-occurrence.
+     * @return a list of CoTerms.
      */
     List<CoTerm> findCoTerms(String id, CoTermSource source, int limit, Predicate<CoTerm> filter);
 }
