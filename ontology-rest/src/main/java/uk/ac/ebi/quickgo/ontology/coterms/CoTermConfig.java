c960e5d94579e20b42f1fc735f72c392ca547e51
twardell <twardell@ebi.ac.uk>
2016-10-17 14:58:56 +0100

    Bring the contents of CoTermRepoConfig to this configuration file, so all client related configuration is here.

--- ontology-rest/src/main/java/uk/ac/ebi/quickgo/ontology/coterms/CoTermConfig.java
+++ ontology-rest/src/main/java/uk/ac/ebi/quickgo/ontology/coterms/CoTermConfig.java
@@ -1,11 +1,14 @@
 package uk.ac.ebi.quickgo.ontology.coterms;
 
-import uk.ac.ebi.quickgo.ontology.common.coterms.CoTermRepoConfig;
+import uk.ac.ebi.quickgo.ontology.common.coterms.CoTermRepository;
+import uk.ac.ebi.quickgo.ontology.common.coterms.CoTermRepositorySimpleMap;
+import uk.ac.ebi.quickgo.rest.service.ServiceConfigException;
 
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.core.io.Resource;
 
 /**
  * @author Tony Wardell
@@ -14,15 +17,33 @@ import org.springframework.context.annotation.Import;
  * Created with IntelliJ IDEA.
  */
 @Configuration
-@Import({CoTermRepoConfig.class})
 public class CoTermConfig {
 
     @Value("${coterm.default.limit:50}")
     private int defaultLimit;
 
+    @Value("${coterm.source.manual}")
+    private Resource manualResource;
+
+    @Value("${coterm.source.all}")
+    private Resource allResource;
+
     @Bean
     public CoTermLimit coTermLimit(){
         return new CoTermLimit(defaultLimit);
+    }
+
+    @Bean
+    public CoTermRepository coTermRepository() {
+        CoTermRepositorySimpleMap coTermRepository;
+        try{
+            coTermRepository = CoTermRepositorySimpleMap.createCoTermRepositorySimpleMap(manualResource, allResource);
+        } catch (IOException e) {
+            throw new ServiceConfigException("Failed to load co-occurring terms from manual source " +
+                    (manualResource!=null?manualResource.getDescription():"unknown") + " or from all source " +
+                    (allResource!=null?allResource.getDescription():"unknown"));
+        }
+        return coTermRepository;
     }
 
 
