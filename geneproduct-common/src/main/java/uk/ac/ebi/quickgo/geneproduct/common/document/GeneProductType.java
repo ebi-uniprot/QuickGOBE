e70f79140baf547f84c816338dabbb22b63918bf
twardell <twardell@ebi.ac.uk>
2016-11-01 10:58:17 +0000

    RNA -> miRNA

--- geneproduct-common/src/main/java/uk/ac/ebi/quickgo/geneproduct/common/document/GeneProductType.java
+++ geneproduct-common/src/main/java/uk/ac/ebi/quickgo/geneproduct/common/document/GeneProductType.java
@@ -8,7 +8,7 @@ package uk.ac.ebi.quickgo.geneproduct.common.document;
 public enum GeneProductType {
     COMPLEX("complex"),
     PROTEIN("protein"),
-    RNA("miRNA");
+    miRNA("miRNA");
 
     private String name;
 
