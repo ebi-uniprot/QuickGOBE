7bfc384b7f34f1a4bc187ae31d8d0bf6eb21b1c3
Edward Turner <eddturner@gmail.com>
2016-11-02 18:08:48 +0000

    typos and clarity in test methods; refactor common setup code into setup method to reduce duplication

--- rest-common/src/test/java/uk/ac/ebi/quickgo/rest/controller/CORSFilterDefaultPropertiesTest.java
+++ rest-common/src/test/java/uk/ac/ebi/quickgo/rest/controller/CORSFilterDefaultPropertiesTest.java
@@ -1,7 +1,6 @@
 package uk.ac.ebi.quickgo.rest.controller;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +14,9 @@ import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -31,45 +33,45 @@ import static uk.ac.ebi.quickgo.rest.controller.CORSFilter.*;
 public class CORSFilterDefaultPropertiesTest {
     @Autowired
     private CORSFilter filter;
+    private MockHttpServletResponse response;
+
+    @Before
+    public void setup() {
+        response = new MockHttpServletResponse();
+    }
 
     @Test
-    public void checkAllowedOriginsWereOverriden() throws IOException, ServletException {
-        MockHttpServletResponse response = new MockHttpServletResponse();
+    public void checkAllowOriginUsesDefault() throws IOException, ServletException {
         filter.doFilter(new MockHttpServletRequest(), response, new MockFilterChain());
         assertThat(response.getHeader(ACCESS_CONTROL_ALLOW_ORIGIN), is(DEFAULT_ACCESS_CONTROL_ALLOW_ORIGIN));
     }
 
     @Test
-    public void checkAllowedCredentialsWereOverriden() throws IOException, ServletException {
-        MockHttpServletResponse response = new MockHttpServletResponse();
+    public void checkAllowCredentialsUsesDefault() throws IOException, ServletException {
         filter.doFilter(new MockHttpServletRequest(), response, new MockFilterChain());
         assertThat(response.getHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS), is(DEFAULT_ACCESS_CONTROL_ALLOW_CREDENTIALS));
     }
 
     @Test
-    public void checkAllowedHeadersWereOverriden() throws IOException, ServletException {
-        MockHttpServletResponse response = new MockHttpServletResponse();
+    public void checkAllowHeadersUsesDefault() throws IOException, ServletException {
         filter.doFilter(new MockHttpServletRequest(), response, new MockFilterChain());
         assertThat(response.getHeader(ACCESS_CONTROL_ALLOW_HEADERS), is(DEFAULT_ACCESS_CONTROL_ALLOW_HEADERS));
     }
 
     @Test
-    public void checkAllowedMethodsWereOverriden() throws IOException, ServletException {
-        MockHttpServletResponse response = new MockHttpServletResponse();
+    public void checkAllowMethodsUsesDefault() throws IOException, ServletException {
         filter.doFilter(new MockHttpServletRequest(), response, new MockFilterChain());
         assertThat(response.getHeader(ACCESS_CONTROL_ALLOW_METHODS), is(DEFAULT_ACCESS_CONTROL_ALLOW_METHODS));
     }
 
     @Test
-    public void checkExposedHeadersWereOverriden() throws IOException, ServletException {
-        MockHttpServletResponse response = new MockHttpServletResponse();
+    public void checkExposeHeadersHasNoDefault() throws IOException, ServletException {
         filter.doFilter(new MockHttpServletRequest(), response, new MockFilterChain());
         assertThat(response.getHeader(ACCESS_CONTROL_EXPOSE_HEADERS), is(nullValue()));
     }
 
     @Test
-    public void checkMaxAgeWasOverriden() throws IOException, ServletException {
-        MockHttpServletResponse response = new MockHttpServletResponse();
+    public void checkMaxAgeUsesDefault() throws IOException, ServletException {
         filter.doFilter(new MockHttpServletRequest(), response, new MockFilterChain());
         assertThat(response.getHeader(ACCESS_CONTROL_MAX_AGE), is(DEFAULT_ACCESS_CONTROL_MAX_AGE));
     }
--- rest-common/src/test/java/uk/ac/ebi/quickgo/rest/controller/CORSFilterReadingPropertiesTest.java
+++ rest-common/src/test/java/uk/ac/ebi/quickgo/rest/controller/CORSFilterReadingPropertiesTest.java
@@ -1,18 +1,23 @@
 package uk.ac.ebi.quickgo.rest.controller;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.mock.web.MockFilterChain;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -28,45 +33,45 @@ import static uk.ac.ebi.quickgo.rest.controller.CORSFilter.*;
 public class CORSFilterReadingPropertiesTest {
     @Autowired
     private CORSFilter filter;
+    private MockHttpServletResponse response;
+
+    @Before
+    public void setup() {
+        response = new MockHttpServletResponse();
+    }
 
     @Test
-    public void checkAllowedOriginsWereOverriden() throws IOException, ServletException {
-        MockHttpServletResponse response = new MockHttpServletResponse();
+    public void checkAllowedOriginsWereOverridden() throws IOException, ServletException {
         filter.doFilter(new MockHttpServletRequest(), response, new MockFilterChain());
         assertThat(response.getHeader(ACCESS_CONTROL_ALLOW_ORIGIN), is("my-origin"));
     }
 
     @Test
-    public void checkAllowedCredentialsWereOverriden() throws IOException, ServletException {
-        MockHttpServletResponse response = new MockHttpServletResponse();
-        filter.doFilter(new MockHttpServletRequest(), response, new MockFilterChain());
-        assertThat(response.getHeader(ACCESS_CONTROL_ALLOW_HEADERS), is("my-headers"));
-    }
-
-    @Test
-    public void checkAllowedHeadersWereOverriden() throws IOException, ServletException {
-        MockHttpServletResponse response = new MockHttpServletResponse();
+    public void checkAllowedCredentialsWereOverridden() throws IOException, ServletException {
         filter.doFilter(new MockHttpServletRequest(), response, new MockFilterChain());
         assertThat(response.getHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS), is("my-credentials"));
     }
 
     @Test
-    public void checkAllowedMethodsWereOverriden() throws IOException, ServletException {
-        MockHttpServletResponse response = new MockHttpServletResponse();
+    public void checkAllowedHeadersWereOverridden() throws IOException, ServletException {
+        filter.doFilter(new MockHttpServletRequest(), response, new MockFilterChain());
+        assertThat(response.getHeader(ACCESS_CONTROL_ALLOW_HEADERS), is("my-headers"));
+    }
+
+    @Test
+    public void checkAllowedMethodsWereOverridden() throws IOException, ServletException {
         filter.doFilter(new MockHttpServletRequest(), response, new MockFilterChain());
         assertThat(response.getHeader(ACCESS_CONTROL_ALLOW_METHODS), is("my-methods"));
     }
 
     @Test
-    public void checkExposedHeadersWereOverriden() throws IOException, ServletException {
-        MockHttpServletResponse response = new MockHttpServletResponse();
+    public void checkExposedHeadersWereOverridden() throws IOException, ServletException {
         filter.doFilter(new MockHttpServletRequest(), response, new MockFilterChain());
         assertThat(response.getHeader(ACCESS_CONTROL_EXPOSE_HEADERS), is("my-exposed-headers"));
     }
 
     @Test
-    public void checkMaxAgeWasOverriden() throws IOException, ServletException {
-        MockHttpServletResponse response = new MockHttpServletResponse();
+    public void checkMaxAgeWasOverridden() throws IOException, ServletException {
         filter.doFilter(new MockHttpServletRequest(), response, new MockFilterChain());
         assertThat(response.getHeader(ACCESS_CONTROL_MAX_AGE), is("my-max-age"));
     }
