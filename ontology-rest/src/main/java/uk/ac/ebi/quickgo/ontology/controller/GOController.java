5c21d243aba8960dfbb56ea529998d01abf54d58
rantunes <rantunes@ebi.ac.uk>
2016-11-09 11:33:40 +0000

    Change GoTermPredicate to OntologyIdPredicate, so that it can also generate a predicate for the ECO terms.

--- ontology-rest/src/main/java/uk/ac/ebi/quickgo/ontology/controller/GOController.java
+++ ontology-rest/src/main/java/uk/ac/ebi/quickgo/ontology/controller/GOController.java
@@ -14,7 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static uk.ac.ebi.quickgo.common.validator.GOTermPredicate.isValidGOTermId;
+import static uk.ac.ebi.quickgo.common.validator.OntologyIdPredicate.isValidGOTermId;
 
 /**
  * REST controller for accessing GO related information.
