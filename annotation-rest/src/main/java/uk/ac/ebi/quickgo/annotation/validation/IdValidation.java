d9c5f3ed6c1e049521b7c0152a47dbd9511459af
twardell <twardell@ebi.ac.uk>
2016-11-21 14:33:14 +0000

    Change  interface QuickGoValidation in to class IdValidation that contains methods for validating ids.

---
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/validation/IdValidation.java
@@ -0,0 +1,26 @@
+package uk.ac.ebi.quickgo.annotation.validation;
+
+import com.google.common.base.Preconditions;
+
+/**
+ * @author Tony Wardell
+ * Date: 10/11/2016
+ * Time: 13:41
+ * Created with IntelliJ IDEA.
+ */
+public class IdValidation {
+
+    private static final String DELIMITER = ":";
+
+    static String db(final String idWithDb) {
+        Preconditions.checkArgument(idWithDb.contains(DELIMITER), "The id should contain contain the delimiter %s",
+                DELIMITER);
+        return idWithDb.substring(0, idWithDb.indexOf(":")).toLowerCase();
+    }
+
+    static String id(final String id){
+        Preconditions.checkArgument(id.contains(DELIMITER), "The id should contain contain the delimiter %s",
+                DELIMITER);
+        return id.substring(id.indexOf(":") + 1);
+    }
+}
