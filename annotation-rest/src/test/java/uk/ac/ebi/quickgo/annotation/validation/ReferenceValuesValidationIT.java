fd2bec5f8903cff59e63ce335c21e65a7ae64651
twardell <twardell@ebi.ac.uk>
2016-11-22 11:28:46 +0000

    Simplify tests.

--- annotation-rest/src/test/java/uk/ac/ebi/quickgo/annotation/validation/ReferenceValuesValidationIT.java
+++ annotation-rest/src/test/java/uk/ac/ebi/quickgo/annotation/validation/ReferenceValuesValidationIT.java
@@ -1,18 +1,14 @@
 package uk.ac.ebi.quickgo.annotation.validation;
 
-import javax.validation.ConstraintValidatorContext;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationContextLoader;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Mockito.mock;
+import static uk.ac.ebi.quickgo.annotation.validation.MockValidationConfig.ID_SUCCEEDS;
 
 /**
  * @author Tony Wardell
@@ -21,41 +17,34 @@ import static org.mockito.Mockito.mock;
  * Created with IntelliJ IDEA.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = MockValidationConfig.class, loader = SpringApplicationContextLoader.class)
-@ActiveProfiles(profiles = MockValidationConfig.DB_REF_VALIDATION_SUCCEEDS)
+@ContextConfiguration(classes = MockValidationConfig.class)
 public class ReferenceValuesValidationIT {
 
     @Autowired
     ReferenceValuesValidation refValidator;
-    private ConstraintValidatorContext mockContext;
-
-    @Before
-    public void setup(){
-        mockContext = mock(ConstraintValidatorContext.class);
-    }
 
     @Test
     public void validationSucceedsIfKnownDb(){
-        assertThat(refValidator.isValid(new String[]{"PMID:123456"}, mockContext), is(true));
+        assertThat(refValidator.isValid(new String[]{ID_SUCCEEDS}, null), is(true));
     }
 
     @Test
     public void validationSucceedsIfDbNotSpecified(){
-        assertThat(refValidator.isValid(new String[]{"123456"}, mockContext), is(true));
+        assertThat(refValidator.isValid(new String[]{"123456"}, null), is(true));
     }
 
     @Test
     public void validationSucceedsIfArgumentListIsNull(){
-        assertThat(refValidator.isValid(null, mockContext), is(true));
+        assertThat(refValidator.isValid(null, null), is(true));
     }
 
     @Test
     public void validationFailsIfUnknownDb(){
-        assertThat(refValidator.isValid(new String[]{"XXXX:123456"}, mockContext), is(false));
+        assertThat(refValidator.isValid(new String[]{"XXXX:123456"}, null), is(false));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void validationFailsIfArgumentListContainsNull(){
-       refValidator.isValid(new String[]{null}, mockContext);
+       refValidator.isValid(new String[]{null}, null);
     }
 }
