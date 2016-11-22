da013268e6f2ba7022a7e1bf50602653427ba4a9
twardell <twardell@ebi.ac.uk>
2016-11-22 14:55:22 +0000

    Fix copy and paste error in javadoc.

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/validation/ReferenceValidator.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/validation/ReferenceValidator.java
@@ -5,7 +5,7 @@ import javax.validation.Constraint;
 import javax.validation.Payload;
 
 /**
- * Annotation that allows a list of strings, separated by commas to be validated as Gene Product IDs
+ * Annotation that allows a list of strings, separated by commas to be validated as Reference Ids (full or partial).
  * @author Tony Wardell
  * Date: 14/06/2016
  * Time: 12:03
