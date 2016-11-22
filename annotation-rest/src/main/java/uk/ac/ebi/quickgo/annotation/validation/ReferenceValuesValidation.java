9800cc21a1d3afd1d8294a44e5fcd61a00e9316f
twardell <twardell@ebi.ac.uk>
2016-11-22 11:43:06 +0000

    Add logic for reference validation directly to this class.

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/validation/ReferenceValuesValidation.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/validation/ReferenceValuesValidation.java
@@ -1,9 +1,15 @@
 package uk.ac.ebi.quickgo.annotation.validation;
 
+import com.google.common.base.Preconditions;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import java.util.stream.Stream;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static uk.ac.ebi.quickgo.annotation.validation.IdValidation.db;
 
 /**
  * Validate the reference values used to filter annotations.
@@ -18,11 +24,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 class ReferenceValuesValidation implements ConstraintValidator<ReferenceValidator, String[]> {
 
     @Autowired
-    private ReferenceDBXRefEntityValidation validator;
+    DBXRefEntityValidation validator;
+    private List<String> referenceDatabases = Arrays.asList("pmid","doi","go_ref","reactome");
 
     @Override public void initialize(ReferenceValidator constraintAnnotation) {}
 
     @Override public boolean isValid(String[] values, ConstraintValidatorContext context) {
-        return values == null || Stream.of(values).allMatch(validator::isValid);
+        return values == null || Stream.of(values).allMatch(this::isValid);
+    }
+
+    private boolean isValid(String value){
+        Preconditions.checkArgument(Objects.nonNull(value), "The value for id cannot be null");
+
+        if (value.trim().isEmpty()) {
+            return false;
+        }
+
+        if (!value.contains(":")) {
+            return true;
+        }
+
+        if (!referenceDatabases.contains(db(value))) {
+            return false;
+        }
+
+        return validator.isValid(value);
     }
 }
