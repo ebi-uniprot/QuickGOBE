594c0a995251dabc8f882a9adac90b7d29d0e04a
twardell <twardell@ebi.ac.uk>
2016-11-22 12:02:16 +0000

    Change validating regex for qualifier - remove case insensitivity from the regex and add as flagged requirement.

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/model/AnnotationRequest.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/model/AnnotationRequest.java
@@ -300,7 +300,7 @@ public class AnnotationRequest {
         filterMap.put(QUALIFIER, qualifier);
     }
 
-    @ArrayPattern(regexp = "^(?i:NOT \\| )?[A-Za-z_]+$", paramName = QUALIFIER)
+    @ArrayPattern(regexp = "^(NOT \\| )?[A-Z_]+$", flags = CASE_INSENSITIVE, paramName = QUALIFIER)
     public String[] getQualifier() {
         return filterMap.get(QUALIFIER);
     }
