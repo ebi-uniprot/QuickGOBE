8a2acd505138e2f1092f5b618c4124898b48b28b
twardell <twardell@ebi.ac.uk>
2016-11-25 14:32:24 +0000

    Renamed, make id() method static

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/validation/service/DbCrossReferenceId.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/validation/service/DbCrossReferenceId.java
@@ -1,15 +1,17 @@
-package uk.ac.ebi.quickgo.annotation.validation;
+package uk.ac.ebi.quickgo.annotation.validation.service;
 
 import com.google.common.base.Preconditions;
 import java.util.Objects;
 
 /**
+ * Utility methods related to Database Cross Reference ids.
+ *
  * @author Tony Wardell
  * Date: 10/11/2016
  * Time: 13:41
  * Created with IntelliJ IDEA.
  */
-class IdValidation {
+public class DbCrossReferenceId {
 
     private static final String DELIMITER = ":";
 
@@ -19,7 +21,7 @@ class IdValidation {
         return idWithDb.substring(0, idWithDb.indexOf(":")).toLowerCase().trim();
     }
 
-    static String id(final String idWithDb){
+    public static String id(final String idWithDb){
         Preconditions.checkArgument(Objects.nonNull(idWithDb), "The id should not be null");
         Preconditions.checkArgument(idWithDb.contains(DELIMITER), "The id should contain the delimiter %s", DELIMITER);
         return idWithDb.substring(idWithDb.indexOf(":") + 1).trim();
