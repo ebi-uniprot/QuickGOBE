af0b93d3af353dfa31e47f5bc64907df4957166f
twardell <twardell@ebi.ac.uk>
2016-11-25 14:50:17 +0000

    New - common object for using the validation entities.

---
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/validation/service/ValidationEntityChecker.java
@@ -0,0 +1,41 @@
+package uk.ac.ebi.quickgo.annotation.validation.service;
+
+import uk.ac.ebi.quickgo.annotation.validation.model.ValidationEntities;
+import uk.ac.ebi.quickgo.annotation.validation.model.ValidationEntity;
+
+import java.util.*;
+import java.util.Objects;
+
+/**
+ * Use the database cross reference information contained in this class to verify a list of potential database cross
+ * reference identifiers.
+ *
+ * @author Tony Wardell
+ * Date: 08/11/2016
+ * Time: 14:41
+ * Created with IntelliJ IDEA.
+ */
+
+public class ValidationEntityChecker {
+
+    private final ValidationEntities<ValidationEntity> validationEntities;
+
+    public ValidationEntityChecker(ValidationEntities<ValidationEntity> validationEntities) {
+        this.validationEntities = validationEntities;
+    }
+
+    /**
+     * If the value passed to this method can be successfully validated isValid will return true.
+     * @param value a potential database cross reference identifier.
+     * @return validation result.
+     */
+
+    public boolean isValid(String value) {
+        return Objects.nonNull(value) && (!value.contains(":") || isValidAgainstEntity(value));
+    }
+
+    private boolean isValidAgainstEntity(String value) {
+        final List<ValidationEntity> entities = validationEntities.get(DbCrossReferenceId.db(value));
+        return entities != null && entities.stream().anyMatch(e -> e.test(DbCrossReferenceId.id(value)));
+    }
+}
