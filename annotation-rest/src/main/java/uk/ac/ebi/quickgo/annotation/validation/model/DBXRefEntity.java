88a150f1cfe1fe85a350a6f4b6c52c975c92b149
twardell <twardell@ebi.ac.uk>
2016-11-25 14:05:13 +0000

    New - aggregate validation entities.

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/validation/model/DBXRefEntity.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/validation/model/DBXRefEntity.java
@@ -3,12 +3,14 @@ package uk.ac.ebi.quickgo.annotation.validation.model;
 import java.util.regex.Pattern;
 
 /**
+ * A validation entity for Database Cross References and their associated regular expressions for validating ids.
+ *
  * @author Tony Wardell
  * Date: 07/11/2016
  * Time: 18:07
  * Created with IntelliJ IDEA.
  */
-public class DBXRefEntity {
+public class DBXRefEntity implements ValidationEntity {
 
     public String database;
     public String entityType;
@@ -16,7 +18,12 @@ public class DBXRefEntity {
     public Pattern idValidationPattern;
     public String databaseURL;
 
+    @Override
     public boolean test(String value) {
         return idValidationPattern.matcher(value).matches();
+    }
+
+    @Override public String keyValue() {
+        return database;
     }
 }
