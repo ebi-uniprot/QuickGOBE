c47c5352478392e2cb57a21438c3a95ad24a458d
twardell <twardell@ebi.ac.uk>
2016-12-07 09:04:40 +0000

    Remove redundant import.

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/validation/loader/ValidationConfig.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/validation/loader/ValidationConfig.java
@@ -26,7 +26,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 
