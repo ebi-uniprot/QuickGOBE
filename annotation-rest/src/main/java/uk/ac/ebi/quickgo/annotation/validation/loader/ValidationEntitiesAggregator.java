37853b33365ba38ef1bf1a471198be3dfd8e0aab
twardell <twardell@ebi.ac.uk>
2016-12-07 12:12:37 +0000

    Move the accumulation to the ValidationEntities class.

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/validation/loader/ValidationEntitiesAggregator.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/validation/loader/ValidationEntitiesAggregator.java
@@ -1,13 +1,12 @@
-package uk.ac.ebi.quickgo.annotation.validation.model;
+package uk.ac.ebi.quickgo.annotation.validation.loader;
+
+import uk.ac.ebi.quickgo.annotation.validation.model.ValidationEntities;
+import uk.ac.ebi.quickgo.annotation.validation.model.ValidationEntity;
 
 import com.google.common.base.Preconditions;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import org.springframework.batch.item.ItemWriter;
-
-import static java.util.stream.Collectors.groupingBy;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Aggregate ValidationEntity instances.
@@ -19,15 +18,18 @@ import static java.util.stream.Collectors.groupingBy;
  */
 public class ValidationEntitiesAggregator implements ItemWriter<ValidationEntity> {
 
-    Map<String, List<ValidationEntity>> mappedEntities = new HashMap<>();
+    private final ValidationEntities validationEntities;
+
+    @Autowired
+    public ValidationEntitiesAggregator(ValidationEntities validationEntities) {
+        this.validationEntities = validationEntities;
+    }
 
     @Override public void write(List<? extends ValidationEntity> items) {
         Preconditions.checkArgument(items != null, "The list of items written to ValidationEntitiesAggregator " +
                 "cannot be null.");
 
-        mappedEntities.putAll(items.stream()
-                .filter(Objects::nonNull)
-                .filter(e -> Objects.nonNull(e.keyValue()))
-                .collect(groupingBy(e -> e.keyValue().toLowerCase())));
+        validationEntities.addEntities(items);
     }
+
 }
