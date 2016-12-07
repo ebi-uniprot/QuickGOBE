7193f1e9ffabd974f0f5e9d530aff7c993706e25
twardell <twardell@ebi.ac.uk>
2016-12-07 10:12:53 +0000

    db now returns null if not doesn't contain db.

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/validation/service/DbCrossReferenceId.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/validation/service/DbCrossReferenceId.java
@@ -17,18 +17,42 @@ class DbCrossReferenceId {
 
     private DbCrossReferenceId() {}
 
+    /**
+     * Parse a target id that is in the format DB:ID e.g IntAct:EBI-10043081, and return the DB portion of the id.
+     * @param idWithDb
+     * @return The DB portion of the id, or null if there is no DB part.
+     */
     public static String db(final String idWithDb) {
         checkPreconditions(idWithDb);
+        if(!idWithDb.contains(DELIMITER)){
+            return null;
+        }
         return idWithDb.substring(0, idWithDb.indexOf(":")).trim();
     }
 
+    /**
+     * Parse a target id that is in the format DB:ID e.g IntAct:EBI-10043081, and return the ID portion of the id.
+     * @param idWithDb
+     * @return The ID portion of the id, or null if there is no ID part.
+     */
     public static String id(final String idWithDb){
         checkPreconditions(idWithDb);
+        if(!idWithDb.contains(DELIMITER)){
+            return null;
+        }
         return idWithDb.substring(idWithDb.indexOf(":") + 1).trim();
+    }
+
+    /**
+     * Check to see if the String is contains a delimiter, so hopefully is in the DB:ID format.
+     * @param value a DB:ID candidate.
+     * @return true if contains delimiter, else false.
+     */
+    public static boolean isFullId(String value){
+        return (value.contains(DELIMITER));
     }
 
     private static void checkPreconditions(String idWithDb) {
         Preconditions.checkArgument(Objects.nonNull(idWithDb), "The id should not be null");
-        Preconditions.checkArgument(idWithDb.contains(DELIMITER), "The id should contain the delimiter %s", DELIMITER);
     }
 }
