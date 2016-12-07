b562ec7a62a2290de9fac636ab0087e863251154
twardell <twardell@ebi.ac.uk>
2016-12-07 12:10:52 +0000

    Add test for null.

--- annotation-rest/src/test/java/uk/ac/ebi/quickgo/annotation/validation/model/ValidationEntitiesImplTest.java
+++ annotation-rest/src/test/java/uk/ac/ebi/quickgo/annotation/validation/model/ValidationEntitiesImplTest.java
@@ -1,12 +1,10 @@
 package uk.ac.ebi.quickgo.annotation.validation.model;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
@@ -26,25 +24,26 @@ public class ValidationEntitiesImplTest {
 
     @Before
     public void setup(){
-        ValidationEntitiesAggregator aggregator = new ValidationEntitiesAggregator();
-        Map<String, List<ValidationEntity>> mappedEntities = new HashMap<>();
-        mappedEntities.put("interpro", Arrays.asList(mockEntity,mockEntity));
-        aggregator.mappedEntities = mappedEntities;
-
-        validationEntities = new ValidationEntitiesImpl(aggregator);
+        validationEntities = new ValidationEntitiesImpl();
         mockEntity = mock(ValidationEntity.class);
         when(mockEntity.keyValue()).thenReturn(INTERPRO);
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void failsConstructionIfConstructorArgumentIsNull(){
-        new ValidationEntitiesImpl(null);
+    @Test
+    public void nothingReturnedIfNotWrittenTo(){
+        assertThat(validationEntities.get(INTERPRO), is(equalTo(null)));
     }
 
 
     @Test
     public void checkTwoSuccessfulElements(){
+        validationEntities.addEntities( Arrays.asList(mockEntity,mockEntity));
         assertThat(validationEntities.get(INTERPRO.toLowerCase()), hasSize(equalTo(2)));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void addingNullThrowsException(){
+        validationEntities.addEntities(null);
     }
 
 }
