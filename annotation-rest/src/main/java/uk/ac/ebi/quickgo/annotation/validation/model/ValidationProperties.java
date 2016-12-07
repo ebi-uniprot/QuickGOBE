5a6882375a1e5aa966aebc02ea55afcd40a04d93
twardell <twardell@ebi.ac.uk>
2016-12-07 09:12:27 +0000

    Add missing javadoc.

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/validation/model/ValidationProperties.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/validation/model/ValidationProperties.java
@@ -21,26 +21,48 @@ public class ValidationProperties {
     private Resource validationResource;
     private List<String> referenceDbs;
 
+    /**
+     * The batch size to process when reading validation properties.
+     * @return chunk size in number of records.
+     */
     public int getChunk() {
         return chunk;
     }
 
+    /**
+     * The batch size to process when reading validation properties.
+     * @param chunk
+     */
     public void setChunk(int chunk) {
         this.chunk = chunk;
     }
 
+    /**
+     * The source of validating data.
+     * @return resource to use.
+     */
     public Resource getValidationResource() {
         return validationResource;
     }
 
+    /**
+     * The source of validating data.
+     */
     public void setValidationResource(Resource validationResource) {
         this.validationResource = validationResource;
     }
 
+    /**
+     * A list of database ids that are valid to use when filtering annotations by reference.
+     * @return A list of database ids.
+     */
     public List<String> getReferenceDbs() {
         return referenceDbs;
     }
 
+    /**
+     * A list of database codes that are valid to use when filtering annotations by reference.
+     */
     public void setReferenceDbs(List<String> referenceDbs) {
         this.referenceDbs = referenceDbs;
     }
