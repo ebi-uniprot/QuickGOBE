cf22317d9cbfb3bdc82357f111d7d92cfd30b984
twardell <twardell@ebi.ac.uk>
2016-12-07 12:08:57 +0000

    Simply test to match what the aggregator is doing.

--- annotation-rest/src/test/java/uk/ac/ebi/quickgo/annotation/validation/model/ValidationEntitiesAggregatorTest.java
+++ annotation-rest/src/test/java/uk/ac/ebi/quickgo/annotation/validation/model/ValidationEntitiesAggregatorTest.java
@@ -1,14 +1,15 @@
 package uk.ac.ebi.quickgo.annotation.validation.model;
 
+import uk.ac.ebi.quickgo.annotation.validation.loader.ValidationEntitiesAggregator;
+
 import java.util.Arrays;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 /**
  * @author Tony Wardell
@@ -18,49 +19,29 @@ import static org.mockito.Mockito.when;
  */
 public class ValidationEntitiesAggregatorTest {
 
-    private static final String INTERPRO = "INTERPRO";
-    private static final String PIM = "PIM";
-    private ValidationEntitiesAggregator  aggregator;
+    private ValidationEntitiesAggregator aggregator;
     private ValidationEntity mockEntityInterpro;
     private ValidationEntity mockEntityPim;
+    private ValidationEntities mockValidationEntities;
 
     @Before
     public void setup(){
-        aggregator = new ValidationEntitiesAggregator();
+        mockValidationEntities = mock(ValidationEntities.class);
+        aggregator = new ValidationEntitiesAggregator(mockValidationEntities);
         mockEntityInterpro = mock(ValidationEntity.class);
-        when(mockEntityInterpro.keyValue()).thenReturn(INTERPRO);
         mockEntityPim = mock(ValidationEntity.class);
-        when(mockEntityPim.keyValue()).thenReturn(PIM);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void writeNullEntitiesThrowsException(){
+        aggregator.write(null);
     }
 
     @Test
-    public void loadTwoEntitiesWithTheSameName(){
-        aggregator.write(Arrays.asList(mockEntityInterpro, mockEntityInterpro));
-        assertThat(aggregator.mappedEntities.keySet(), hasSize(equalTo(1)));
-        assertThat(aggregator.mappedEntities.get(INTERPRO.toLowerCase()), hasSize(equalTo(2)));
+    public void writeEntitiesPassedOn(){
+        final List<ValidationEntity> items = Arrays.asList(mockEntityInterpro, mockEntityPim);
+        aggregator.write(items);
+        verify(mockValidationEntities, times(1)).addEntities(items);
     }
 
-    @Test
-    public void loadTwoEntitiesWithTheDifferentNames(){
-        aggregator.write(Arrays.asList(mockEntityInterpro, mockEntityPim));
-        assertThat(aggregator.mappedEntities.keySet(), hasSize(equalTo(2)));
-        assertThat(aggregator.mappedEntities.get(INTERPRO.toLowerCase()), hasSize(equalTo(1)));
-        assertThat(aggregator.mappedEntities.get(PIM.toLowerCase()), hasSize(equalTo(1)));
-    }
-
-        @Test
-        public void loadTwoSuccessfulElementsIgnoreNull(){
-            aggregator.write(Arrays.asList(mockEntityInterpro, mockEntityInterpro, null));
-            assertThat(aggregator.mappedEntities.keySet(), hasSize(equalTo(1)));
-            assertThat(aggregator.mappedEntities.get(INTERPRO.toLowerCase()), hasSize(equalTo(2)));
-        }
-
-        @Test
-        public void loadTwoSuccessfulElementsIgnoreKeyValueIsNull(){
-            ValidationEntity mockEntityKeyIsNull = mock(ValidationEntity.class);
-            when(mockEntityKeyIsNull.keyValue()).thenReturn(null);
-            aggregator.write(Arrays.asList(mockEntityInterpro, mockEntityInterpro, mockEntityKeyIsNull));
-            assertThat(aggregator.mappedEntities.keySet(), hasSize(equalTo(1)));
-            assertThat(aggregator.mappedEntities.get(INTERPRO.toLowerCase()), hasSize(equalTo(2)));
-        }
 }
