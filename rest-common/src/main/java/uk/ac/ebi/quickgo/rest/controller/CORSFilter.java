77e2da68c0b120acb805eb91e4a35f077639d874
Edward Turner <eddturner@ebi.ac.uk>
2016-12-09 09:17:41 +0000

    CORS to send back only 1 allowed origin host in header. A whitelist of accepted origins is loaded from a properties file, checked against the request origin, and if list, then sends back this origin in the header. If not specified, '*' is used. If prohibited origin, then send back space separated list of origins that are accepted.

--- rest-common/src/main/java/uk/ac/ebi/quickgo/rest/controller/CORSFilter.java
+++ rest-common/src/main/java/uk/ac/ebi/quickgo/rest/controller/CORSFilter.java
@@ -1,25 +1,70 @@
 package uk.ac.ebi.quickgo.rest.controller;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.context.annotation.Configuration;
+import org.slf4j.Logger;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 /**
- * Simple filter for specifying cross-origin resource sharing headers.
+ * <p>Simple filter for specifying cross-origin resource sharing headers.
+ *
+ * <p>To specify your own CORS properties, please specify one of the following properties
+ * in your application.properties file.
+ * <ul>
+ *     <li>cors.allow-credentials</li>
+ *     <li>cors.allow-headers</li>
+ *     <li>cors.allow-methods</li>
+ *     <li>cors.allow-origins</li>
+ *     <li>cors.expose-headers</li>
+ *     <li>cors.max-age</li>
+ * </ul>
  *
  * Created 16/05/16
  * @author Edd
  */
-@Configuration
+@Component
+@ConfigurationProperties(prefix = "cors")
 public class CORSFilter implements Filter {
+    static final String DEFAULT_ACCESS_CONTROL_ALLOW_CREDENTIALS = "true";
+    static final String DEFAULT_ACCESS_CONTROL_ALLOW_HEADERS =
+            "Origin, " +
+                    "Accept, " +
+                    "X-Requested-With, " +
+                    "Content-Type, " +
+                    "Access-Control-Request-Method, " +
+                    "Access-Control-Request-Headers";
+    static final String DEFAULT_ACCESS_CONTROL_ALLOW_METHODS = "GET";
+    static final String DEFAULT_ACCESS_CONTROL_ALLOW_ORIGIN = "*";
+    static final String DEFAULT_ACCESS_CONTROL_MAX_AGE = "3600";
+    static final String ACCESS_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials";
+    static final String ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
+    static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+    static final String ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
+    static final String ACCESS_CONTROL_EXPOSE_HEADERS = "Access-Control-Expose-Headers";
+    static final String ACCESS_CONTROL_MAX_AGE = "Access-Control-Max-Age";
+    private static final Logger LOGGER = getLogger(CORSFilter.class);
+    private static final Set<String> DEFAULT_CONTROL_ALLOW_ORIGINS_LIST =
+            Stream.of(DEFAULT_ACCESS_CONTROL_ALLOW_ORIGIN).collect(Collectors.toSet());
+    private static final String DELIMITER = " ";
+    public static final String PROTOCOL_HOST_DELIMITER = "://";
 
-    private static final String DEFAULT_ACCESS_CONTROL_ALLOW_ORIGIN = "*";
-    private static final String DEFAULT_ACCESS_CONTROL_MAX_AGE = "3600";
-    private static final String DEFAULT_ACCESS_CONTROL_ALLOW_CREDENTIALS = "true";
-    private static final String DEFAULT_ACCESS_CONTROL_ALLOW_METHODS = "POST, GET, OPTIONS, DELETE";
-    private static final String DEFAULT_ACCESS_CONTROL_ALLOW_HEADERS =
-            "Content-Type, Accept, X-Requested-With";
+    private String allowCredentials = DEFAULT_ACCESS_CONTROL_ALLOW_CREDENTIALS;
+    private String allowHeaders = DEFAULT_ACCESS_CONTROL_ALLOW_HEADERS;
+    private String allowMethods = DEFAULT_ACCESS_CONTROL_ALLOW_METHODS;
+    private Set<String> allowOrigins = DEFAULT_CONTROL_ALLOW_ORIGINS_LIST;
+    private String allowOriginsStr = DEFAULT_ACCESS_CONTROL_ALLOW_ORIGIN;
+    private String exposeHeaders;
+    private String maxAge = DEFAULT_ACCESS_CONTROL_MAX_AGE;
 
     @Override public void init(FilterConfig filterConfig) throws ServletException {
 
@@ -30,16 +75,85 @@ public class CORSFilter implements Filter {
             throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
-        response.setHeader("Access-Control-Allow-Origin", DEFAULT_ACCESS_CONTROL_ALLOW_ORIGIN);
-        response.setHeader("Access-Control-Allow-Credentials", DEFAULT_ACCESS_CONTROL_ALLOW_CREDENTIALS);
-        response.setHeader("Access-Control-Allow-Methods", DEFAULT_ACCESS_CONTROL_ALLOW_METHODS);
-        response.setHeader("Access-Control-Max-Age", DEFAULT_ACCESS_CONTROL_MAX_AGE);
-        response.setHeader("Access-Control-Allow-Headers", DEFAULT_ACCESS_CONTROL_ALLOW_HEADERS);
+        addHeaderIfNotNull(response, ACCESS_CONTROL_ALLOW_CREDENTIALS, allowCredentials);
+        addHeaderIfNotNull(response, ACCESS_CONTROL_ALLOW_HEADERS, allowHeaders);
+        addHeaderIfNotNull(response, ACCESS_CONTROL_ALLOW_METHODS, allowMethods);
+        addHeaderIfNotNull(response, ACCESS_CONTROL_EXPOSE_HEADERS, exposeHeaders);
+        addHeaderIfNotNull(response, ACCESS_CONTROL_MAX_AGE, maxAge);
+        setAllowOrigin(servletRequest, response);
 
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
     @Override public void destroy() {
 
+    }
+
+    public void setAllowCredentials(String allowCredentials) {
+        this.allowCredentials = allowCredentials;
+    }
+
+    public void setAllowHeaders(String allowHeaders) {
+        this.allowHeaders = allowHeaders;
+    }
+
+    public void setAllowMethods(String allowMethods) {
+        this.allowMethods = allowMethods;
+    }
+
+    public void setExposeHeaders(String exposeHeaders) {
+        this.exposeHeaders = exposeHeaders;
+    }
+
+    public void setAllowOrigins(Set<String> allowOrigins) {
+        Set<String> originsInProperties = allowOrigins.stream()
+                .map(origin -> {
+                    try {
+                        return new URI(origin).getHost();
+                    } catch (URISyntaxException e) {
+                        LOGGER.warn("Could not parse {} header: {}", ACCESS_CONTROL_ALLOW_ORIGIN, e);
+                    }
+                    return null;
+                })
+                .filter(Objects::nonNull)
+                .collect(Collectors.toSet());
+
+        if (!originsInProperties.isEmpty()) {
+            this.allowOrigins = originsInProperties;
+        } else {
+            this.allowOrigins = DEFAULT_CONTROL_ALLOW_ORIGINS_LIST;
+        }
+
+        this.allowOriginsStr = allowOrigins.stream().collect(Collectors.joining(DELIMITER));
+    }
+
+    public void setMaxAge(String maxAge) {
+        this.maxAge = maxAge;
+    }
+
+    private void setAllowOrigin(ServletRequest servletRequest, HttpServletResponse response) {
+        String requestHost = servletRequest.getServerName();
+        String protocol = servletRequest.getScheme();
+        String origin = buildOrigin(protocol, requestHost);
+
+        if (allowOrigins.contains(DEFAULT_ACCESS_CONTROL_ALLOW_ORIGIN)) {
+            response.setHeader(ACCESS_CONTROL_ALLOW_ORIGIN, DEFAULT_ACCESS_CONTROL_ALLOW_ORIGIN);
+        } else if (allowOrigins.contains(requestHost)) {
+            response.setHeader(ACCESS_CONTROL_ALLOW_ORIGIN, origin);
+        } else {
+            LOGGER.warn("Attempted access from unauthorized origin: {}", origin);
+            response.setHeader(ACCESS_CONTROL_ALLOW_ORIGIN, allowOriginsStr);
+        }
+
+    }
+
+    private String buildOrigin(String protocol, String requestHost) {
+        return protocol + PROTOCOL_HOST_DELIMITER + requestHost;
+    }
+
+    private void addHeaderIfNotNull(HttpServletResponse response, String header, String value) {
+        if (value != null) {
+            response.setHeader(header, value);
+        }
     }
 }
