da7664f9cf047724787bc55070bbd40e6edef305
twardell <twardell@ebi.ac.uk>
2016-12-09 11:50:58 +0000

    Remove extension change.

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/model/AnnotationRequest.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/model/AnnotationRequest.java
@@ -81,8 +81,7 @@ public class AnnotationRequest {
             Searchable.REFERENCE,
             Searchable.TAXON_ID,
             Searchable.TARGET_SET,
-            Searchable.WITH_FROM,
-            Searchable.EXTENSION
+            Searchable.WITH_FROM
     };
 
     /**
@@ -223,45 +222,6 @@ public class AnnotationRequest {
                     "separated values.",
             example = "EXP,IDA")
     private String goIdEvidence;
-
-    @ApiModelProperty(value = "An annotation extension is used to extend " +
-            "(i.e., add more specificity to) the GO term used in an annotation; the combination of the GO term plus the" +
-            " extension is equivalent to a more specific GO term." +
-            "An annotation extension is stored in the database, and transmitted in annotation files, as a single " +
-            "string, structured as a pipe-separated list of comma-separated lists of components. So, the following " +
-            "are all valid (where c represents a component):\n" +
-            "c" +
-            "c|c|c" +
-            "c,c" +
-            "c|c,c|c,c,c" +
-            "The lists of comma-separated components are referred to as \"conjunctive statements\", while the " +
-            "pipe-separated ones are referred to as \"independent statements\"; the pipe characters can be read as " +
-            "\"OR\", while the commas can be read as \"AND\"." +
-            "Each independent statement represents logically a separate annotation. For example, if we have the " +
-            "following annotation (where GP1 is the identifier of a gene product, and G1 is the identifier of a GO " +
-            "term):" +
-            "GP1 G1 c1,c2|c3|c4,c5" +
-            "then that is logically equivalent to these three annotations:" +
-            "GP1 G1 c1,c2" +
-            "GP1 G1 c3" +
-            "GP1 G1 c4,c5" +
-            "A component takes the form:" +
-            "r(t)" +
-            "where r is the name of a relation (one of the subset \"valid_for_annotation_extension\" defined in " +
-            "http://viewvc.geneontology.org/viewvc/GO-SVN/trunk/ontology/extensions/gorel.obo, and provided in the " +
-            "file ANNOTATION_EXTENSION_RELATIONS.dat.gz), and t is the target (or, to be strictly correct, the local " +
-            "range) of the relation, which is the fully-qualified identifier (i.e., db:id format) of an entry in some" +
-            " database / ontology." +
-            "The following are all valid examples of annotation extension components:" +
-            "causally_upstream_of(GO:1903406)" +
-            "has_regulation_target(UniProtKB:Q6PIC6)" +
-            "occurs_in(CL:0002131)" +
-            "occurs_in(GO:0005886)" +
-            "occurs_in(UBERON:0000922)",
-            example = "occurs_in(CL:0000032),transports_or_maintains_localization_of(UniProtKB:P10288)|" +
-                    "results_in_formation_of(UBERON:0003070),occurs_in(CL:0000032),occurs_in(CL:0000008)," +
-                    "results_in_formation_of(UBERON:0001675)")
-    private String[] extensions;
 
     private final Map<String, String[]> filterMap = new HashMap<>();
 
@@ -506,23 +466,6 @@ public class AnnotationRequest {
 
     public void setPage(int page) {
         this.page = page;
-    }
-
-    /**
-     * A list of extension relationship values, separated by commas
-     * In the format extension=occurs_in(PomBase:SPBP23A10.14c),RGD:621207 etc
-     * Users can supply just the id (e.g. PomBase) or id SPBP23A10.14c
-     */
-    public void setExtension(String... extension) {
-        filterMap.put(Searchable.EXTENSION, extension);
-    }
-
-    /**
-     * Return a list of annotation extension values, separated by commas
-     * @return String containing comma separated list of extension values.
-     */
-    public String[] getExtension() {
-        return filterMap.get(Searchable.EXTENSION);
     }
 
     /**
