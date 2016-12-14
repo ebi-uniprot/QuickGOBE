b09ddd1f369a6ba2282e9392cfcf0db02005536c
twardell <twardell@ebi.ac.uk>
2016-12-14 10:57:32 +0000

    Remove unused import.

--- ontology-common/src/test/java/uk/ac/ebi/quickgo/ontology/common/coterms/CoTermRepositorySimpleMapFailedRetrievalIT.java
+++ ontology-common/src/test/java/uk/ac/ebi/quickgo/ontology/common/coterms/CoTermRepositorySimpleMapFailedRetrievalIT.java
@@ -1,6 +1,5 @@
 package uk.ac.ebi.quickgo.ontology.common.coterms;
 
-import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
