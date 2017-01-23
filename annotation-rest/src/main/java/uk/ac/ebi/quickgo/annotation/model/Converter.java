f525a447eb0db584b30621ccaf9d6c6083fe9904
twardell <twardell@ebi.ac.uk>
2017-01-23 11:34:28 +0000

    Add some javadoc.

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/model/Converter.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/model/Converter.java
@@ -11,7 +11,7 @@ public interface Converter {
 
     /**
      * Convert an annotation to a String based representation. The implementing class determines the format.
-     * @param annotation
+     * @param annotation instance
      * @return String representation.
      */
     String convert(Annotation annotation);
