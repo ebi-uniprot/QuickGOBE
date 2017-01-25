c536b919c3f977c2ea87aaf6423fa4e42adf1a67
twardell <twardell@ebi.ac.uk>
2017-01-25 16:59:52 +0000

    Reformat code.

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/converter/Header.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/converter/Header.java
@@ -1,8 +1,8 @@
 package uk.ac.ebi.quickgo.annotation.converter;
 
 import java.io.IOException;
-import java.nio.file.Path;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -34,7 +34,8 @@ import static java.util.Arrays.stream;
  * !URL: http://www.ebi.ac.uk/GOA
  * !Contact Email: goa@ebi.ac.uk
  * !Date downloaded from the QuickGO browser: 20170117
- * !Filtering parameters selected to generate file: GAnnotation?count=25&protein=A0A000&select=normal&advanced=&termUse=ancestor&slimTypes=IPO%3D
+ * !Filtering parameters selected to generate file:
+ * GAnnotation?count=25&protein=A0A000&select=normal&advanced=&termUse=ancestor&slimTypes=IPO%3D
  */
 public class Header {
 
@@ -55,7 +56,7 @@ public class Header {
         this.ontologyPath = ontologyPath;
     }
 
-    public void write(ResponseBodyEmitter emitter, HttpServletRequest request,  MediaType acceptHeader ) {
+    public void write(ResponseBodyEmitter emitter, HttpServletRequest request, MediaType acceptHeader) {
         send(emitter, version(acceptHeader));
         send(emitter, PROJECT_NAME);
         send(emitter, URL);
@@ -67,7 +68,7 @@ public class Header {
 
     }
 
-    private void send(ResponseBodyEmitter emitter, String content)  {
+    private void send(ResponseBodyEmitter emitter, String content) {
         try {
             emitter.send((HEADER_LINE_PREFIX + content), MediaType.TEXT_PLAIN);
         } catch (IOException e) {
@@ -75,10 +76,12 @@ public class Header {
         }
     }
 
-    private String version(MediaType acceptHeader){
-        switch (acceptHeader.getSubtype().toLowerCase()){
-            case "gaf": return GAF_VERSION;
-            case "gpad": return GPAD_VERSION;
+    private String version(MediaType acceptHeader) {
+        switch (acceptHeader.getSubtype().toLowerCase()) {
+            case "gaf":
+                return GAF_VERSION;
+            case "gpad":
+                return GPAD_VERSION;
         }
         throw new IllegalArgumentException("Unknown Mime Type subtype requested: " + acceptHeader.getSubtype());
     }
@@ -87,7 +90,7 @@ public class Header {
         return YYYYMMDD_DATE_FORMAT.format(new Date());
     }
 
-    private String  request(HttpServletRequest request){
+    private String request(HttpServletRequest request) {
         return request.getRequestURI() + parameterString(request);
     }
 
@@ -100,16 +103,16 @@ public class Header {
                       .collect(Collectors.joining("&"));
     }
 
-    private List<String> ontology()  {
+    private List<String> ontology() {
         try {
-            String lastModifiedTime = (String)Files.getAttribute(ontologyPath,"lastModifiedTime");
+            String lastModifiedTime = (String) Files.getAttribute(ontologyPath, "lastModifiedTime");
 
-            if(!lastModifiedTime.equals(previousTimeStamp)){
+            if (!lastModifiedTime.equals(previousTimeStamp)) {
                 previousTimeStamp = lastModifiedTime;
                 savedOntologyLines = Files.readAllLines(ontologyPath).stream()
-                                     .skip(1)
-                                     .map(s -> s.substring(s.indexOf("http:")))
-                                        .collect(Collectors.toList());
+                                          .skip(1)
+                                          .map(s -> s.substring(s.indexOf("http:")))
+                                          .collect(Collectors.toList());
 
             }
         } catch (Exception e) {
