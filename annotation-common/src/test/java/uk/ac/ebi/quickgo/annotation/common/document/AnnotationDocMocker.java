e5a0a1cbe76115055222b349b2496eb2048e4aaf
Edward Turner <eddturner@ebi.ac.uk>
2017-01-27 18:09:05 +0000

    adding todo

--- annotation-common/src/test/java/uk/ac/ebi/quickgo/annotation/common/document/AnnotationDocMocker.java
+++ annotation-common/src/test/java/uk/ac/ebi/quickgo/annotation/common/document/AnnotationDocMocker.java
@@ -114,6 +114,6 @@ public class AnnotationDocMocker {
 
 
     public static String createGPId(int idNum) {
-        return String.format("UniProtKB:A0A%03d", idNum);
+        return String.format("UniProtKB:A0A%03d", idNum); // todo: check this doesn't affect other tests
     }
 }
