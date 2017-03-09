caac43c6fb5f6318e5ddc07ad5fd0a101fe61ce0
twardell <twardell@ebi.ac.uk>
2017-03-09 14:06:48 +0000

    Replace @Value with @ConfigurationProperties.

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/metadata/MetaDataConfig.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/metadata/MetaDataConfig.java
@@ -1,6 +1,7 @@
 package uk.ac.ebi.quickgo.annotation.metadata;
 
 import uk.ac.ebi.quickgo.rest.metadata.MetaData;
+import uk.ac.ebi.quickgo.rest.metadata.MetaDataConfigProperties;
 import uk.ac.ebi.quickgo.rest.metadata.MetaDataProvider;
 
 import java.io.IOException;
@@ -12,7 +13,7 @@ import java.util.function.Function;
 import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
@@ -34,9 +35,9 @@ public class MetaDataConfig {
     private static final Path DEFAULT_METADATA_PATH = Paths.get("goa_uniprot.gpa-version");
     private static final Function<Path, List<MetaData>> PATH_TO_METADATA_MAPPER = (Path p) -> {
         try {
-            Stream<String> stream =  Files.lines(p);
+            Stream<String> stream = Files.lines(p);
             return stream.map(s -> new MetaData(null, s.substring(s.indexOf("2"))))
-            .collect(toList());
+                         .collect(toList());
         } catch (IOException e) {
             throw new java.io.UncheckedIOException(e);
         }
@@ -44,19 +45,23 @@ public class MetaDataConfig {
 
     private static final int NUMBER_OF_METADATA_LINES = 1;
 
-    @Value("${annotation.metadata.source}")
-    private Resource source;
+    @Bean
+    @ConfigurationProperties(prefix = "annotation.metadata")
+    public MetaDataConfigProperties metaDataConfigProperties() {
+        return new MetaDataConfigProperties();
+    }
 
     @Bean
-    MetaDataProvider metaDataProvider()  {
+    MetaDataProvider metaDataProvider(MetaDataConfigProperties metaDataConfigProperties) {
         Path metaDataPath = DEFAULT_METADATA_PATH;
-       if (source != null ) {
-           try {
-               metaDataPath = Paths.get(source.getURI());
-           } catch (IOException e) {
-               LOGGER.error("Failed to get the URI of the metadata source " + source,e);
-           }
-       }
+        final Resource source = metaDataConfigProperties.getSource();
+        if (source != null) {
+            try {
+                metaDataPath = Paths.get(source.getURI());
+            } catch (IOException e) {
+                LOGGER.error("Failed to get the URI of the metadata source " + source, e);
+            }
+        }
         return new MetaDataProvider(SERVICE, PATH_TO_METADATA_MAPPER, metaDataPath, NUMBER_OF_METADATA_LINES);
     }
 }
