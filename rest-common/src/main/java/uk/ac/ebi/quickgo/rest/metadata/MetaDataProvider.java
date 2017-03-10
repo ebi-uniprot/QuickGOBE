96beca40fe11aa2a2545243d6564f057127d8537
twardell <twardell@ebi.ac.uk>
2017-03-10 16:40:29 +0000

    Replace MetaData with hardcoded properties to Map based solution.

--- rest-common/src/main/java/uk/ac/ebi/quickgo/rest/metadata/MetaDataProvider.java
+++ rest-common/src/main/java/uk/ac/ebi/quickgo/rest/metadata/MetaDataProvider.java
@@ -19,34 +19,23 @@ import static com.google.common.base.Preconditions.checkState;
  * Created with IntelliJ IDEA.
  */
 public class MetaDataProvider {
-    private final String service;
-    private final Function<Path, List<MetaData>> metaDataMapper;
+    private final Function<Path, MetaData> metaDataMapper;
     private final Path path;
-    private final int expectedNumberOfLines;
 
-    public MetaDataProvider(String service, Function<Path, List<MetaData>> metaDataMapper, Path path, int noOfLines) {
-        checkArgument(Objects.nonNull(service), "The name of the service for the meta data provider " +
-                "cannot be null.");
+    public MetaDataProvider(Function<Path, MetaData> metaDataMapper, Path path) {
         checkArgument(Objects.nonNull(metaDataMapper), "The metadata mapper cannot be null.");
         checkArgument(Objects.nonNull(path), "The path cannot be null.");
-        checkArgument(noOfLines > 0, "The expected number of lines of metadata for the " +
-                "service should not be greater zero.");
-        this.service = service;
         this.metaDataMapper = metaDataMapper;
         this.path = path;
-        this.expectedNumberOfLines = noOfLines;
     }
 
     /**
      * Lookup the MetaData for the selected service
      * @return MetaData instance
      */
-    public List<MetaData> lookupMetaData() {
+    public MetaData lookupMetaData() {
         try {
-            List<MetaData> metaLines = metaDataMapper.apply(path);
-            checkState(metaLines.size() == expectedNumberOfLines, "Unable to read the correct number of lines for " +
-                    "%s metadata", service);
-            return metaLines;
+            return metaDataMapper.apply(path);
         } catch (Exception e) {
             throw new ServiceConfigException(e);
         }
