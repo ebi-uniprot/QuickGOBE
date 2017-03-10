e03002d7e7d97595fa9496d38af68863b7798531
twardell <twardell@ebi.ac.uk>
2017-03-10 16:41:40 +0000

    Remove redundant tests.

--- rest-common/src/test/java/uk/ac/ebi/quickgo/rest/MetaDataProviderTest.java
+++ rest-common/src/test/java/uk/ac/ebi/quickgo/rest/MetaDataProviderTest.java
@@ -26,43 +26,24 @@ import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
  */
 public class MetaDataProviderTest {
 
-    private static final  Function<Path,List<MetaData>> FAKE_MAPPER = (Path p)
-                                                            -> Arrays.asList(new MetaData("1.0",""));
-    private static final String SERVICE_NAME = "Test Service";
+    private static final MetaData METADATA = new MetaData();
+    private static final  Function<Path,MetaData> FAKE_MAPPER = (Path p) -> METADATA;
     private static final Path PATH_TO_NOWHERE = Paths.get("/path/to/nowhere");
 
     @Test(expected = IllegalArgumentException.class)
-    public void serviceIsNullThrowsIllegalArgumentException(){
-        new MetaDataProvider(null, FAKE_MAPPER, PATH_TO_NOWHERE, 1 );
-    }
-
-    @Test(expected = IllegalArgumentException.class)
     public void mapperIsNullThrowsIllegalArgumentException(){
-        new MetaDataProvider(SERVICE_NAME, null, PATH_TO_NOWHERE, 1 );
+        new MetaDataProvider(null, PATH_TO_NOWHERE);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void rawStreamIsNullThrowsIllegalArgumentException(){
-        new MetaDataProvider(SERVICE_NAME, FAKE_MAPPER, null, 1 );
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void expectedNumberOfMetaDataLinesThrowsIllegalArgumentException(){
-        new MetaDataProvider(SERVICE_NAME, FAKE_MAPPER, PATH_TO_NOWHERE, 0 );
+    public void pathIsNullThrowsIllegalArgumentException(){
+        new MetaDataProvider(FAKE_MAPPER, null);
     }
 
     @Test
     public void readFileForMetaDataWithoutProblems() throws Exception{
-        MetaDataProvider metaDataProvider = new MetaDataProvider(SERVICE_NAME, FAKE_MAPPER, PATH_TO_NOWHERE, 1 );
-        List<MetaData> metaDataList = metaDataProvider.lookupMetaData();
-        assertThat(metaDataList, hasSize(1));
-        assertThat(metaDataList.get(0).timestamp, is(""));
-        assertThat(metaDataList.get(0).version, is("1.0"));
-    }
-
-    @Test(expected = ServiceConfigException.class)
-    public void readFileForMetaDataProducesUnexpectedNumberOfLines() throws Exception{
-        MetaDataProvider metaDataProvider = new MetaDataProvider(SERVICE_NAME, FAKE_MAPPER, PATH_TO_NOWHERE, 2 );
-        metaDataProvider.lookupMetaData();
+        MetaDataProvider metaDataProvider = new MetaDataProvider(FAKE_MAPPER, PATH_TO_NOWHERE);
+        MetaData md = metaDataProvider.lookupMetaData();
+        assertThat(md, is(METADATA));
     }
 }
