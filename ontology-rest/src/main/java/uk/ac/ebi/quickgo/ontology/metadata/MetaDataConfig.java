a6171fb1c77ff30eedc6bceb647dcf5a42728d1f
twardell <twardell@ebi.ac.uk>
2017-03-14 13:57:36 +0000

    Replace Ontology specific with generic MetaDataProvider.

--- ontology-rest/src/main/java/uk/ac/ebi/quickgo/ontology/metadata/MetaDataConfig.java
+++ ontology-rest/src/main/java/uk/ac/ebi/quickgo/ontology/metadata/MetaDataConfig.java
@@ -1,9 +1,18 @@
 package uk.ac.ebi.quickgo.ontology.metadata;
 
+import uk.ac.ebi.quickgo.common.loader.GZIPFiles;
+import uk.ac.ebi.quickgo.rest.metadata.MetaData;
+import uk.ac.ebi.quickgo.rest.metadata.MetaDataConfigProperties;
+import uk.ac.ebi.quickgo.rest.metadata.MetaDataProvider;
+import uk.ac.ebi.quickgo.rest.metadata.MetaDataStringOnly;
+
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import org.springframework.beans.factory.annotation.Value;
+import java.util.function.Function;
+import java.util.stream.Stream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,13 +29,40 @@ import org.springframework.core.io.Resource;
 @Configuration
 @ConfigurationProperties(prefix = "metadata.ontology")
 public class MetaDataConfig {
-    private static final Path DEFAULT_ONTOLOGY_PATH = Paths.get("ONTOLOGY_IRI.dat.gz");
-    @Value("${metadata.ontology.source}")
-    private Resource source;
+    private static final Logger LOGGER = LoggerFactory.getLogger(MetaDataConfig.class);
+    private static final Path DEFAULT_METADATA_PATH = Paths.get("ONTOLOGY_IRI.dat.gz");
+    private static final String SERVICE = "GO";
+    private static final Function<Path, MetaData> MAPPER = (Path p) -> {
+        try (Stream<String> stream = GZIPFiles.lines(p)) {
+            MetaDataStringOnly metaDataStringOnly = new MetaDataStringOnly();
+            stream.forEach(s -> {
+                String[] a = s.split("\t");
+                metaDataStringOnly.add(MetaData.VERSION, a[2]);
+                metaDataStringOnly.add(MetaData.TIMESTAMP, a[1]);
+            });
+            MetaData metaData = new MetaData();
+            metaData.add(SERVICE, metaDataStringOnly);
+            return metaData;
+        }
+    };
 
     @Bean
-    MetaDataProvider metaDataProvider() throws IOException {
-        Path osPath = source != null ? Paths.get(source.getURI()) : DEFAULT_ONTOLOGY_PATH;
-        return new MetaDataProvider(osPath);
+    @ConfigurationProperties(prefix = "ontology.metadata")
+    public MetaDataConfigProperties metaDataConfigProperties() {
+        return new MetaDataConfigProperties();
+    }
+
+    @Bean
+    MetaDataProvider metaDataProvider(MetaDataConfigProperties metaDataConfigProperties) throws IOException {
+        Path metaDataPath = DEFAULT_METADATA_PATH;
+        final Resource source = metaDataConfigProperties.getSource();
+        if (source != null) {
+            try {
+                metaDataPath = Paths.get(source.getURI());
+            } catch (IOException e) {
+                LOGGER.error("Failed to get the URI of the metadata source " + source, e);
+            }
+        }
+        return new MetaDataProvider(MAPPER, metaDataPath);
     }
 }
