ec653c14cfcfba2cfeed54cf41ea1935d358d9f3
twardell <twardell@ebi.ac.uk>
2017-03-15 13:33:41 +0000

    Lower case the service name in the /about endpoint.

--- annotation-rest/src/test/java/uk/ac/ebi/quickgo/annotation/controller/AnnotationControllerIT.java
+++ annotation-rest/src/test/java/uk/ac/ebi/quickgo/annotation/controller/AnnotationControllerIT.java
@@ -1547,7 +1547,7 @@ public class AnnotationControllerIT {
         ResultActions response = mockMvc.perform(get(RESOURCE_URL + "/about"));
 
         response.andDo(print())
-                .andExpect(jsonPath("$.Annotation.timestamp").value("2017-03-01 18:00"));
+                .andExpect(jsonPath("$.annotation.timestamp").value("2017-03-01 18:00"));
     }
 
     // ------------------------------- Helpers -------------------------------
