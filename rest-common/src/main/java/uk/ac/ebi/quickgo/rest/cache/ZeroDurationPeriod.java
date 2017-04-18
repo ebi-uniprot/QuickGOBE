546382538f9a9a8b8e8e36c6bde74ffa203b6fd0
twardell <twardell@ebi.ac.uk>
2017-04-18 11:49:51 +0100

    New class ZeroDurationPeriod always returns Duration.ZERO for remaining time.

---
+++ rest-common/src/main/java/uk/ac/ebi/quickgo/rest/cache/ZeroDurationPeriod.java
@@ -0,0 +1,17 @@
+package uk.ac.ebi.quickgo.rest.cache;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+/**
+ * @author Tony Wardell
+ * Date: 12/04/2017
+ * Time: 15:20
+ * Created with IntelliJ IDEA.
+ */
+public class ZeroDurationPeriod implements Period{
+
+    @Override public Duration remainingTime(LocalDateTime target) {
+        return Duration.ZERO;
+    }
+}
