191dd814d42719f2578055f0ff8183c4d31289e7
twardell <twardell@ebi.ac.uk>
2017-04-18 15:38:11 +0100

    From a collection of Periods, find the first one to cover this instant in time, and if one is found, find out how much time is left before this period ends.

---
+++ rest-common/src/main/java/uk/ac/ebi/quickgo/rest/cache/RemainingTimeSupplier.java
@@ -0,0 +1,44 @@
+package uk.ac.ebi.quickgo.rest.cache;
+
+import com.google.common.base.Preconditions;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import static java.util.Objects.nonNull;
+
+/**
+ * From a collection of {@link Period}s, find the first one to cover this instant in time, and if one is found,
+ * find out how much time is left before this period ends.
+ *
+ * @author Tony Wardell
+ * Date: 11/04/2017
+ * Time: 12:45
+ * Created with IntelliJ IDEA.
+ */
+public class RemainingTimeSupplier implements Supplier<Duration> {
+
+    private Collection<Period> periodCollection;
+
+    public RemainingTimeSupplier(Collection<Period> periodCollection) {
+        Preconditions.checkArgument(nonNull(periodCollection), "The collection of periods to check must not " +
+                "be null");
+        this.periodCollection = periodCollection;
+    }
+
+    /**
+     * Calculate and return the {@link Duration} between {@link LocalDateTime} 'now' and the {@link Period}s held by
+     * this instance. Return first non-zero Duration, or a Duration of Zero.
+     * @return Duration left of any active periods, if they exist.
+     */
+    public Duration get() {
+        LocalDateTime now = LocalDateTime.now();
+        Optional<Duration> remainingTime = periodCollection.stream()
+                                                           .map(p -> p.remainingTime(now))
+                                                           .filter(d -> !d.isZero())
+                                                           .findFirst();
+        return remainingTime.orElse(Duration.ZERO);
+    }
+}
---
+++ rest-common/src/test/java/uk/ac/ebi/quickgo/rest/cache/RemainingTimeSupplierTest.java
@@ -0,0 +1,83 @@
+package uk.ac.ebi.quickgo.rest.cache;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+
+/**
+ * Test RemainingTimeSupplier.
+ *
+ * @author Tony Wardell
+ * Date: 12/04/2017
+ * Time: 13:57
+ * Created with IntelliJ IDEA.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class RemainingTimeSupplierTest {
+
+    @Mock private
+    Period allowedPeriod;
+    private static final Duration ONE_HOUR = Duration.ofHours(1);
+
+    @Test
+    public void durationReturnedFromSinglePeriod() {
+        final Duration ONE_HOUR = Duration.ofHours(1);
+        when(allowedPeriod.remainingTime(any(LocalDateTime.class))).thenReturn(ONE_HOUR);
+
+        RemainingTimeSupplier remainingTimeSupplier =
+                new RemainingTimeSupplier(Collections.singletonList(allowedPeriod));
+
+        assertThat(remainingTimeSupplier.get(), is(ONE_HOUR));
+    }
+
+    @Test
+    public void durationReturnedFromThirdPeriod() {
+        when(allowedPeriod.remainingTime(any(LocalDateTime.class))).thenReturn(Duration.ZERO);
+        when(allowedPeriod.remainingTime(any(LocalDateTime.class))).thenReturn(Duration.ZERO);
+        when(allowedPeriod.remainingTime(any(LocalDateTime.class))).thenReturn(ONE_HOUR);
+
+        RemainingTimeSupplier remainingTimeSupplier = new RemainingTimeSupplier(Arrays.asList(allowedPeriod,
+                                                                                              allowedPeriod,
+                                                                                              allowedPeriod));
+
+        assertThat(remainingTimeSupplier.get(), is(ONE_HOUR));
+    }
+
+    @Test
+    public void noActivePeriodSoDurationIsZero() {
+        when(allowedPeriod.remainingTime(any(LocalDateTime.class))).thenReturn(Duration.ZERO);
+        when(allowedPeriod.remainingTime(any(LocalDateTime.class))).thenReturn(Duration.ZERO);
+        when(allowedPeriod.remainingTime(any(LocalDateTime.class))).thenReturn(Duration.ZERO);
+
+        RemainingTimeSupplier remainingTimeSupplier = new RemainingTimeSupplier(Arrays.asList(allowedPeriod,
+                                                                                              allowedPeriod,
+                                                                                              allowedPeriod));
+
+        assertThat(remainingTimeSupplier.get(), is(Duration.ZERO));
+    }
+
+    @Test
+    public void noDurationFoundFromEmptyPeriodList() {
+        RemainingTimeSupplier remainingTimeSupplier = new RemainingTimeSupplier(new ArrayList<>());
+
+        final Duration timeLeft = remainingTimeSupplier.get();
+
+        assertThat(timeLeft, is(Duration.ZERO));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void collectionOfPeriodsWhenNullThrowsException() {
+        new RemainingTimeSupplier(null);
+    }
+}
