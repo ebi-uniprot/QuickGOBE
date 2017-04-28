3ef2b6a73c7862168c5470d0f20c1d425611a048
twardell <twardell@ebi.ac.uk>
2017-04-28 11:26:34 +0100

    Use MediaTypeFactory for MediaType definintions. Add TSVMandatoryFieldMatcher.

--- annotation-rest/src/test/java/uk/ac/ebi/quickgo/annotation/controller/DownloadResponseVerifier.java
+++ annotation-rest/src/test/java/uk/ac/ebi/quickgo/annotation/controller/DownloadResponseVerifier.java
@@ -1,7 +1,6 @@
 package uk.ac.ebi.quickgo.annotation.controller;
 
-import uk.ac.ebi.quickgo.annotation.download.http.GAFHttpMessageConverter;
-import uk.ac.ebi.quickgo.annotation.download.http.GPADHttpMessageConverter;
+import uk.ac.ebi.quickgo.annotation.download.http.MediaTypeFactory;
 
 import java.util.List;
 import org.hamcrest.Description;
@@ -21,17 +20,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * Created 30/01/17
  * @author Edd
  */
-public class DownloadResponseVerifier {
+class DownloadResponseVerifier {
     private static final Logger LOGGER = getLogger(DownloadResponseVerifier.class);
 
     private DownloadResponseVerifier() {}
 
     static ResultMatcher nonNullMandatoryFieldsExist(MediaType mediaType) {
         Matcher<String> fieldMatcher;
-        if (mediaType.getSubtype().equals(GAFHttpMessageConverter.SUB_TYPE)) {
+        if (mediaType.getSubtype().equals(MediaTypeFactory.GAF_SUB_TYPE)) {
             fieldMatcher = new GAFMandatoryFieldMatcher();
-        } else if (mediaType.getSubtype().equals(GPADHttpMessageConverter.SUB_TYPE)) {
+        } else if (mediaType.getSubtype().equals(MediaTypeFactory.GPAD_SUB_TYPE)) {
             fieldMatcher = new GPADMandatoryFieldMatcher();
+        } else if (mediaType.getSubtype().equals(MediaTypeFactory.TSV_SUB_TYPE)) {
+            fieldMatcher = new TSVMandatoryFieldMatcher();
         } else {
             throw new IllegalArgumentException("Unknown media type: " + mediaType);
         }
@@ -86,6 +87,37 @@ public class DownloadResponseVerifier {
                     for (Integer mandatoryIndex : MANDATORY_INDICES) {
                         if (components[mandatoryIndex].isEmpty()) {
                             LOGGER.error("Mandatory GPAD index should not be empty: " + mandatoryIndex);
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+    }
+
+    static class TSVMandatoryFieldMatcher extends TypeSafeMatcher<String> {
+        private static final int FIELD_COUNT = 11;
+        private static final String TYPE = "TSV";
+        private static final List<Integer> MANDATORY_INDICES = asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        @Override public void describeTo(Description description) {
+            description.appendText("mandatory indices were not populated: " + MANDATORY_INDICES);
+        }
+
+        @Override protected boolean matchesSafely(String s) {
+            String[] lines = s.split("\n");
+            for (String line : lines) {
+                if (!line.startsWith("!")) {
+                    String[] components = line.split("\t");
+                    if (components.length != FIELD_COUNT) {
+                        LOGGER.error(TYPE + " line should contain " + FIELD_COUNT + " fields, but found: " + components
+                                .length);
+                        return false;
+                    }
+                    for (Integer mandatoryIndex : MANDATORY_INDICES) {
+                        if (components[mandatoryIndex].isEmpty()) {
+                            LOGGER.error("Mandatory " + TYPE + " index should not be empty: " + mandatoryIndex);
                             return false;
                         }
                     }
