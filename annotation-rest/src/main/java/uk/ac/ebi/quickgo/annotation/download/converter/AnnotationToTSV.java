35c9ddc2c9ee0ebf953f2a8fc4923e55763d2466
twardell <twardell@ebi.ac.uk>
2017-05-09 13:30:37 +0100

    Create output for with and without slimmed ids. Prevent synchronization issues with Date formatter.

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/download/converter/AnnotationToTSV.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/download/converter/AnnotationToTSV.java
@@ -2,6 +2,7 @@ package uk.ac.ebi.quickgo.annotation.download.converter;
 
 import uk.ac.ebi.quickgo.annotation.model.Annotation;
 
+import java.text.SimpleDateFormat;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
@@ -23,29 +24,75 @@ import static java.util.stream.Collectors.toList;
  */
 public class AnnotationToTSV extends AnnotationTo implements Function<Annotation, List<String>> {
 
+    public static final String YEAR_MONTH_DAY = "yyyyMMdd";
+
     @Override public List<String> apply(Annotation annotation) {
         if (Objects.isNull(annotation.slimmedIds) || annotation.slimmedIds.isEmpty()) {
-            return Collections.singletonList(toOutputRecord(annotation, annotation.goId));
+            return Collections.singletonList(toOutputRecord(annotation));
         } else {
             return annotation.slimmedIds.stream()
-                                        .map(goId -> this.toOutputRecord(annotation, goId))
+                                        .map(goId -> this.toSlimmedOutputRecord(annotation, goId))
                                         .collect(toList());
         }
     }
 
-    private String toOutputRecord(Annotation annotation, String goId) {
+    private String toOutputRecord(Annotation annotation) {
         StringJoiner tsvJoiner = new StringJoiner(OUTPUT_DELIMITER);
-        return tsvJoiner.add(annotation.geneProductId)
+        //todo replace annotation.date with annotation.dateTime,
+        //todo then we can replace DateFormat with DateTimeFormatter from java 8
+        //todo DateFormat is not thread safe so we cannot use an instance variable
+        final SimpleDateFormat dateFormat = new SimpleDateFormat(YEAR_MONTH_DAY);
+        //todo missing the following fields that come from other services yet to be plugged in
+        //todo name, synonym, type from the gene product service
+        return tsvJoiner.add(nullToEmptyString.apply(annotation.geneProductId))
+                        .add(nullToEmptyString.apply(annotation.symbol))
+                        .add(nullToEmptyString.apply(annotation.qualifier))
+                        .add(nullToEmptyString.apply(annotation.goId))
+                        .add(nullToEmptyString.apply(annotation.goName))
+                        .add(determineEvidence(annotation))
+                        .add(nullToEmptyString.apply(annotation.reference))
+                        .add(withFromAsString(annotation.withFrom))
+                        .add(annotation.taxonId == 0 ? "" : Integer.toString(annotation.taxonId))
+                        .add(nullToEmptyString.apply(annotation.assignedBy))
+                        .add(extensionsAsString(annotation.extensions))
+                        .add(annotation.date == null? "":dateFormat.format(annotation.date))
+                        .add(nullToEmptyString.apply(annotation.taxonName))
+                        .toString();
+    }
+
+    private String determineEvidence(Annotation annotation) {
+        StringBuilder evidenceBuilder = new StringBuilder();
+        evidenceBuilder.append(nullToEmptyString.apply((annotation.evidenceCode)));
+        if ((annotation.goEvidence != null) && !annotation.goEvidence.isEmpty()) {
+            evidenceBuilder.append("(" + annotation.goEvidence + ")");
+        }
+        return evidenceBuilder.toString();
+    }
+
+    private String toSlimmedOutputRecord(Annotation annotation, String goId) {
+        StringJoiner tsvJoiner = new StringJoiner(OUTPUT_DELIMITER);
+        //todo replace annotation.date with annotation.dateTime,
+        //todo then we can replace DateFormat with DateTimeFormatter from java 8
+        //todo DateFormat is not thread safe so we cannot use an instance variable
+        final SimpleDateFormat dateFormat = new SimpleDateFormat(YEAR_MONTH_DAY);
+        //todo missing the following fields that come from other services yet to be plugged in
+        //todo name, synonym, type from the gene product service
+        return tsvJoiner.add(nullToEmptyString.apply(annotation.geneProductId))
                         .add(nullToEmptyString.apply(annotation.symbol))
                         .add(nullToEmptyString.apply(annotation.qualifier))
                         .add(nullToEmptyString.apply(goId))
                         .add(nullToEmptyString.apply(annotation.goName))
+                        .add(nullToEmptyString.apply(annotation.goId))
                         .add(nullToEmptyString.apply(annotation.evidenceCode + " (" + annotation.goEvidence + ")"))
                         .add(nullToEmptyString.apply(annotation.reference))
                         .add(withFromAsString(annotation.withFrom))
                         .add(Integer.toString(annotation.taxonId))
                         .add(nullToEmptyString.apply(annotation.assignedBy))
-                        .add(extensionsAsString(annotation.extensions)).toString();
+                        .add(extensionsAsString(annotation.extensions))
+                        .add(nullToEmptyString.apply(dateFormat.format(annotation.date)))
+                        .add(nullToEmptyString.apply(annotation.taxonName))
+                        .toString();
+
     }
 
 }
