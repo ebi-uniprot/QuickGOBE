375cec1918aa932e957a075eb428988714bfee86
twardell <twardell@ebi.ac.uk>
2017-05-24 15:30:50 +0100

    Makes use of selectedFields.

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/download/header/TsvHeaderCreator.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/download/header/TsvHeaderCreator.java
@@ -1,46 +1,57 @@
 package uk.ac.ebi.quickgo.annotation.download.header;
 
-import uk.ac.ebi.quickgo.common.loader.GZIPFiles;
-
 import com.google.common.base.Preconditions;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.attribute.FileTime;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Collections;
+
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
-import javax.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.StringJoiner;
+
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 
-import static java.util.Arrays.stream;
-import static uk.ac.ebi.quickgo.annotation.download.http.MediaTypeFactory.GAF_SUB_TYPE;
-import static uk.ac.ebi.quickgo.annotation.download.http.MediaTypeFactory.GPAD_SUB_TYPE;
-import static uk.ac.ebi.quickgo.annotation.download.http.MediaTypeFactory.TSV_SUB_TYPE;
-
 /**
- * Produce a header for TSV downloaded files. Only the column names are required.
+ * Produce a header for TSV downloaded files. Only the (selected) column names are required. Or all columns if none
+ * are selected.
  *
  * @author Tony Wardell
  * Date: 25/01/2017
  * Time: 10:09
  * Created with IntelliJ IDEA.
  */
-@Component
 public class TsvHeaderCreator implements HeaderCreator{
-    static final String TSV_COL_HEADINGS_INCLUDING_SLIM = "GENE PRODUCT\tSYMBOL\tQUALIFIER\tGO TERM\tGO TERM " +
-            "NAME\tSLIMMED FROM\tEVIDENCE\tREFERENCE\tWITH/FROM\tTAXON\tASSIGNED BY\tANNOTATION EXTENSION" +
-            "\tDATE\tTAXON NAME";
-    static final String TSV_COL_HEADINGS_EXCLUDING_SLIM = "GENE PRODUCT\tSYMBOL\tQUALIFIER\tGO TERM\tGO TERM " +
-            "NAME\tEVIDENCE\tREFERENCE\tWITH/FROM\tTAXON\tASSIGNED BY\tANNOTATION EXTENSION\tDATE\tTAXON NAME";
 
+    private static final String OUTPUT_DELIMITER = "\t";
+    static final String GENE_PRODUCT_ID = "GENE PRODUCT ID";
+    static final String SYMBOL = "SYMBOL";
+    static final String QUALIFIER = "QUALIFIER";
+    static final String GO_TERM = "GO TERM";
+    static final String GO_NAME = "GO NAME";
+    static final String SLIMMED_FROM = "SLIMMED FROM";
+    static final String ECO_ID = "ECO_ID";
+    static final String GO_EVIDENCE_CODE = "GO EVIDENCE CODE";
+    static final String REFERENCE = "REFERENCE";
+    static final String WITH_FROM = "WITH/FROM";
+    static final String TAXON_ID = "TAXON ID";
+    static final String ASSIGNED_BY = "ASSIGNED BY";
+    static final String ANNOTATION_EXTENSION = "ANNOTATION EXTENSION";
+    static final String DATE = "DATE";
+    static final String TAXON_NAME = "TAXON NAME";
+
+    public static final String GENE_PRODUCT_ID_FIELD_NAME = "geneProductId";
+    public static final String SYMBOL_FIELD_NAME = "symbol";
+    public static final String QUALIFIER_FIELD_NAME = "qualifier";
+    public static final String GO_TERM_FIELD_NAME = "goId";
+    public static final String GO_NAME_FIELD_NAME = "goName";
+    public static final String ECO_ID_FIELD_NAME = "evidenceCode";
+    public static final String GO_EVIDENCE_CODE_FIELD_NAME = "goEvidence";
+    public static final String REFERENCE_FIELD_NAME = "reference";
+    public static final String WITH_FROM_FIELD_NAME = "withFrom";
+    public static final String TAXON_ID_FIELD_NAME = "taxonId";
+    public static final String ASSIGNED_BY_FIELD_NAME = "assignedBy";
+    public static final String ANNOTATION_EXTENSION_FIELD_NAME = "extensions";
+    public static final String DATE_FIELD_NAME = "date";
+    public static final String TAXON_NAME_FIELD_NAME = "taxonName";
     /**
      * Write the contents of the header to the ResponseBodyEmitter instance.
      * @param emitter streams the header content to the client
@@ -59,6 +70,55 @@ public class TsvHeaderCreator implements HeaderCreator{
     }
 
     private String colHeadings(HeaderContent headerContent) {
-       return headerContent.isSlimmed()? TSV_COL_HEADINGS_INCLUDING_SLIM : TSV_COL_HEADINGS_EXCLUDING_SLIM;
+        StringJoiner tsvJoiner = new StringJoiner(OUTPUT_DELIMITER);
+        //todo missing the following fields that come from other services yet to be plugged in
+        //todo name, synonym, type from the gene product service
+        List<String> selectedFields = headerContent.selectedFields();
+        if (selectedFields.isEmpty() || selectedFields.contains(GENE_PRODUCT_ID_FIELD_NAME)) {
+            tsvJoiner.add(GENE_PRODUCT_ID);
+        }
+        if (selectedFields.isEmpty() || selectedFields.contains(SYMBOL_FIELD_NAME)) {
+            tsvJoiner.add(SYMBOL);
+        }
+        if (selectedFields.isEmpty() || selectedFields.contains(QUALIFIER_FIELD_NAME)) {
+            tsvJoiner.add(QUALIFIER);
+        }
+        if (selectedFields.isEmpty() || selectedFields.contains(GO_TERM_FIELD_NAME)) {
+            tsvJoiner.add(GO_TERM);
+        }
+        if (headerContent.isSlimmed()) {
+            tsvJoiner.add(SLIMMED_FROM);
+        }
+        if (selectedFields.isEmpty() || selectedFields.contains(GO_NAME_FIELD_NAME)) {
+            tsvJoiner.add(GO_NAME);
+        }
+        if (selectedFields.isEmpty() || selectedFields.contains(ECO_ID_FIELD_NAME)) {
+            tsvJoiner.add(ECO_ID);
+        }
+        if (selectedFields.isEmpty() || selectedFields.contains(GO_EVIDENCE_CODE_FIELD_NAME)) {
+            tsvJoiner.add(GO_EVIDENCE_CODE);
+        }
+        if (selectedFields.isEmpty() || selectedFields.contains(REFERENCE_FIELD_NAME)) {
+            tsvJoiner.add(REFERENCE);
+        }
+        if (selectedFields.isEmpty() || selectedFields.contains(WITH_FROM_FIELD_NAME)) {
+            tsvJoiner.add(WITH_FROM);
+        }
+        if (selectedFields.isEmpty() || selectedFields.contains(TAXON_ID_FIELD_NAME)) {
+            tsvJoiner.add(TAXON_ID);
+        }
+        if (selectedFields.isEmpty() || selectedFields.contains(ASSIGNED_BY_FIELD_NAME)) {
+            tsvJoiner.add(ASSIGNED_BY);
+        }
+        if (selectedFields.isEmpty() || selectedFields.contains(ANNOTATION_EXTENSION_FIELD_NAME)) {
+            tsvJoiner.add(ANNOTATION_EXTENSION);
+        }
+        if (selectedFields.isEmpty() || selectedFields.contains(DATE_FIELD_NAME)) {
+            tsvJoiner.add(DATE);
+        }
+        if (selectedFields.isEmpty() || selectedFields.contains(TAXON_NAME_FIELD_NAME)) {
+            tsvJoiner.add(TAXON_NAME);
+        }
+        return tsvJoiner.toString();
     }
 }
