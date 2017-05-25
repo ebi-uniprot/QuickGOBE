9d4305d72a3697d669c31074295b2a9bf83b7f7f
twardell <twardell@ebi.ac.uk>
2017-05-25 15:42:02 +0100

    Replace field names with import from AnnotationToTSV.

--- annotation-rest/src/test/java/uk/ac/ebi/quickgo/annotation/download/converter/AnnotationToTSVTest.java
+++ annotation-rest/src/test/java/uk/ac/ebi/quickgo/annotation/download/converter/AnnotationToTSVTest.java
@@ -14,7 +14,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
-import static uk.ac.ebi.quickgo.annotation.download.header.TsvHeaderCreator.*;
+import static uk.ac.ebi.quickgo.annotation.download.converter.AnnotationToTSV.*;
 import static uk.ac.ebi.quickgo.annotation.model.AnnotationMocker.*;
 
 /**
