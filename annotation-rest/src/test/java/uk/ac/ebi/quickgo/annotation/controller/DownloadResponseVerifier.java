78f4760422ed87585401f3289858462d988a4f28
twardell <twardell@ebi.ac.uk>
2017-05-25 14:09:03 +0100

    Don't check header line for TSV.

--- annotation-rest/src/test/java/uk/ac/ebi/quickgo/annotation/controller/DownloadResponseVerifier.java
+++ annotation-rest/src/test/java/uk/ac/ebi/quickgo/annotation/controller/DownloadResponseVerifier.java
@@ -2,6 +2,7 @@ package uk.ac.ebi.quickgo.annotation.controller;
 
 import uk.ac.ebi.quickgo.annotation.download.http.MediaTypeFactory;
 
+import java.util.Arrays;
 import java.util.List;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
@@ -97,17 +98,18 @@ class DownloadResponseVerifier {
     }
 
     static class TSVMandatoryFieldMatcher extends TypeSafeMatcher<String> {
-        private static final int FIELD_COUNT = 11;
+        private static final int FIELD_COUNT = 13;
         private static final String TYPE = "TSV";
-        private static final List<Integer> MANDATORY_INDICES = asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+        private static final List<Integer> MANDATORY_INDICES = asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
 
         @Override public void describeTo(Description description) {
             description.appendText("mandatory indices were not populated: " + MANDATORY_INDICES);
         }
 
         @Override protected boolean matchesSafely(String s) {
-            String[] lines = s.split("\n");
-            for (String line : lines) {
+            String[] allLines = s.split("\n");
+            String[] dataLines = Arrays.copyOfRange(allLines, 1, allLines.length-1 );
+            for (String line : dataLines) {
                 if (!line.startsWith("!")) {
                     String[] components = line.split("\t");
                     if (components.length != FIELD_COUNT) {
