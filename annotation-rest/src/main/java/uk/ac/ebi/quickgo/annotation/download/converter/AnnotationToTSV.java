06469d3c3f30bf6f8c8523f6c772758539e39714
twardell <twardell@ebi.ac.uk>
2017-05-26 12:09:45 +0100

    Write output in selected fields order.

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/download/converter/AnnotationToTSV.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/download/converter/AnnotationToTSV.java
@@ -3,13 +3,9 @@ package uk.ac.ebi.quickgo.annotation.download.converter;
 import uk.ac.ebi.quickgo.annotation.model.Annotation;
 
 import java.text.SimpleDateFormat;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
-import java.util.StringJoiner;
+import java.util.*;
+import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static java.util.stream.Collectors.toList;
 
@@ -44,76 +40,94 @@ public class AnnotationToTSV extends AnnotationTo implements BiFunction<Annotati
     public static final String DATE_FIELD_NAME = "date";
     public static final String TAXON_NAME_FIELD_NAME = "taxonname";
 
+    private static final List<String> FULL_FIELD_LIST = Arrays.asList(GENE_PRODUCT_ID_FIELD_NAME, SYMBOL_FIELD_NAME,
+                                                                    QUALIFIER_FIELD_NAME, GO_TERM_FIELD_NAME,
+                                                                    GO_NAME_FIELD_NAME, ECO_ID_FIELD_NAME,
+                                                                    GO_EVIDENCE_CODE_FIELD_NAME,
+                                                                    REFERENCE_FIELD_NAME, WITH_FROM_FIELD_NAME,
+                                                                    TAXON_ID_FIELD_NAME, ASSIGNED_BY_FIELD_NAME,
+                                                                    ANNOTATION_EXTENSION_FIELD_NAME, DATE_FIELD_NAME,
+                                                                    TAXON_NAME_FIELD_NAME);
+
     private static final String YEAR_MONTH_DAY = "yyyyMMdd";
-    private Logger LOGGER = LoggerFactory.getLogger(AnnotationToTSV.class);
 
-    @Override public List<String> apply(Annotation annotation, List<String> selectedFields) {
-        if (Objects.isNull(annotation.slimmedIds) || annotation.slimmedIds.isEmpty()) {
-            return Collections.singletonList(toOutputRecord(annotation, selectedFields,null));
-        } else {
-            return annotation.slimmedIds.stream()
-                                        .map(slimmedToGoId -> this.toOutputRecord(annotation, selectedFields, slimmedToGoId))
-                                        .collect(toList());
-        }
+    private Map<String, BiConsumer<OutputContent, StringJoiner>> selected2Content;
 
-    }
-
-    private String toOutputRecord(Annotation annotation, List<String> selectedFields, String slimmedToGoId) {
-
-        LOGGER.info("Converting annotation to TSV output record to download.");
-
-        StringJoiner tsvJoiner = new StringJoiner(OUTPUT_DELIMITER);
+    public AnnotationToTSV() {
+        selected2Content = new HashMap<>();
+        selected2Content.put(GENE_PRODUCT_ID_FIELD_NAME, (c, j) -> j.add(nullToEmptyString.apply(c.annotation
+                                                                                                         .geneProductId)));
+        selected2Content.put(SYMBOL_FIELD_NAME, (c, j) -> j.add(nullToEmptyString.apply(c.annotation.symbol)));
+        selected2Content.put(QUALIFIER_FIELD_NAME, (c, j) -> j.add(nullToEmptyString.apply(c.annotation.qualifier)));
+        selected2Content.put(GO_TERM_FIELD_NAME, (c, j) -> {
+            if (Objects.nonNull(c.slimmedToGoId)) {
+                j.add(c.slimmedToGoId);
+            }
+            j.add(nullToEmptyString.apply(c.annotation.goId));
+        });
+        selected2Content.put(GO_NAME_FIELD_NAME, (c, j) -> j.add(nullToEmptyString.apply(c.annotation.goName)));
+        selected2Content.put(ECO_ID_FIELD_NAME, (c, j) -> j.add(nullToEmptyString.apply(c.annotation.evidenceCode)));
+        selected2Content.put(GO_EVIDENCE_CODE_FIELD_NAME, (c, j) -> j.add(nullToEmptyString.apply(c.annotation
+                                                                                                          .goEvidence)));
+        selected2Content.put(REFERENCE_FIELD_NAME, (c, j) -> j.add(nullToEmptyString.apply(c.annotation.reference)));
+        selected2Content.put(WITH_FROM_FIELD_NAME, (c, j) -> j.add(withFromAsString(c.annotation.withFrom)));
+        selected2Content.put(TAXON_ID_FIELD_NAME, (c, j) -> j.add(c.annotation.taxonId == 0 ? "" : Integer.toString
+                (c.annotation.taxonId)));
+        selected2Content.put(ASSIGNED_BY_FIELD_NAME, (c, j) -> j.add(nullToEmptyString.apply(c.annotation
+                                                                                                     .assignedBy)));
+        selected2Content.put(ANNOTATION_EXTENSION_FIELD_NAME, (c, j) -> j.add(extensionsAsString(c.annotation
+                                                                                                         .extensions)));
         //todo replace annotation.date with annotation.dateTime,
         //todo then we can replace DateFormat with DateTimeFormatter from java 8
         //todo DateFormat is not thread safe so we cannot use an instance variable
-        final SimpleDateFormat dateFormat = new SimpleDateFormat(YEAR_MONTH_DAY);
-        //todo missing the following fields that come from other services yet to be plugged in
-        //todo name, synonym, type from the gene product service
-        if (selectedFields.isEmpty() || selectedFields.contains(GENE_PRODUCT_ID_FIELD_NAME)) {
-            tsvJoiner.add(nullToEmptyString.apply(annotation.geneProductId));
+        selected2Content.put(DATE_FIELD_NAME,
+                             (c, j) -> {
+                                 final SimpleDateFormat dateFormat = new SimpleDateFormat(YEAR_MONTH_DAY);
+                                 j.add(c.annotation.date == null ? "" : dateFormat.format(c.annotation.date));
+                             });
+        selected2Content.put(TAXON_NAME_FIELD_NAME, (c, j) -> j.add(nullToEmptyString.apply(c.annotation.taxonName)));
+        selected2Content.put(SYMBOL_FIELD_NAME, (c, j) -> j.add(nullToEmptyString.apply(c.annotation.symbol)));
+        selected2Content.put(SYMBOL_FIELD_NAME, (c, j) -> j.add(nullToEmptyString.apply(c.annotation.symbol)));
+        selected2Content.put(SYMBOL_FIELD_NAME, (c, j) -> j.add(nullToEmptyString.apply(c.annotation.symbol)));
+
+    }
+
+    @Override public List<String> apply(Annotation annotation, List<String> selectedFields) {
+        final List<String> columns = whichColumnsWillWeShow(selectedFields);
+        if (isSlimmedRequest(annotation)) {
+            return Collections.singletonList(output(new OutputContent(annotation, columns,null)));
+        } else {
+            return annotation.slimmedIds.stream()
+                                        .map(goId -> output(new OutputContent(annotation, columns, goId)))
+                                        .collect(toList());
         }
-        if (selectedFields.isEmpty() || selectedFields.contains(SYMBOL_FIELD_NAME)) {
-            tsvJoiner.add(nullToEmptyString.apply(annotation.symbol));
-        }
-        if (selectedFields.isEmpty() || selectedFields.contains(QUALIFIER_FIELD_NAME)) {
-            tsvJoiner.add(nullToEmptyString.apply(annotation.qualifier));
-        }
-        if (selectedFields.isEmpty() || selectedFields.contains(GO_TERM_FIELD_NAME)) {
-            if (Objects.nonNull(slimmedToGoId)) {
-                tsvJoiner.add(slimmedToGoId);
-            }
-            tsvJoiner.add(nullToEmptyString.apply(annotation.goId));
-        }
-        if (selectedFields.isEmpty() || selectedFields.contains(GO_NAME_FIELD_NAME)) {
-            tsvJoiner.add(nullToEmptyString.apply(annotation.goName));
-        }
-        if (selectedFields.isEmpty() || selectedFields.contains(ECO_ID_FIELD_NAME)) {
-            tsvJoiner.add(nullToEmptyString.apply(annotation.evidenceCode));
-        }
-        if (selectedFields.isEmpty() || selectedFields.contains(GO_EVIDENCE_CODE_FIELD_NAME)) {
-            tsvJoiner.add(nullToEmptyString.apply(annotation.goEvidence));
-        }
-        if (selectedFields.isEmpty() || selectedFields.contains(REFERENCE_FIELD_NAME)) {
-            tsvJoiner.add(nullToEmptyString.apply(annotation.reference));
-        }
-        if (selectedFields.isEmpty() || selectedFields.contains(WITH_FROM_FIELD_NAME)) {
-            tsvJoiner.add(withFromAsString(annotation.withFrom));
-        }
-        if (selectedFields.isEmpty() || selectedFields.contains(TAXON_ID_FIELD_NAME)) {
-            tsvJoiner.add(annotation.taxonId == 0 ? "" :Integer.toString(annotation.taxonId));
-        }
-        if (selectedFields.isEmpty() || selectedFields.contains(ASSIGNED_BY_FIELD_NAME)) {
-            tsvJoiner.add(nullToEmptyString.apply(annotation.assignedBy));
-        }
-        if (selectedFields.isEmpty() || selectedFields.contains(ANNOTATION_EXTENSION_FIELD_NAME)) {
-            tsvJoiner.add(extensionsAsString(annotation.extensions));
-        }
-        if (selectedFields.isEmpty() || selectedFields.contains(DATE_FIELD_NAME)) {
-            tsvJoiner.add(annotation.date == null ?"" : dateFormat.format(annotation.date));
-        }
-        if (selectedFields.isEmpty() || selectedFields.contains(TAXON_NAME_FIELD_NAME)) {
-            tsvJoiner.add(nullToEmptyString.apply(annotation.taxonName));
+    }
+
+    private boolean isSlimmedRequest(Annotation annotation) {
+        return Objects.isNull(annotation.slimmedIds) || annotation.slimmedIds.isEmpty();
+    }
+
+    private List<String> whichColumnsWillWeShow(List<String> selectedFields) {
+        return selectedFields.isEmpty() ? FULL_FIELD_LIST : selectedFields;
+    }
+
+    private String output(OutputContent outputContent) {
+        StringJoiner tsvJoiner = new StringJoiner(OUTPUT_DELIMITER);
+        for (String selectedField : outputContent.selectedFields) {
+            selected2Content.get(selectedField).accept(outputContent, tsvJoiner);
         }
         return tsvJoiner.toString();
+    }
+
+    private static class OutputContent{
+        Annotation annotation;
+        List<String> selectedFields;
+        String slimmedToGoId;
+
+        private OutputContent(Annotation annotation, List<String> selectedFields, String slimmedToGoId) {
+            this.annotation = annotation;
+            this.selectedFields = selectedFields;
+            this.slimmedToGoId = slimmedToGoId;
+        }
     }
 }
