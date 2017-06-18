47bd5a435b384f4876bcb80f767c8e442ca67352
TonyWardell <tonyandamie@yahoo.co.uk>
2017-06-18 22:44:33 +0100

    An alternative to recursion.

---
+++ ontology-rest/src/main/java/uk/ac/ebi/quickgo/ontology/traversal/Trampoline.java
@@ -0,0 +1,30 @@
+package uk.ac.ebi.quickgo.ontology.traversal;
+
+import java.util.Optional;
+
+/**
+ * An alternative to recursion.
+ * <p>
+ * https://stackoverflow.com/questions/32685660/achieving-stackless-recursion-in-java-8
+ * <p>
+ * Created by Tony on 17-Jun-17.
+ */
+public class Trampoline<T> {
+    public T getValue() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public Optional<Trampoline<T>> nextTrampoline() {
+        return Optional.empty();
+    }
+
+    public final T compute() {
+        Trampoline<T> trampoline = this;
+
+        while (trampoline.nextTrampoline().isPresent()) {
+            trampoline = trampoline.nextTrampoline().get();
+        }
+
+        return trampoline.getValue();
+    }
+}
