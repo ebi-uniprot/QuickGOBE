d0bb9df0758b6e34066dacdfc57fbe3bfee6eaf8
twardell <twardell@ebi.ac.uk>
2017-06-26 11:59:20 +0100

    Add gene product fields sourced from external restful service.

---
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/service/comm/rest/geneproduct/model/BasicGeneProduct.java
@@ -0,0 +1,77 @@
+package uk.ac.ebi.quickgo.annotation.service.comm.rest.geneproduct.model;
+
+import java.util.List;
+
+/**
+ * Represents part of the model corresponding to the response available from the resource:
+ *
+ * <ul>
+ *     <li>/geneproduct/{id}</li>
+ * </ul>
+ *
+ * This model captures the parts reached by the JSON path expression, "$.results.name; $.results.synonyms;".
+ * @author Tony Wardell
+ * Date: 23/06/2017
+ * Time: 15:07
+ * Created with IntelliJ IDEA.
+ */
+public class BasicGeneProduct {
+
+    private List<Result> results;
+
+    public BasicGeneProduct() {}
+
+    public List<Result> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Result> results) {
+        this.results = results;
+    }
+
+    @Override public String toString() {
+        return "ResponseType{" +
+                "results=" + results +
+                '}';
+    }
+
+    public static class Result {
+        private String id;
+        private String name;
+        private List<String> synonyms;
+
+        public Result() {}
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public List<String> getSynonyms() {
+            return synonyms;
+        }
+
+        public void setSynonyms(List<String> synonyms) {
+            this.synonyms = synonyms;
+        }
+
+        @Override public String toString() {
+            return "Result{" +
+                    "id='" + id + '\'' +
+                    ", name='" + name + '\'' +
+                    ", synonyms=" + synonyms +
+                    '}';
+        }
+    }
+}
