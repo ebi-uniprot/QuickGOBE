6e4fd0413d80dafddbe91ae67635cc3582ca63db
Edward Turner <eddturner@ebi.ac.uk>
2017-07-11 15:07:55 +0100

    adjust when repo contents are deleted

--- annotation-rest/src/test/java/uk/ac/ebi/quickgo/annotation/controller/AnnotationTransformerControllerIT.java
+++ annotation-rest/src/test/java/uk/ac/ebi/quickgo/annotation/controller/AnnotationTransformerControllerIT.java
@@ -96,10 +96,7 @@ public class AnnotationTransformerControllerIT {
                 .build();
         mockRestServiceServer = MockRestServiceServer.createServer((RestTemplate) restOperations);
         dtoMapper = new ObjectMapper();
-    }
 
-    @After
-    public void deleteCoreContents() {
         annotationRepository.deleteAll();
     }
 
@@ -164,7 +161,6 @@ public class AnnotationTransformerControllerIT {
     // ----------------- taxonomy name values -----------------
     @Test
     public void requestOmittingTaxonNameProducesResultWhereTaxonNameIsNull() throws Exception {
-        annotationRepository.deleteAll();
         annotationRepository.save(annotationDocWithTaxon(goId(1), taxonId(1)));
 
         expectTaxonIdHasGivenTaxonNameViaRest(taxonId(1), taxonName(1));
