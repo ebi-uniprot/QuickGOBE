8f25c8a398bfcbed4ae117f66c781ad807229fa7
Edward Turner <eddturner@ebi.ac.uk>
2017-08-17 11:07:47 +0100

    Add equals method to RequiredStatisticsType, and reduce code in accompanying tests; but retain verbosity of tests when necessary.

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/service/statistics/RequiredStatisticType.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/service/statistics/RequiredStatisticType.java
@@ -15,7 +15,7 @@ import static org.slf4j.LoggerFactory.getLogger;
  * Each {@link RequiredStatisticType} can have a {@code limit} set, indicating how many of each type to be
  * shown once the statistics calculation has been performed. By not setting {@code limit}, a default
  * of 10 will be displayed.
- * 
+ *
  * Created 16/08/17
  * @author Edd
  */
@@ -48,6 +48,29 @@ public class RequiredStatisticType {
         } else {
             this.limit = limit;
         }
+    }
+
+    @Override public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + limit;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        RequiredStatisticType that = (RequiredStatisticType) o;
+
+        if (limit != that.limit) {
+            return false;
+        }
+        return name != null ? name.equals(that.name) : that.name == null;
     }
 
     static RequiredStatisticType statsType(String name) {
--- annotation-rest/src/test/java/uk/ac/ebi/quickgo/annotation/service/statistics/StatisticsTypeConfigurerTest.java
+++ annotation-rest/src/test/java/uk/ac/ebi/quickgo/annotation/service/statistics/StatisticsTypeConfigurerTest.java
@@ -8,7 +8,9 @@ import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static uk.ac.ebi.quickgo.annotation.service.statistics.RequiredStatisticType.statsType;
 
 /**
@@ -20,13 +22,14 @@ import static uk.ac.ebi.quickgo.annotation.service.statistics.RequiredStatisticT
  */
 public class StatisticsTypeConfigurerTest {
 
-    public static final String GO_ID = "goId";
-    private static final int GO_ID_LIMIT = 5555;
     private StatisticsTypeConfigurer typeConfigurer;
     private Map<String, Integer> typeLimitProperties;
     private List<RequiredStatistic> requests;
     private static final String TAXON_ID = "taxonId";
-    private static final RequiredStatisticType TAXON_ID_TYPE = statsType(TAXON_ID);
+    private static final RequiredStatisticType EXPECTED_TAXON_ID_TYPE = statsType(TAXON_ID);
+    public static final String GO_ID = "goId";
+    private static final int GO_ID_LIMIT = 5555;
+    private static final RequiredStatisticType EXPECTED_GO_ID_TYPE = statsType(GO_ID, GO_ID_LIMIT);
 
     @Before
     public void setUp() {
@@ -36,7 +39,7 @@ public class StatisticsTypeConfigurerTest {
     }
 
     @Test
-    public void emptyStatsRequestsRemainEmpty() {
+    public void emptyRequiredStatsRemainEmpty() {
         // Given
         typeLimitProperties.put(GO_ID, 1111);
 
@@ -50,74 +53,70 @@ public class StatisticsTypeConfigurerTest {
     }
 
     @Test
-    public void noMatchingTypeLeavesStatsRequestsUntouched() {
+    public void noMatchingTypeLeavesRequiredStatsUntouched() {
         // Given
         typeLimitProperties.put(GO_ID, 1111);
         RequiredStatisticType requestType = statsType(TAXON_ID);
-        RequiredStatistic statsReq = statsReq(requestType);
-        requests.add(statsReq);
+        RequiredStatistic stat = stat(requestType);
+        requests.add(stat);
         
-        assertThat(requestType.getName(), is(TAXON_ID));
-        assertThat(requestType.getLimit(), is(Optional.empty()));
+        assertThat(requestType, is(equalTo(EXPECTED_TAXON_ID_TYPE)));
 
         // When
         typeConfigurer.configureStatsRequests(requests);
 
         // Then
-        assertThat(requests, contains(statsReq));
-        assertThat(statsReq.getTypes(), contains(requestType));
-        assertThat(requestType.getName(), is(TAXON_ID));
-        assertThat(requestType.getLimit(), is(Optional.empty()));
+        assertThat(requests, contains(stat));
+        assertThat(stat.getTypes(), contains(requestType));
+        assertThat(requestType, is(equalTo(EXPECTED_TAXON_ID_TYPE)));
     }
 
     @Test
-    public void emptyTypePropertiesLeavesStatsRequestsUntouched() {
+    public void emptyTypePropertiesLeavesRequiredStatsUntouched() {
         // Given
         RequiredStatisticType requestType = statsType(TAXON_ID);
-        RequiredStatistic statsReq = statsReq(requestType);
-        requests.add(statsReq);
+        RequiredStatistic stat = stat(requestType);
+        requests.add(stat);
 
         assertThat(typeLimitProperties.entrySet(), is(empty()));
-        assertThat(requestType.getName(), is(TAXON_ID));
-        assertThat(requestType.getLimit(), is(Optional.empty()));
+        assertThat(requestType, is(equalTo(EXPECTED_TAXON_ID_TYPE)));
 
         // When
         typeConfigurer.configureStatsRequests(requests);
 
         // Then
-        assertThat(requestType.getLimit(), is(Optional.empty())); // unchanged limit
-        assertThat(requests, contains(statsReq));
-        assertThat(statsReq.getTypes(), contains(requestType));
-        assertThat(requestType.getName(), is(TAXON_ID));
+        assertThat(requests, contains(stat));
+        assertThat(stat.getTypes(), contains(requestType));
+        assertThat(requestType, is(equalTo(EXPECTED_TAXON_ID_TYPE)));
     }
 
     @Test
-    public void matchingTypeUpdatesStatsRequests() {
+    public void matchingTypeUpdatesRequiredStats() {
         // Given
         typeLimitProperties.put(GO_ID, GO_ID_LIMIT);
         typeLimitProperties.put("aspectId", 20);
         RequiredStatisticType goIdType = statsType(GO_ID);
-        RequiredStatistic statsReq = statsReq(TAXON_ID_TYPE, goIdType);
-        requests.add(statsReq);
+        RequiredStatistic stat = stat(EXPECTED_TAXON_ID_TYPE, goIdType);
+        requests.add(stat);
 
-        assertThat(TAXON_ID_TYPE.getName(), is(TAXON_ID));
-        assertThat(TAXON_ID_TYPE.getLimit(), is(Optional.empty()));
+        assertThat(EXPECTED_TAXON_ID_TYPE.getName(), is(TAXON_ID));
+        assertThat(EXPECTED_TAXON_ID_TYPE.getLimit(), is(Optional.empty()));
         assertThat(goIdType.getName(), is(GO_ID));
         assertThat(goIdType.getLimit(), is(Optional.empty()));
+        assertThat(goIdType, is(not(equalTo(EXPECTED_GO_ID_TYPE)))); // limits differ
 
         // When
         typeConfigurer.configureStatsRequests(requests);
 
         // Then
-        assertThat(goIdType.getLimit(), is(Optional.of(GO_ID_LIMIT))); // limit updated
-        assertThat(requests, contains(statsReq));
-        assertThat(statsReq.getTypes(), contains(TAXON_ID_TYPE, goIdType));
-        assertThat(TAXON_ID_TYPE.getName(), is(TAXON_ID));
-        assertThat(TAXON_ID_TYPE.getLimit(), is(Optional.empty()));
-        assertThat(goIdType.getName(), is(GO_ID));
+        assertThat(requests, contains(stat));
+        assertThat(stat.getTypes(), contains(EXPECTED_TAXON_ID_TYPE, goIdType));
+        assertThat(EXPECTED_TAXON_ID_TYPE.getName(), is(TAXON_ID));
+        assertThat(EXPECTED_TAXON_ID_TYPE.getLimit(), is(Optional.empty()));
+        assertThat(goIdType, is(equalTo(EXPECTED_GO_ID_TYPE))); // where limits are now equal
     }
 
-    private RequiredStatistic statsReq(RequiredStatisticType... types) {
+    private RequiredStatistic stat(RequiredStatisticType... types) {
         return new RequiredStatistic(
                 "fakeGroupName",
                 "fakeGroupField",
