b7d7f08f837201c8bd95ef8fcf39b5e945a167c5
twardell <twardell@ebi.ac.uk>
2017-08-23 14:32:25 +0100

    Fail with excetion if the ontology files cannot be loaded, remove returning an optional.

--- sources/src/main/java/uk/ac/ebi/quickgo/ff/loader/ontology/AnnotationExtensionRelsLoader.java
+++ sources/src/main/java/uk/ac/ebi/quickgo/ff/loader/ontology/AnnotationExtensionRelsLoader.java
@@ -5,7 +5,6 @@ import uk.ac.ebi.quickgo.ff.loader.SourceInfoLoader;
 import uk.ac.ebi.quickgo.model.ontology.go.AnnotationExtensionRelations;
 import uk.ac.ebi.quickgo.model.ontology.go.GeneOntology;
 
-import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,88 +25,86 @@ public class AnnotationExtensionRelsLoader extends SourceInfoLoader<GOSourceFile
         this.geneOntology = geneOntology;
     }
 
-    @Override
-    protected AnnotationExtensionRelations newInstance() {
+    @Override protected AnnotationExtensionRelations newInstance() {
         return new AnnotationExtensionRelations(geneOntology);
     }
 
-    @Override
-    public Optional<AnnotationExtensionRelations> load() {
+    @Override public AnnotationExtensionRelations load() throws Exception {
         AnnotationExtensionRelations aer = getInstance();
 
-        try {
-            for (String[] row : sourceFiles.annExtRelations
-                    .reader(EAnnExtRelation.RELATION, EAnnExtRelation.USAGE,
-                            EAnnExtRelation.DOMAIN)) {
-                aer.annExtRelations.put(row[0],
-                        new AnnotationExtensionRelations.AnnotationExtensionRelation(row[0], row[1], row[2]));
-            }
-
-            for (String[] row : sourceFiles.aerRelations.reader(EAnnExtRelRelation.CHILD,
-                    EAnnExtRelRelation.PARENT, EAnnExtRelRelation.RELATION_TYPE)) {
-                AnnotationExtensionRelations.AnnotationExtensionRelation child = aer.annExtRelations.get(row[0]);
-                AnnotationExtensionRelations.AnnotationExtensionRelation parent = aer.annExtRelations.get(row[1]);
-                if (child != null && parent != null) {
-                    aer.relations.add(new AnnotationExtensionRelations.Relation(child, parent, row[2]));
-                    child.addParent(parent);
-                }
-            }
-
-            for (String[] row : sourceFiles.aerSecondaries.reader(EAnnExtRelSecondary.RELATION,
-                    EAnnExtRelSecondary.SECONDARY_ID)) {
-                AnnotationExtensionRelations.AnnotationExtensionRelation rel = aer.annExtRelations.get(row[0]);
-                if (rel != null) {
-                    rel.addSecondary(row[1]);
-                }
-            }
-
-            for (String[] row : sourceFiles.aerSubsets.reader(EAnnExtRelSubset.SUBSET,
-                    EAnnExtRelSubset.RELATION)) {
-                AnnotationExtensionRelations.AnnotationExtensionRelation rel = aer.annExtRelations.get(row[1]);
-                if (rel != null) {
-                    if ("_valid_relations_".equals(row[0])) {
-                        rel.setValidInExtension(true);
-                    } else if ("_displayed_relations_".equals(row[0])) {
-                        rel.setDisplayForCurators(true);
-                    } else {
-                        rel.addSubset(row[0]);
-                    }
-                }
-            }
-
-            for (String[] row : sourceFiles.aerDomains.reader(EAnnExtRelDomain.RELATION,
-                    EAnnExtRelDomain.ENTITY, EAnnExtRelDomain.ENTITY_TYPE)) {
-                AnnotationExtensionRelations.AnnotationExtensionRelation relation = aer.annExtRelations.get(row[0]);
-                if (relation != null && row[1] != null) {
-                    relation.addDomain(aer.getEntity(row[1], row[2]));
-                }
-            }
-
-            for (String[] row : sourceFiles.aerEntitySyntax.reader(EAnnExtRelEntitySyntax.ENTITY,
-                    EAnnExtRelEntitySyntax.ENTITY_TYPE, EAnnExtRelEntitySyntax.NAMESPACE,
-                    EAnnExtRelEntitySyntax.ID_SYNTAX)) {
-                AnnotationExtensionRelations.Entity entity = aer.getEntity(row[0], row[1]);
-                entity.addMatcher(aer.entityMatchers.getMatcher(row[2], row[3]));
-            }
-
-            for (String[] row : sourceFiles.aerRanges.reader(EAnnExtRelRange.RELATION,
-                    EAnnExtRelRange.ENTITY, EAnnExtRelRange.ENTITY_TYPE)) {
-                AnnotationExtensionRelations.AnnotationExtensionRelation relation = aer.annExtRelations.get(row[0]);
-                if (relation != null && row[1] != null) {
-                    relation.addRange(aer.getEntity(row[1], row[2]));
-                }
-            }
-
-            for (String[] row : sourceFiles.aerRangeDefaults.reader(EAnnExtRelRangeDefault.NAMESPACE,
-                    EAnnExtRelRangeDefault.ID_SYNTAX)) {
-                aer.rangeDefaults.add(aer.entityMatchers.getMatcher(row[0], row[1]));
-            }
-
-            return Optional.of(aer);
-        } catch (Exception e) {
-            LOGGER.error("Problem loading Annotation Extension Relationship information from source files: ", e);
+        for (String[] row : sourceFiles.annExtRelations.reader(EAnnExtRelation.RELATION,
+                                                               EAnnExtRelation.USAGE,
+                                                               EAnnExtRelation.DOMAIN)) {
+            aer.annExtRelations.put(row[0],
+                                    new AnnotationExtensionRelations.AnnotationExtensionRelation(row[0],
+                                                                                                 row[1],
+                                                                                                 row[2]));
         }
 
-        return Optional.empty();
+        for (String[] row : sourceFiles.aerRelations.reader(EAnnExtRelRelation.CHILD,
+                                                            EAnnExtRelRelation.PARENT,
+                                                            EAnnExtRelRelation.RELATION_TYPE)) {
+            AnnotationExtensionRelations.AnnotationExtensionRelation child = aer.annExtRelations.get(row[0]);
+            AnnotationExtensionRelations.AnnotationExtensionRelation parent = aer.annExtRelations.get(row[1]);
+            if (child != null && parent != null) {
+                aer.relations.add(new AnnotationExtensionRelations.Relation(child, parent, row[2]));
+                child.addParent(parent);
+            }
+        }
+
+        for (String[] row : sourceFiles.aerSecondaries.reader(EAnnExtRelSecondary.RELATION,
+                                                              EAnnExtRelSecondary.SECONDARY_ID)) {
+            AnnotationExtensionRelations.AnnotationExtensionRelation rel = aer.annExtRelations.get(row[0]);
+            if (rel != null) {
+                rel.addSecondary(row[1]);
+            }
+        }
+
+        for (String[] row : sourceFiles.aerSubsets.reader(EAnnExtRelSubset.SUBSET, EAnnExtRelSubset.RELATION)) {
+            AnnotationExtensionRelations.AnnotationExtensionRelation rel = aer.annExtRelations.get(row[1]);
+            if (rel != null) {
+                if ("_valid_relations_".equals(row[0])) {
+                    rel.setValidInExtension(true);
+                } else if ("_displayed_relations_".equals(row[0])) {
+                    rel.setDisplayForCurators(true);
+                } else {
+                    rel.addSubset(row[0]);
+                }
+            }
+        }
+
+        for (String[] row : sourceFiles.aerDomains.reader(EAnnExtRelDomain.RELATION,
+                                                          EAnnExtRelDomain.ENTITY,
+                                                          EAnnExtRelDomain.ENTITY_TYPE)) {
+            AnnotationExtensionRelations.AnnotationExtensionRelation relation = aer.annExtRelations.get(row[0]);
+            if (relation != null && row[1] != null) {
+                relation.addDomain(aer.getEntity(row[1], row[2]));
+            }
+        }
+
+        for (String[] row : sourceFiles.aerEntitySyntax.reader(EAnnExtRelEntitySyntax.ENTITY,
+                                                               EAnnExtRelEntitySyntax.ENTITY_TYPE,
+                                                               EAnnExtRelEntitySyntax.NAMESPACE,
+                                                               EAnnExtRelEntitySyntax.ID_SYNTAX)) {
+            AnnotationExtensionRelations.Entity entity = aer.getEntity(row[0], row[1]);
+            entity.addMatcher(aer.entityMatchers.getMatcher(row[2], row[3]));
+        }
+
+        for (String[] row : sourceFiles.aerRanges.reader(EAnnExtRelRange.RELATION,
+                                                         EAnnExtRelRange.ENTITY,
+                                                         EAnnExtRelRange.ENTITY_TYPE)) {
+            AnnotationExtensionRelations.AnnotationExtensionRelation relation = aer.annExtRelations.get(row[0]);
+            if (relation != null && row[1] != null) {
+                relation.addRange(aer.getEntity(row[1], row[2]));
+            }
+        }
+
+        for (String[] row : sourceFiles.aerRangeDefaults.reader(EAnnExtRelRangeDefault.NAMESPACE,
+                                                                EAnnExtRelRangeDefault.ID_SYNTAX)) {
+            aer.rangeDefaults.add(aer.entityMatchers.getMatcher(row[0], row[1]));
+        }
+
+        return aer;
+
     }
 }
