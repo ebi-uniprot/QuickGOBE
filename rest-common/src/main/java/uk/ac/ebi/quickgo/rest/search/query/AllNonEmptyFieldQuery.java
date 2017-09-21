a6d71cd5569f90cf4f4d9d02479832c5c91b4b7b
twardell <twardell@ebi.ac.uk>
2017-09-21 11:06:31 +0100

    Change description. Add equals(), hashCode() and toString() methods.

--- rest-common/src/main/java/uk/ac/ebi/quickgo/rest/search/query/AllNonEmptyFieldQuery.java
+++ rest-common/src/main/java/uk/ac/ebi/quickgo/rest/search/query/AllNonEmptyFieldQuery.java
@@ -1,7 +1,8 @@
 package uk.ac.ebi.quickgo.rest.search.query;
 
 /**
- * Represents the simplest of {@link QuickGOQuery} objects, containing just the field and values to query against.
+ * A version of {@link FieldQuery}, the use of which signifies that the requester is not looking for specific values
+ * from a field, but all of the target data where the selected field has a value that isn't empty or null.
  */
 public class AllNonEmptyFieldQuery extends FieldQuery {
 
@@ -11,5 +12,35 @@ public class AllNonEmptyFieldQuery extends FieldQuery {
 
     @Override public <T> T accept(QueryVisitor<T> visitor) {
         return visitor.visit(this);
+    }
+
+    @Override public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        AllNonEmptyFieldQuery that = (AllNonEmptyFieldQuery) o;
+
+        if (!field.equals(that.field)) {
+            return false;
+        }
+
+        return value.equals(that.value);
+    }
+
+    @Override public int hashCode() {
+        int result = field.hashCode();
+        result = 33 * result + value.hashCode();
+        return result;
+    }
+
+    @Override public String toString() {
+        return "AllNonEmptyFieldQuery{" +
+                "field='" + field + '\'' +
+                ", value='" + value + '\'' +
+                '}';
     }
 }
