3df9ee6a5f730681f606546f22d8a6818352d3e0
Edward Turner <eddturner@ebi.ac.uk>
2017-09-27 17:38:32 +0100

    hide catchall endpoint

--- geneproduct-rest/src/main/java/uk/ac/ebi/quickgo/geneproduct/controller/GeneProductController.java
+++ geneproduct-rest/src/main/java/uk/ac/ebi/quickgo/geneproduct/controller/GeneProductController.java
@@ -90,7 +90,7 @@ public class GeneProductController {
      *
      * @return a 400 response
      */
-    @ApiOperation(value = "Catches any bad requests and returns an error response with a 400 status")
+    @ApiOperation(value = "Catches any bad requests and returns an error response with a 400 status", hidden = true)
     @RequestMapping(value = "/*", method = {RequestMethod.GET}, produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<ResponseExceptionHandler.ErrorInfo> emptyId() {
         throw new ParameterException("The requested end-point does not exist.");
