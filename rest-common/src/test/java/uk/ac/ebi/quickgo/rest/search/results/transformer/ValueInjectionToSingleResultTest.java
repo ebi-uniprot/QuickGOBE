74f31aaa4bfd9440c6f4d53d0baef94be17ce5b0
twardell <twardell@ebi.ac.uk>
2017-10-25 10:10:38 +0100

    Reformat code.

--- rest-common/src/test/java/uk/ac/ebi/quickgo/rest/search/results/transformer/ValueInjectionToSingleResultTest.java
+++ rest-common/src/test/java/uk/ac/ebi/quickgo/rest/search/results/transformer/ValueInjectionToSingleResultTest.java
@@ -20,19 +20,23 @@ import static org.mockito.Mockito.when;
  * Time: 15:54
  * Created with IntelliJ IDEA.
  */
-@RunWith(MockitoJUnitRunner.class) public class ValueInjectionToSingleResultTest {
+@RunWith(MockitoJUnitRunner.class)
+public class ValueInjectionToSingleResultTest {
 
     private static final String GO_NAME_REQUEST = "goName";
     private static final String TAXON_NAME_REQUEST = "taxonName";
     private final List<ResponseValueInjector<FakeResponseModel>> requiredInjectors = new ArrayList<>();
-    @Mock private RESTFilterConverterFactory mockRestFetcher;
-    @Mock private FakeValueInjector mockGoNameInjector;
-    @Mock private FakeValueInjector mockTaxonNameInjector;
+    @Mock
+    private RESTFilterConverterFactory mockRestFetcher;
+    @Mock
+    private FakeValueInjector mockGoNameInjector;
+    @Mock
+    private FakeValueInjector mockTaxonNameInjector;
     private FakeResponseModel result;
     private ValueInjectionToSingleResult<FakeResponseModel> resultMutator;
 
-    @Before public void setup() {
-
+    @Before
+    public void setup() {
         when(mockGoNameInjector.getId()).thenReturn(GO_NAME_REQUEST);
         when(mockTaxonNameInjector.getId()).thenReturn(TAXON_NAME_REQUEST);
         requiredInjectors.add(mockGoNameInjector);
@@ -46,24 +50,25 @@ import static org.mockito.Mockito.when;
         new ValueInjectionToSingleResult(null);
     }
 
-    @Test public void everyInjectorUsedForEveryModel() {
+    @Test
+    public void everyInjectorUsedForEveryModel() {
         resultMutator.mutate(result, requiredInjectors);
 
         verify(mockGoNameInjector, times(1)).inject(mockRestFetcher, result);
         verify(mockTaxonNameInjector, times(1)).inject(mockRestFetcher, result);
-
     }
-
 
     /**
      * Used only for mocking purposes
      */
     private static class FakeValueInjector implements ResponseValueInjector<FakeResponseModel> {
-        @Override public String getId() {
+        @Override
+        public String getId() {
             return "fake id";
         }
 
-        @Override public void inject(RESTFilterConverterFactory restFetcher, FakeResponseModel annotation) {
+        @Override
+        public void inject(RESTFilterConverterFactory restFetcher, FakeResponseModel annotation) {
             // not implemented
         }
     }
