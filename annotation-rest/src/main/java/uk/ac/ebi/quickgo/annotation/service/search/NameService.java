a25d92ee3760500acacac5fc099d41b4714bd04c
twardell <twardell@ebi.ac.uk>
2017-11-15 11:31:33 +0000

    Don't pass CacheManager to the NameService. Use SpringEL to stop nulls being saved to the cache.

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/service/search/NameService.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/service/search/NameService.java
@@ -6,17 +6,14 @@ import uk.ac.ebi.quickgo.rest.search.results.transformer.ResultTransformationReq
 import uk.ac.ebi.quickgo.rest.search.results.transformer.ResultTransformationRequests;
 import uk.ac.ebi.quickgo.rest.search.results.transformer.ResultTransformerChain;
 
-import java.util.Objects;
-import net.sf.ehcache.CacheManager;
-import net.sf.ehcache.Element;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.interceptor.SimpleKey;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 /**
- * Provide a lookup service for names, caching results for use later when not null
+ * Provide a lookup service for names, caching results for use later.
  *
  * @author Tony Wardell
  * Date: 13/11/2017
@@ -28,43 +25,19 @@ import org.springframework.stereotype.Service;
 public class NameService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(NameService.class);
-    private static final String CACHE_NAME = "names";
     private final ResultTransformerChain<CompletableValue> completableValueTransformerChain;
-    private final CacheManager cacheManager;
 
-    public NameService(
-            ResultTransformerChain<CompletableValue> completableValueTransformerChain, CacheManager cacheManager) {
+    public NameService(ResultTransformerChain<CompletableValue> completableValueTransformerChain) {
         this.completableValueTransformerChain = completableValueTransformerChain;
-        this.cacheManager = cacheManager;
     }
 
+    @Cacheable(unless="#result == null")
     public String findName(String targetName, String targetKey) {
-        String returnValue;
-        SimpleKey simpleKey = new SimpleKey(targetName, targetKey);
-
-        Element value = cacheManager.getEhcache("names").get(simpleKey);
-        if(Objects.nonNull(value)){
-            returnValue = (String)value.getObjectValue();
-        }else {
-            returnValue = nameLookup(targetName, targetKey);
-            saveToCache(simpleKey, returnValue);
-        }
-        return returnValue;
-    }
-
-    private String nameLookup(String targetName, String targetKey) {
         FilterContext filterContext = createFilterContextForName(targetName);
         CompletableValue completableValue = new CompletableValue(targetKey);
         LOGGER.debug("Getting " + filterContext + " : " + targetKey);
         completableValueTransformerChain.applyTransformations(completableValue, filterContext);
         return completableValue.value;
-    }
-
-    private void saveToCache(SimpleKey simpleKey, String returnValue) {
-        if(Objects.nonNull(returnValue)) {
-            Element element = new Element(simpleKey, returnValue);
-            cacheManager.getEhcache(CACHE_NAME).put(element);
-        }
     }
 
     private static FilterContext createFilterContextForName(String targetName) {
