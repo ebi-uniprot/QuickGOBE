19af66a307d940a2e8822cc0e4de46aa8e0a8039
twardell <twardell@ebi.ac.uk>
2017-12-11 13:27:03 +0000

    Reformat code.

--- sources/src/main/java/uk/ac/ebi/quickgo/ff/loader/ontology/ECOLoader.java
+++ sources/src/main/java/uk/ac/ebi/quickgo/ff/loader/ontology/ECOLoader.java
@@ -3,10 +3,6 @@ package uk.ac.ebi.quickgo.ff.loader.ontology;
 import uk.ac.ebi.quickgo.ff.files.ontology.ECOSourceFiles;
 import uk.ac.ebi.quickgo.model.ontology.eco.EvidenceCodeOntology;
 
-import java.util.Optional;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * Loads a complete ECO ontology from a suite of source files.
  *
@@ -19,16 +15,16 @@ public class ECOLoader extends AbstractGenericOLoader<ECOSourceFiles, EvidenceCo
     }
 
     @Override
-    protected EvidenceCodeOntology newInstance() {
-        return new EvidenceCodeOntology();
+    public EvidenceCodeOntology load() throws Exception {
+        EvidenceCodeOntology eco = getInstance();
+
+        // add generic ontology info
+        createWithGenericOInfo(EvidenceCodeOntology.NAME_SPACE, null);
+        return eco;
     }
 
     @Override
-    public EvidenceCodeOntology load() throws Exception{
-            EvidenceCodeOntology eco = getInstance();
-
-            // add generic ontology info
-            createWithGenericOInfo(EvidenceCodeOntology.NAME_SPACE, null);
-            return eco;
+    protected EvidenceCodeOntology newInstance() {
+        return new EvidenceCodeOntology();
     }
 }
