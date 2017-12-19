fec8d1ad73124eb79eb26f486a51841240bbfe79
twardell <twardell@ebi.ac.uk>
2017-12-19 14:05:03 +0000

    Make class a extendable. Remove reference to gene product version of the stats type.

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/service/statistics/RequiredStatistics.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/service/statistics/RequiredStatistics.java
@@ -24,14 +24,13 @@ import static uk.ac.ebi.quickgo.annotation.service.statistics.RequiredStatisticT
  */
 public class RequiredStatistics {
     static final int DEFAULT_GO_TERM_LIMIT = 200;
-    static final List<RequiredStatisticType> ANNOTATION_STATS_TYPES;
-    static final List<RequiredStatisticType> GENE_PRODUCT_STATS_TYPES;
+    static final List<RequiredStatisticType> STATS_TYPES;
 
     static final String ANNOTATION = "annotation";
     static final String GENE_PRODUCT = "geneProduct";
 
     static {
-        ANNOTATION_STATS_TYPES = asList(
+        STATS_TYPES = asList(
                 statsType(AnnotationFields.Facetable.GO_ID, DEFAULT_GO_TERM_LIMIT),
                 statsType(AnnotationFields.Facetable.TAXON_ID),
                 statsType(AnnotationFields.Facetable.REFERENCE),
@@ -41,37 +40,29 @@ public class RequiredStatistics {
                 statsType(AnnotationFields.Facetable.GENE_PRODUCT_ID)
         );
 
-        GENE_PRODUCT_STATS_TYPES = asList(
-                statsType(AnnotationFields.Facetable.GO_ID, DEFAULT_GO_TERM_LIMIT),
-                statsType(AnnotationFields.Facetable.TAXON_ID),
-                statsType(AnnotationFields.Facetable.REFERENCE),
-                statsType(AnnotationFields.Facetable.EVIDENCE_CODE),
-                statsType(AnnotationFields.Facetable.ASSIGNED_BY),
-                statsType(AnnotationFields.Facetable.GO_ASPECT),
-                statsType(AnnotationFields.Facetable.GENE_PRODUCT_ID)
-        );
     }
 
-    private final List<RequiredStatistic> requiredStats;
-    private final List<RequiredStatisticType> configuredTypes;
+    protected List<RequiredStatistic> requiredStats;
+    protected List<RequiredStatisticType> configuredTypes;
+
+    public RequiredStatistics() {}
 
     RequiredStatistics(StatisticsTypeConfigurer statsConfigurer) {
         checkArgument(statsConfigurer != null, "Stats configurer cannot be null");
         configuredTypes = statsConfigurer.getConfiguredStatsTypes(STATS_TYPES);
-        requiredStats = Collections.unmodifiableList(
-                asList(annotationStats(), geneProductStats()));
+        requiredStats = Collections.unmodifiableList(asList(annotationStats(), geneProductStats()));
     }
 
-    public List<RequiredStatistic> getStats() {
+    public List<RequiredStatistic> getRequiredStatistics() {
         return requiredStats;
     }
 
-    private RequiredStatistic annotationStats() {
+    protected RequiredStatistic annotationStats() {
         return new RequiredStatistic(ANNOTATION, AnnotationFields.Facetable.ID,
                 AggregateFunction.COUNT.getName(), configuredTypes);
     }
 
-    private RequiredStatistic geneProductStats() {
+    protected RequiredStatistic geneProductStats() {
         return new RequiredStatistic(GENE_PRODUCT, AnnotationFields.Facetable.GENE_PRODUCT_ID,
                 AggregateFunction.UNIQUE.getName(), configuredTypes);
     }
