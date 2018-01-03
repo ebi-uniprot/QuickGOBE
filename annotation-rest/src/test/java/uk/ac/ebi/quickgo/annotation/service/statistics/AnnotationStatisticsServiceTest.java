6fcc7ddff6fcd59020220e1787ed9361494be24a
twardell <twardell@ebi.ac.uk>
2018-01-03 15:10:57 +0000

    Add tests to make sure the right permutation of RequiredStatistics is provided.

--- annotation-rest/src/test/java/uk/ac/ebi/quickgo/annotation/service/statistics/AnnotationStatisticsServiceTest.java
+++ annotation-rest/src/test/java/uk/ac/ebi/quickgo/annotation/service/statistics/AnnotationStatisticsServiceTest.java
@@ -3,7 +3,13 @@ package uk.ac.ebi.quickgo.annotation.service.statistics;
 import uk.ac.ebi.quickgo.annotation.model.Annotation;
 import uk.ac.ebi.quickgo.annotation.model.AnnotationRequest;
 import uk.ac.ebi.quickgo.rest.search.SearchService;
+import uk.ac.ebi.quickgo.rest.search.query.QuickGOQuery;
+import uk.ac.ebi.quickgo.rest.search.request.FilterRequest;
+import uk.ac.ebi.quickgo.rest.search.request.converter.ConvertedFilter;
+import uk.ac.ebi.quickgo.rest.search.request.converter.FilterConverter;
 import uk.ac.ebi.quickgo.rest.search.request.converter.FilterConverterFactory;
+import uk.ac.ebi.quickgo.rest.search.results.AggregateResponse;
+import uk.ac.ebi.quickgo.rest.search.results.QueryResult;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -11,9 +17,10 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
@@ -25,6 +32,7 @@ public class AnnotationStatisticsServiceTest {
     public ExpectedException thrown = ExpectedException.none();
 
     private AnnotationStatisticsService statsService;
+    private AnnotationRequest request;
 
     @Mock
     private FilterConverterFactory filterFactoryMock;
@@ -38,10 +46,25 @@ public class AnnotationStatisticsServiceTest {
     @Mock
     private RequiredStatisticsProvider requiredStatisticsProvider;
 
+    @Mock
+    private QueryResult<Annotation> queryResult;
+
+    @Mock
+    private AggregateResponse aggregateResponse;
+
+    @Mock
+    private ConvertedFilter<QuickGOQuery> convertedFilter;
+
     @Before
     public void setUp() {
+        request = new AnnotationRequest();
+
         statsService = new AnnotationStatisticsService(filterFactoryMock, searchServiceMock,
                 statsConverterMock, requiredStatisticsProvider);
+
+        when(searchServiceMock.findByQuery(any())).thenReturn(queryResult);
+        when(queryResult.getAggregation()).thenReturn(aggregateResponse);
+        when(filterFactoryMock.convert(any())).thenReturn(convertedFilter);
     }
 
     @Test
@@ -86,5 +109,54 @@ public class AnnotationStatisticsServiceTest {
         thrown.expectMessage("Annotation request cannot be null");
 
         statsService.calculateForStandardUsage(null);
+    }
+
+    @Test
+    public void requiredStatisticsProviderCalledForStandardUsageOnly() {
+        statsService = new AnnotationStatisticsService(filterFactoryMock, searchServiceMock,
+                statsConverterMock, requiredStatisticsProvider);
+
+        statsService.calculateForStandardUsage(request);
+
+        verify(requiredStatisticsProvider).getStandardUsage();
+    }
+
+    @Test
+    public void requiredStatisticsProviderCalledForStandardUsageWithGeneProductFiltering() {
+        statsService = new AnnotationStatisticsService(filterFactoryMock, searchServiceMock,
+                statsConverterMock, requiredStatisticsProvider);
+        request.setGeneProductId("A0A000");
+
+        statsService.calculateForStandardUsage(request);
+
+        verify(requiredStatisticsProvider).getStandardUsageWithGeneProductFiltering();
+    }
+
+    @Test
+    public void requiredStatisticsProviderCalledForDownloadUsageOnly() {
+        statsService = new AnnotationStatisticsService(filterFactoryMock, searchServiceMock,
+                statsConverterMock, requiredStatisticsProvider);
+
+        statsService.calculateForDownloadUsage(request);
+
+        verify(requiredStatisticsProvider).getDownloadUsage();
+    }
+
+    @Test
+    public void requiredStatisticsProviderCalledForDownloadUsageWithGeneProductFiltering() {
+        statsService = new AnnotationStatisticsService(filterFactoryMock, searchServiceMock,
+                statsConverterMock, requiredStatisticsProvider);
+        request.setGeneProductId("A0A000");
+
+        statsService.calculateForDownloadUsage(request);
+
+        verify(requiredStatisticsProvider).getDownloadUsageWithGeneProductFiltering();
+    }
+
+    class TestFilterConverter implements FilterConverter<FilterRequest, QuickGOQuery> {
+
+        @Override public ConvertedFilter<QuickGOQuery> transform(FilterRequest request) {
+            return convertedFilter;
+        }
     }
 }
