d60caa02a142b9684888453276e7b12de905c2f7
TonyWardell <ebitony@gmail.com>
2018-02-27 10:06:36 +0000

    We have a new evidence presets file.

--- quickgo-client/src/main/java/uk/ac/ebi/quickgo/client/service/loader/presets/ff/SourceColumnsFactory.java
+++ quickgo-client/src/main/java/uk/ac/ebi/quickgo/client/service/loader/presets/ff/SourceColumnsFactory.java
@@ -50,6 +50,13 @@ public class SourceColumnsFactory {
                 return RawNamedPresetColumnsBuilder
                         .createWithNamePosition(0)
                         .build();
+            case EVIDENCE_PRESETS_COLUMNS:
+                return RawNamedPresetColumnsBuilder
+                        .createWithNamePosition(0)
+                        .withDescriptionPosition(1)
+                        .withAssociationPosition(2)
+                        .withRelevancyPosition(3)
+                        .build();
             default:
                 throw new IllegalStateException("Source type: " + source + " is not handled.");
         }
@@ -63,6 +70,7 @@ public class SourceColumnsFactory {
         REF_COLUMNS,
         EXT_RELATION_COLUMNS,
         TAXON_COLUMNS,
-        EXT_DATABASE_COLUMNS
+        EXT_DATABASE_COLUMNS,
+        EVIDENCE_PRESETS_COLUMNS
     }
 }
