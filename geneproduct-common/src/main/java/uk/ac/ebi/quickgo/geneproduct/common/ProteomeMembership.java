9b8266e239abcdc9d26ac7c4f37e121129a57d6c
TonyWardell <ebitony@gmail.com>
2018-03-06 11:42:17 +0000

    An enumeration of the possible states of proteome membership a gene product can have.

---
+++ geneproduct-common/src/main/java/uk/ac/ebi/quickgo/geneproduct/common/ProteomeMembership.java
@@ -0,0 +1,12 @@
+package uk.ac.ebi.quickgo.geneproduct.common;
+
+/**
+ * An enumeration of the possible states of proteome membership a gene product can have.
+ * @author Tony Wardell
+ * Date: 06/03/2018
+ * Time: 10:33
+ * Created with IntelliJ IDEA.
+ */
+public enum ProteomeMembership {
+    REFERENCE, COMPLETE, NONE
+}
