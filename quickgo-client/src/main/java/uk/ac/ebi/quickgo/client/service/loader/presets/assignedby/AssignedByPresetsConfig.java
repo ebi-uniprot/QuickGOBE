f85fc8cdf4c2703e659e1de82c5570bd2b88748a
TonyWardell <ebitony@gmail.com>
2018-03-13 11:08:47 +0000

    Rename the key used for the RESTful communication, clean up the code a little.

--- quickgo-client/src/main/java/uk/ac/ebi/quickgo/client/service/loader/presets/assignedby/AssignedByPresetsConfig.java
+++ quickgo-client/src/main/java/uk/ac/ebi/quickgo/client/service/loader/presets/assignedby/AssignedByPresetsConfig.java
@@ -1,44 +1,22 @@
 package uk.ac.ebi.quickgo.client.service.loader.presets.assignedby;
 
 import uk.ac.ebi.quickgo.client.model.presets.PresetItem;
-import uk.ac.ebi.quickgo.client.model.presets.PresetType;
 import uk.ac.ebi.quickgo.client.model.presets.impl.CompositePresetImpl;
 import uk.ac.ebi.quickgo.client.service.loader.presets.LogStepListener;
 import uk.ac.ebi.quickgo.client.service.loader.presets.PresetsCommonConfig;
 import uk.ac.ebi.quickgo.client.service.loader.presets.ff.RawNamedPreset;
-import uk.ac.ebi.quickgo.client.service.loader.presets.ff.RawNamedPresetValidator;
-import uk.ac.ebi.quickgo.client.service.loader.presets.ff.SourceColumnsFactory;
-import uk.ac.ebi.quickgo.client.service.loader.presets.ff.StringToRawNamedPresetMapper;
-import uk.ac.ebi.quickgo.rest.search.RetrievalException;
-import uk.ac.ebi.quickgo.rest.search.request.FilterRequest;
-import uk.ac.ebi.quickgo.rest.search.request.config.FilterConfigRetrieval;
-import uk.ac.ebi.quickgo.rest.search.request.converter.ConvertedFilter;
 import uk.ac.ebi.quickgo.rest.search.request.converter.RESTFilterConverterFactory;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import org.slf4j.Logger;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.mapping.FieldSetMapper;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.core.io.Resource;
-import org.springframework.web.client.RestOperations;
 
-import static org.slf4j.LoggerFactory.getLogger;
+import static uk.ac.ebi.quickgo.client.model.presets.PresetType.*;
 import static uk.ac.ebi.quickgo.client.service.loader.presets.PresetsConfig.SKIP_LIMIT;
-import static uk.ac.ebi.quickgo.client.service.loader.presets.PresetsConfigHelper.compositeItemProcessor;
-import static uk.ac.ebi.quickgo.client.service.loader.presets.PresetsConfigHelper.fileReader;
-import static uk.ac.ebi.quickgo.client.service.loader.presets.PresetsConfigHelper.rawPresetMultiFileReader;
 import static uk.ac.ebi.quickgo.client.service.loader.presets.PresetsConfigHelper.topItemsFromRESTReader;
-import static uk.ac.ebi.quickgo.client.service.loader.presets.ff.SourceColumnsFactory.Source.DB_COLUMNS;
 
 /**
  * Exposes the {@link Step} bean that is used to read and populate information relating to the assignedBy preset data.
@@ -50,7 +28,7 @@ import static uk.ac.ebi.quickgo.client.service.loader.presets.ff.SourceColumnsFa
 @Import({PresetsCommonConfig.class})
 public class AssignedByPresetsConfig {
     public static final String ASSIGNED_BY_LOADING_STEP_NAME = "AssignedByReadingStep";
-    public static final String REST_KEY = "assignedBy";
+    public static final String ASSIGNED_BY_REST_KEY = "assignedBy";
 
     @Bean
     public Step assignedByStep(
@@ -63,7 +41,7 @@ public class AssignedByPresetsConfig {
                 .<RawNamedPreset, RawNamedPreset>chunk(chunkSize)
                 .faultTolerant()
                 .skipLimit(SKIP_LIMIT)
-                .reader(topItemsFromRESTReader(converterFactory, REST_KEY))
+                .reader(topItemsFromRESTReader(converterFactory, ASSIGNED_BY_REST_KEY))
                 .writer(rawPresetWriter(presets))
                 .listener(new LogStepListener())
                 .build();
@@ -75,13 +53,11 @@ public class AssignedByPresetsConfig {
      * @return the corresponding {@link ItemWriter}
      */
     ItemWriter<RawNamedPreset> rawPresetWriter(CompositePresetImpl presets) {
-        return rawItemList -> {
-            rawItemList.forEach(rawItem -> {
-                presets.addPreset(PresetType.ASSIGNED_BY,
-                        PresetItem.createWithName(rawItem.name)
-                                .withRelevancy(rawItem.relevancy)
-                                .build());
-            });
-        };
+        return rawItemList -> rawItemList.forEach(rawItem -> {
+            presets.addPreset(ASSIGNED_BY,
+                    PresetItem.createWithName(rawItem.name)
+                            .withRelevancy(rawItem.relevancy)
+                            .build());
+        });
     }
 }
