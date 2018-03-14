ca0fd81d3478b4bf843a30bceb5cf1453e51a527
TonyWardell <ebitony@gmail.com>
2018-03-14 10:17:11 +0000

    Since we need the descriptions from file, read the file first and then filter by facet of the rest results.

--- quickgo-client/src/main/java/uk/ac/ebi/quickgo/client/service/loader/presets/withFrom/WithFromPresetsConfig.java
+++ quickgo-client/src/main/java/uk/ac/ebi/quickgo/client/service/loader/presets/withFrom/WithFromPresetsConfig.java
@@ -1,22 +1,40 @@
 package uk.ac.ebi.quickgo.client.service.loader.presets.withFrom;
 
 import uk.ac.ebi.quickgo.client.model.presets.PresetItem;
+import uk.ac.ebi.quickgo.client.model.presets.PresetType;
 import uk.ac.ebi.quickgo.client.model.presets.impl.CompositePresetImpl;
 import uk.ac.ebi.quickgo.client.service.loader.presets.LogStepListener;
 import uk.ac.ebi.quickgo.client.service.loader.presets.PresetsCommonConfig;
-import uk.ac.ebi.quickgo.client.service.loader.presets.ff.RawNamedPreset;
+import uk.ac.ebi.quickgo.client.service.loader.presets.ff.*;
+import uk.ac.ebi.quickgo.rest.search.RetrievalException;
+import uk.ac.ebi.quickgo.rest.search.request.FilterRequest;
+import uk.ac.ebi.quickgo.rest.search.request.config.FilterConfigRetrieval;
+import uk.ac.ebi.quickgo.rest.search.request.converter.ConvertedFilter;
 import uk.ac.ebi.quickgo.rest.search.request.converter.RESTFilterConverterFactory;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.io.Resource;
+import org.springframework.web.client.RestOperations;
 
-import static uk.ac.ebi.quickgo.client.model.presets.PresetType.*;
 import static uk.ac.ebi.quickgo.client.service.loader.presets.PresetsConfig.SKIP_LIMIT;
-import static uk.ac.ebi.quickgo.client.service.loader.presets.PresetsConfigHelper.topItemsFromRESTReader;
+import static uk.ac.ebi.quickgo.client.service.loader.presets.PresetsConfigHelper.compositeItemProcessor;
+import static uk.ac.ebi.quickgo.client.service.loader.presets.PresetsConfigHelper.fileReader;
+import static uk.ac.ebi.quickgo.client.service.loader.presets.PresetsConfigHelper.rawPresetMultiFileReader;
+import static uk.ac.ebi.quickgo.client.service.loader.presets.ff.SourceColumnsFactory.Source.DB_COLUMNS;
 
 /**
  * Exposes the {@link Step} bean that is used to read and populate information relating to the with/from preset data.
@@ -27,24 +45,48 @@ import static uk.ac.ebi.quickgo.client.service.loader.presets.PresetsConfigHelpe
 @Configuration
 @Import({PresetsCommonConfig.class})
 public class WithFromPresetsConfig {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WithFromPresetsConfig.class);
+
     public static final String WITH_FROM_DB_LOADING_STEP_NAME = "WithFromDBReadingStep";
     public static final String WITH_FROM_REST_KEY = "withFrom";
 
+    @Value("#{'${withfrom.db.preset.source:}'.split(',')}")
+    private Resource[] resources;
+    @Value("${withfrom.db.preset.header.lines:1}")
+    private int headerLines;
+    private Set<String> duplicatePrevent = new HashSet<>();
+
     @Bean
-    public Step withFromStep(
+    public Step withFromDbStep(
             StepBuilderFactory stepBuilderFactory,
             Integer chunkSize,
             CompositePresetImpl presets,
-            RESTFilterConverterFactory converterFactory) {
+            FilterConfigRetrieval externalFilterConfigRetrieval,
+            RestOperations restOperations) {
+
+        RESTFilterConverterFactory converterFactory = converterFactory(externalFilterConfigRetrieval,
+                restOperations);
+
+        FlatFileItemReader<RawNamedPreset> itemReader = fileReader(rawPresetFieldSetMapper());
+        itemReader.setLinesToSkip(headerLines);
 
         return stepBuilderFactory.get(WITH_FROM_DB_LOADING_STEP_NAME)
                 .<RawNamedPreset, RawNamedPreset>chunk(chunkSize)
                 .faultTolerant()
                 .skipLimit(SKIP_LIMIT)
-                .reader(topItemsFromRESTReader(converterFactory, WITH_FROM_REST_KEY))
+                .<RawNamedPreset>reader(rawPresetMultiFileReader(resources, itemReader))
+                .processor(compositeItemProcessor(
+                        new RawNamedPresetValidator(),
+                        filterUsingRestResults(converterFactory),
+                        duplicateChecker()))
                 .writer(rawPresetWriter(presets))
                 .listener(new LogStepListener())
                 .build();
+    }
+
+    private RESTFilterConverterFactory converterFactory(FilterConfigRetrieval filterConfigRetrieval,
+            RestOperations restOperations) {
+        return new RESTFilterConverterFactory(filterConfigRetrieval, restOperations);
     }
 
     /**
@@ -52,12 +94,36 @@ public class WithFromPresetsConfig {
      * @param presets the presets to write to
      * @return the corresponding {@link ItemWriter}
      */
-    ItemWriter<RawNamedPreset> rawPresetWriter(CompositePresetImpl presets) {
-        return rawItemList -> rawItemList.forEach(rawItem -> {
-            presets.addPreset(WITH_FROM,
-                    PresetItem.createWithName(rawItem.name)
-                            .withRelevancy(rawItem.relevancy)
-                            .build());
-        });
+    private ItemWriter<RawNamedPreset> rawPresetWriter(CompositePresetImpl presets) {
+        return rawItemList -> rawItemList.forEach(rawItem ->
+                presets.addPreset(PresetType.WITH_FROM,
+                        PresetItem.createWithName(rawItem.name)
+                                .withProperty(PresetItem.Property.DESCRIPTION.getKey(), rawItem.description)
+                                .withRelevancy(rawItem.relevancy)
+                                .build())
+        );
+    }
+
+    private FieldSetMapper<RawNamedPreset> rawPresetFieldSetMapper() {
+        return new StringToRawNamedPresetMapper(SourceColumnsFactory.createFor(DB_COLUMNS));
+    }
+
+    ItemProcessor<RawNamedPreset, RawNamedPreset> duplicateChecker() {
+        return rawNamedPreset -> duplicatePrevent.add(rawNamedPreset.name.toLowerCase()) ? rawNamedPreset : null;
+    }
+
+    private ItemProcessor<RawNamedPreset, RawNamedPreset> filterUsingRestResults(
+            RESTFilterConverterFactory converterFactory) {
+        FilterRequest restRequest = FilterRequest.newBuilder().addProperty(WITH_FROM_REST_KEY).build();
+
+        try {
+            ConvertedFilter<List<String>> convertedFilter = converterFactory.convert(restRequest);
+            final List<String> convertedValues = convertedFilter.getConvertedValue();
+            final Set<String> validValues = new HashSet<>(convertedValues);
+            return rawNamedPreset -> validValues.contains(rawNamedPreset.name) ? rawNamedPreset : null;
+        } catch (RetrievalException | IllegalStateException e) {
+            LOGGER.error("Failed to retrieve via REST call the relevant 'with/from' values: ", e);
+        }
+        return rawNamedPreset -> rawNamedPreset;
     }
 }
