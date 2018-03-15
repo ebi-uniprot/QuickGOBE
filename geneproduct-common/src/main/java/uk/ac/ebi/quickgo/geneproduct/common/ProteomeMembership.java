6d50cf83eedcf5f04e2a38ffdc00643722116571
TonyWardell <ebitony@gmail.com>
2018-03-15 14:27:35 +0000

    Add memberhip() method for selection logic.

--- geneproduct-common/src/main/java/uk/ac/ebi/quickgo/geneproduct/common/ProteomeMembership.java
+++ geneproduct-common/src/main/java/uk/ac/ebi/quickgo/geneproduct/common/ProteomeMembership.java
@@ -8,5 +8,24 @@ package uk.ac.ebi.quickgo.geneproduct.common;
  * Created with IntelliJ IDEA.
  */
 public enum ProteomeMembership {
-    REFERENCE, COMPLETE, NONE, NOT_APPLICABLE
+    REFERENCE, COMPLETE, NONE, NOT_APPLICABLE;
+
+    /**
+     * Define the predicates required and order of importance to work out which Proteome membership category is
+     * applicable.
+     * @param isProtein is the gene product a protein
+     * @param isRef is the gene product a reference proteome
+     * @param isComplete is the gene product a member of a complete proteome.
+     * @return the ProteomeMembership matching the applied constraints
+     */
+    public static ProteomeMembership membership(boolean isProtein, boolean isRef, boolean isComplete) {
+        if (!isProtein) {
+            return NOT_APPLICABLE;
+        } else if (isRef) {
+            return REFERENCE;
+        } else if (isComplete) {
+            return COMPLETE;
+        }
+        return NONE;
+    }
 }
