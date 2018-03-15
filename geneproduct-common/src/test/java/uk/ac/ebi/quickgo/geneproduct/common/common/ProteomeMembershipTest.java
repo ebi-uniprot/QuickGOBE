90e45ca8bfe11ccdefed0e07dd50e26037481fa9
TonyWardell <ebitony@gmail.com>
2018-03-15 14:25:55 +0000

    Add tests for ProteomeMembership.

---
+++ geneproduct-common/src/test/java/uk/ac/ebi/quickgo/geneproduct/common/common/ProteomeMembershipTest.java
@@ -0,0 +1,33 @@
+package uk.ac.ebi.quickgo.geneproduct.common.common;
+
+import uk.ac.ebi.quickgo.geneproduct.common.ProteomeMembership;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.util.MatcherAssertionErrors.assertThat;
+import static uk.ac.ebi.quickgo.geneproduct.common.ProteomeMembership.COMPLETE;
+import static uk.ac.ebi.quickgo.geneproduct.common.ProteomeMembership.NONE;
+import static uk.ac.ebi.quickgo.geneproduct.common.ProteomeMembership.NOT_APPLICABLE;
+import static uk.ac.ebi.quickgo.geneproduct.common.ProteomeMembership.REFERENCE;
+
+/**
+ * @author Tony Wardell
+ * Date: 15/03/2018
+ * Time: 13:16
+ * Created with IntelliJ IDEA.
+ */
+public class ProteomeMembershipTest {
+
+    @Test
+    public void checkMembershipCreationRules() {
+        assertThat(ProteomeMembership.membership(false, true, true), is(NOT_APPLICABLE));
+        assertThat(ProteomeMembership.membership(false, false, true), is(NOT_APPLICABLE));
+        assertThat(ProteomeMembership.membership(false, true, false), is(NOT_APPLICABLE));
+        assertThat(ProteomeMembership.membership(true, true, true), is(REFERENCE));
+        assertThat(ProteomeMembership.membership(true, true, false), is(REFERENCE));
+        assertThat(ProteomeMembership.membership(true, false, true), is(COMPLETE));
+        assertThat(ProteomeMembership.membership(true, false, false), is(NONE));
+    }
+
+}
