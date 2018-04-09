9dec507c5d4015c0101d0b4e550a4e3669276edc
TonyWardell <ebitony@gmail.com>
2018-04-09 15:48:05 +0100

    A home for the logic to format dates into Strings.

---
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/download/converter/helpers/Date.java
@@ -0,0 +1,24 @@
+package uk.ac.ebi.quickgo.annotation.download.converter.helpers;
+
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.function.Function;
+
+/**
+ * A home for the logic to format dates into Strings.
+ *
+ * @author Tony Wardell
+ * Date: 09/04/2018
+ * Time: 14:54
+ * Created with IntelliJ IDEA.
+
+ */
+public class Date {
+
+    private Date() {}
+
+    private static final DateTimeFormatter YYYYMMDD_DATE_FORMAT = DateTimeFormatter.ofPattern("yyyyMMdd");
+    public static final Function<java.util.Date, String> toYYYYMMDD =
+            d -> d.toInstant().atZone(ZoneId.systemDefault()).format(YYYYMMDD_DATE_FORMAT);
+
+}
