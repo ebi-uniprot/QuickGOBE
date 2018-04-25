947638935f1e1a4b275dd63b0abb49c9d2782675
twardell <TonyWardell@users.noreply.github.com>
2018-04-25 10:59:09 +0100

    New class to repopulate arrays depending on requirements.

---
+++ common/src/main/java/uk/ac/ebi/quickgo/common/array/ArrayPopulation.java
@@ -0,0 +1,58 @@
+package uk.ac.ebi.quickgo.common.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * New class to repopulate arrays depending on requirements.
+ */
+
+public class ArrayPopulation {
+
+    private ArrayPopulation() {
+    }
+
+    /**
+     * If the targetArray array doesn't contain the value, then add it, else do nothing.
+     * @param targetArray value array to check and update if necessary.
+     * @param value to check for, update with.
+     * @return a String array that WILL contain the value.
+     */
+    public static String[] ensureArrayContains(String[] targetArray, String value) {
+        if (targetArray == null) {
+            return new String[]{value};
+        } else {
+            List<String> targetList = Arrays.asList(targetArray);
+            if (targetList.contains(value)) {
+                return targetArray;
+            } else {
+                List<String> fullList = new ArrayList<>(targetArray.length + 1);
+                fullList.addAll(targetList);
+                fullList.add(value);
+                return fullList.toArray(new String[0]);
+            }
+        }
+    }
+
+    /**
+     * If checkArray contains a value, make sure it exists in targetArray.
+     * @param checkArray the array of values to check if value exists.
+     * @param targetArray the array of values to add the value too (if it does already exist).
+     * @param value to check for and update to targetArray (if it does already exist).
+     * @return targetArray content, including value if checkArray contains value.
+     */
+    public static String[] updateFieldsWithCheckFields(String[] checkArray, String[] targetArray, String value) {
+        if (checkArray == null) {
+            return targetArray;
+        } else {
+            List<String> checkList = Arrays.asList(checkArray);
+            if (checkList.contains(value)) {
+                return ensureArrayContains(targetArray, value);
+            } else {
+                return Objects.isNull(targetArray) ? new String[0] : targetArray;
+            }
+        }
+    }
+}
