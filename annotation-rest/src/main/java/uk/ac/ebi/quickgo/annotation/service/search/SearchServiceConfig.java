10256141c3ac69a3e50f43a6cfe7584d16d701ed
twardell <TonyWardell@users.noreply.github.com>
2018-04-26 12:06:01 +0100

    The field is called 'extension' (singular) not 'extensions' (plural)

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/service/search/SearchServiceConfig.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/service/search/SearchServiceConfig.java
@@ -79,7 +79,7 @@ public class SearchServiceConfig {
                     "qualifier_unsorted,targetSet_unsorted,taxonId_unsorted";
     private static final String DEFAULT_ANNOTATION_SEARCH_RETURN_FIELDS =
             "id,geneProductId,qualifier,goId,goEvidence," +
-                    "evidenceCode,reference,withFrom,taxonId,assignedBy,extensions,symbol,geneProductId," +
+                    "evidenceCode,reference,withFrom,taxonId,assignedBy,extension,symbol,geneProductId," +
                     "interactingTaxonId,date";
     private static final String SOLR_ANNOTATION_QUERY_REQUEST_HANDLER = "/query";
     private static final String DEFAULT_DOWNLOAD_SORT_FIELDS = "rowNumber,id";
