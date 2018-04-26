381c8cea967363f71b11d4d505461fdbec0e257c
twardell <TonyWardell@users.noreply.github.com>
2018-04-26 10:30:49 +0100

    Reformat code.

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/download/converter/AnnotationToGPAD.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/download/converter/AnnotationToGPAD.java
@@ -20,11 +20,10 @@ import static uk.ac.ebi.quickgo.annotation.download.converter.helpers.Helper.nul
  *
  * An excerpt from a GPAD file is below:
  <pre>
-     UniProtKB	A0A000	enables	GO:0003824	GO_REF:0000002	ECO:0000256	InterPro:IPR015421|InterPro:IPR015422		20170107	InterPro		go_evidence=IEA
-     UniProtKB	A0A000	enables	GO:0003870	GO_REF:0000002	ECO:0000256	InterPro:IPR010961		20170107	InterPro		go_evidence=IEA
-     UniProtKB	A0A000	involved_in	GO:0009058	GO_REF:0000002	ECO:0000256	InterPro:IPR004839		20170107	InterPro		go_evidence=IEA
-     UniProtKB	A0A000	enables	GO:0030170	GO_REF:0000002	ECO:0000256	InterPro:IPR004839|InterPro:IPR010961		20170107	InterPro		go_evidence=IEA
-     UniProtKB	A0A000	involved_in	GO:0033014	GO_REF:0000002	ECO:0000256	InterPro:IPR010961		20170107	InterPro		go_evidence=IEA
+ UniProtKB	A0A000	enables	GO:0003824	GO_REF:0000002	ECO:0000256	InterPro:IPR015421|InterPro:IPR015422
+ 20170107	InterPro		go_evidence=IEA
+ UniProtKB	A0A000	enables	GO:0003870	GO_REF:0000002	ECO:0000256	InterPro:IPR010961		20170107	InterPro
+ go_evidence=IEA
  </pre>
  * @author Tony Wardell
  * Date: 17/01/2017
@@ -41,26 +40,26 @@ public class AnnotationToGPAD implements BiFunction<Annotation, List<String>, Li
         if (Objects.isNull(annotation.slimmedIds) || annotation.slimmedIds.isEmpty()) {
             return Collections.singletonList(toOutputRecord(annotation, annotation.goId));
         } else {
-            return annotation.slimmedIds.stream()
-                                        .map(goId -> this.toOutputRecord(annotation, goId))
-                                        .collect(toList());
+            return annotation.slimmedIds.stream().map(goId -> this.toOutputRecord(annotation, goId)).collect(toList());
         }
     }
 
     private String toOutputRecord(Annotation annotation, String goId) {
         StringJoiner tsvJoiner = new StringJoiner(OUTPUT_DELIMITER);
         final GeneProduct geneProduct = GeneProduct.fromString(annotation.geneProductId);
-        return tsvJoiner.add(geneProduct.db()).add(geneProduct.id())
+        return tsvJoiner.add(geneProduct.db())
+                .add(geneProduct.id())
                 .add(nullToEmptyString(annotation.qualifier))
                 .add(nullToEmptyString(goId))
                 .add(nullToEmptyString(annotation.reference))
                 .add(nullToEmptyString(annotation.evidenceCode))
                 .add(WithFrom.nullOrEmptyListToString(annotation.withFrom))
-                        .add(taxonIdAsString(annotation.interactingTaxonId))
+                .add(taxonIdAsString(annotation.interactingTaxonId))
                 .add(ofNullable(annotation.date).map(toYYYYMMDD).orElse(""))
                 .add(nullToEmptyString(annotation.assignedBy))
                 .add(Extensions.asString(annotation.extensions))
-                .add(GO_EVIDENCE + nullToEmptyString(annotation.goEvidence)).toString();
+                .add(GO_EVIDENCE + nullToEmptyString(annotation.goEvidence))
+                .toString();
     }
 
     private static final int LOWEST_VALID_TAXON_ID = 1;
