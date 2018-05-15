ab11c5f550e8083ed51baebb4e0b33f1e5336046
twardell <TonyWardell@users.noreply.github.com>
2018-05-15 11:00:44 +0100

    ISO_8601_FORMATTER now checks input for null,  returns blank String if this is true.

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/download/converter/helpers/DateConverter.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/download/converter/helpers/DateConverter.java
@@ -2,6 +2,7 @@ package uk.ac.ebi.quickgo.annotation.download.converter.helpers;
 
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 import java.util.function.Function;
 
 /**
@@ -17,7 +18,7 @@ public class DateConverter {
     private static final String ISO_8601_DATE_FORMAT = "yyyyMMdd";
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern(ISO_8601_DATE_FORMAT);
     public static final Function<java.util.Date, String> ISO_8601_FORMATTER =
-            d -> d.toInstant().atZone(ZoneId.systemDefault()).format(DATE_FORMATTER);
+            d -> Objects.isNull(d) ? "" : d.toInstant().atZone(ZoneId.systemDefault()).format(DATE_FORMATTER);
 
     private DateConverter() {
     }
