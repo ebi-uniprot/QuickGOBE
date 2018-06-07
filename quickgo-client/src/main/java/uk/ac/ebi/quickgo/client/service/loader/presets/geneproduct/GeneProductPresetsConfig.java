f5de4f2b05648834e770dda52a192bb11c92ab86
TonyWardell <ebitony@gmail.com>
2018-06-07 16:29:29 +0100

    Change layout.

--- quickgo-client/src/main/java/uk/ac/ebi/quickgo/client/service/loader/presets/geneproduct/GeneProductPresetsConfig.java
+++ quickgo-client/src/main/java/uk/ac/ebi/quickgo/client/service/loader/presets/geneproduct/GeneProductPresetsConfig.java
@@ -54,8 +54,8 @@ public class GeneProductPresetsConfig {
                 .<RawNamedPreset, RawNamedPreset>chunk(chunkSize)
                 .faultTolerant()
                 .skipLimit(SKIP_LIMIT)
-                .<RawNamedPreset>reader(
-                        rawPresetMultiFileReader(resources, itemReader)).processor(validatingItemProcessor())
+                .<RawNamedPreset>reader(rawPresetMultiFileReader(resources, itemReader)).processor(
+                        validatingItemProcessor())
                 .writer(rawPresetWriter(presets))
                 .listener(new LogStepListener())
                 .build();
