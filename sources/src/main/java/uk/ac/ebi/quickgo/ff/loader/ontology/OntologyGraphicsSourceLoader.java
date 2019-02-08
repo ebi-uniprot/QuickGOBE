bdbff9351752df880393be8bfb40e8baeb30cc75
Rizwan Ishtiaq <rizwan.ishtiaq@hotmail.com>
2019-02-08 16:36:01 +0000

    loading annotation extension relations into memory

--- sources/src/main/java/uk/ac/ebi/quickgo/ff/loader/ontology/OntologyGraphicsSourceLoader.java
+++ sources/src/main/java/uk/ac/ebi/quickgo/ff/loader/ontology/OntologyGraphicsSourceLoader.java
@@ -4,6 +4,7 @@ import uk.ac.ebi.quickgo.ff.files.ontology.ECOSourceFiles;
 import uk.ac.ebi.quickgo.ff.files.ontology.GOSourceFiles;
 import uk.ac.ebi.quickgo.graphics.service.GraphImageService;
 import uk.ac.ebi.quickgo.model.ontology.eco.EvidenceCodeOntology;
+import uk.ac.ebi.quickgo.model.ontology.go.AnnotationExtensionRelations;
 import uk.ac.ebi.quickgo.model.ontology.go.GeneOntology;
 
 import java.io.File;
@@ -27,6 +28,7 @@ public class OntologyGraphicsSourceLoader {
     private final File sourceDir;
     private GeneOntology geneOntology;
     private EvidenceCodeOntology evidenceCodeOntology;
+    private AnnotationExtensionRelations goAnnotationExtensionRelations;
 
     public OntologyGraphicsSourceLoader(File sourceDir) {
         checkArgument(sourceDir != null && sourceDir.exists(), "Source directory cannot be null and must exist");
@@ -44,15 +46,21 @@ public class OntologyGraphicsSourceLoader {
         return evidenceCodeOntology;
     }
 
+    public AnnotationExtensionRelations getGoAnnotationExtensionRelations(){
+        return goAnnotationExtensionRelations;
+    }
+
     public boolean isLoaded() {
         return geneOntology != null && evidenceCodeOntology != null;
     }
 
     private void loadOntologies() {
         try {
-            geneOntology = new GOLoader(new GOSourceFiles(requireNonNull(sourceDir))).load();
+            GOSourceFiles goSourceFiles = new GOSourceFiles(requireNonNull(sourceDir));
+            geneOntology = new GOLoader(goSourceFiles).load();
+            goAnnotationExtensionRelations = new AnnotationExtensionRelsLoader(goSourceFiles, geneOntology).load();
         } catch (Exception e) {
-            LOGGER.warn("Could not load Gene Ontology correctly. No graphics can be provided.");
+            LOGGER.warn("Could not load Gene Ontology or its Annotation Extension Relations correctly. No graphics can be provided.");
         }
 
         try {
