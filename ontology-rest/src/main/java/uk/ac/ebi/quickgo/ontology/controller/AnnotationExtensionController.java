9579042871f994a64ca1bcb6ab9eb220a2c28fa0
rizwan-ishtiaq <34174422+rizwan-ishtiaq@users.noreply.github.com>
2019-02-09 10:06:23 +0000

    Update AnnotationExtensionController.java

--- ontology-rest/src/main/java/uk/ac/ebi/quickgo/ontology/controller/AnnotationExtensionController.java
+++ ontology-rest/src/main/java/uk/ac/ebi/quickgo/ontology/controller/AnnotationExtensionController.java
@@ -3,6 +3,7 @@ package uk.ac.ebi.quickgo.ontology.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,6 +21,7 @@ public class AnnotationExtensionController {
 
     private AnnotationExtensionService annotationExtensionService;
 
+    @Autowired
     public AnnotationExtensionController(AnnotationExtensionService annotationExtensionService) {
         this.annotationExtensionService = annotationExtensionService;
     }
