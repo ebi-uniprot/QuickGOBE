97647d098a77da2449ffe9edeeb0db03b7ed4868
Rizwan Ishtiaq <rizwan.ishtiaq@hotmail.com>
2019-04-26 14:46:31 +0100

    only show annotations which go ids (including desendents) asked for

--- annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/service/comm/rest/ontology/converter/AndDescendantsFilterConverter.java
+++ annotation-rest/src/main/java/uk/ac/ebi/quickgo/annotation/service/comm/rest/ontology/converter/AndDescendantsFilterConverter.java
@@ -10,6 +10,7 @@ import uk.ac.ebi.quickgo.rest.search.request.converter.ConvertedFilter;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.Objects.nonNull;
 import static uk.ac.ebi.quickgo.common.validator.OntologyIdPredicate.isValidGOTermId;
@@ -21,17 +22,19 @@ public class AndDescendantsFilterConverter extends AbstractOntologyFilterConvert
   @Override
   public ConvertedFilter<QuickGOQuery> transform(OntologyRelatives response) {
     if (nonNull(response.getResults())) {
-      Set<Set<QuickGOQuery>> queries = new HashSet<>();
+      Set<Set<QuickGOQuery>> gpRelatedGoIdQueries = new HashSet<>();
+      Set<Set<QuickGOQuery>> goIdQueries = new HashSet<>();
 
       for (OntologyRelatives.Result result : response.getResults()) {
         if (validResult(result)) {
-          queries.add(process(result));
+          gpRelatedGoIdQueries.add(produceQueries(result, AnnotationFields.Searchable.GP_RELATED_GO_IDS));
+          goIdQueries.add(produceQueries(result, AnnotationFields.Searchable.GO_ID));
         } else {
           addIdWithNoRelative(result.getId());
         }
       }
 
-      convertedFilter = getFilter(queries);
+      convertedFilter = getFilter(gpRelatedGoIdQueries, goIdQueries);
       handleInvalidIds();
     }
 
@@ -52,27 +55,41 @@ public class AndDescendantsFilterConverter extends AbstractOntologyFilterConvert
     throw new UnsupportedOperationException();
   }
 
-  private Set<QuickGOQuery> process(OntologyRelatives.Result result) {
+  private Set<QuickGOQuery> produceQueries(OntologyRelatives.Result result, String fieldName) {
     return result.getDescendants().stream()
       .filter(desc -> !Strings.isNullOrEmpty(desc))
-      .map(this::createQuery)
+      .map(id -> createQuery(id, fieldName))
       .collect(Collectors.toSet());
   }
 
-  private ConvertedFilter<QuickGOQuery> getFilter(Set<Set<QuickGOQuery>> queries) {
-    if (!queries.isEmpty()) {
-      Set<QuickGOQuery> andSet = queries.stream().map(orSet -> or(orSet.toArray(new QuickGOQuery[orSet.size()]))).collect(Collectors.toSet());
-      return new ConvertedFilter<>(and(andSet.toArray(new QuickGOQuery[queries.size()])));
+  private ConvertedFilter<QuickGOQuery> getFilter(Set<Set<QuickGOQuery>> gpRelatedGoIdQueries, Set<Set<QuickGOQuery>> goIdQueries) {
+    if (!gpRelatedGoIdQueries.isEmpty()) {
+      Set<QuickGOQuery> gpRelatedOrPerResult = convertToOrQuerySet(gpRelatedGoIdQueries);
+      Set<QuickGOQuery> goIdsOrSet = convertToOrQuerySet(goIdQueries);
+      QuickGOQuery goIdsQuery = or(goIdsOrSet.toArray(new QuickGOQuery[goIdsOrSet.size()]));
+
+      return new ConvertedFilter<>(and(getArray(goIdsQuery, gpRelatedOrPerResult)));
     } else {
       return FILTER_EVERYTHING;
     }
   }
 
-  private QuickGOQuery createQuery(String id) {
-    if (isValidGOTermId().test(id)) {
-      return QuickGOQuery.createQuery(AnnotationFields.Searchable.GP_RELATED_GO_IDS, id);
+  private QuickGOQuery[] getArray(QuickGOQuery goIdsQuery, Set<QuickGOQuery> gpRelatedOrPerResult) {
+    Set<QuickGOQuery> tmp = new HashSet<>();
+    tmp.add(goIdsQuery);
+    tmp.addAll(gpRelatedOrPerResult);
+    return tmp.toArray(new QuickGOQuery[0]);
+  }
+
+  private Set<QuickGOQuery> convertToOrQuerySet(Set<Set<QuickGOQuery>> queries){
+    return queries.stream().map(orSet -> or(orSet.toArray(new QuickGOQuery[orSet.size()]))).collect(Collectors.toSet());
+  }
+
+  private QuickGOQuery createQuery(String goId, String fieldName) {
+    if (isValidGOTermId().test(goId)) {
+      return QuickGOQuery.createQuery(fieldName, goId);
     } else {
-      throw new RetrievalException(String.format(UNKNOWN_ID_FORMAT, id));
+      throw new RetrievalException(String.format(UNKNOWN_ID_FORMAT, goId));
     }
   }
 }
