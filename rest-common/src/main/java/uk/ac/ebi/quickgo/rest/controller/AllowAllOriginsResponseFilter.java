445d9b78a31dd444428e08bcd4b860406d7ae9c1
Rizwan Ishtiaq <rizwan.ishtiaq@hotmai.com>
2020-03-24 11:29:04 +0000

    allows content type header incase of post json request

--- rest-common/src/main/java/uk/ac/ebi/quickgo/rest/controller/AllowAllOriginsResponseFilter.java
+++ rest-common/src/main/java/uk/ac/ebi/quickgo/rest/controller/AllowAllOriginsResponseFilter.java
@@ -8,7 +8,9 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import static org.springframework.http.HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS;
 import static org.springframework.http.HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 
 /**
  * Defines a simple request filter that adds an Access-Control-Allow-Origin header with the value '*' to REST
@@ -28,6 +30,7 @@ public class AllowAllOriginsResponseFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
             FilterChain filterChain) throws ServletException, IOException {
         response.addHeader(ACCESS_CONTROL_ALLOW_ORIGIN, ALLOW_ALL_ORIGINS);
+        response.addHeader(ACCESS_CONTROL_ALLOW_HEADERS, CONTENT_TYPE);
         filterChain.doFilter(request, response);
     }
 }
